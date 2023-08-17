fbd6baf18844a52b55792f996285214fb7bf59d3
Emanuele Stoppa <my.burning@gmail.com>
2023-08-17 09:30:30 +0100

    chore: let's try this instead

--- website/astro.config.ts
+++ website/astro.config.ts
@@ -9,6 +9,7 @@ import { globby } from "globby";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import rehypeSlug from "rehype-slug";
 import remarkToc from "remark-toc";
+import vercel from "@astrojs/vercel/static";
 
 function resolveFile(relative: string, parent: string, root: string): string {
 	if (relative[0] === "/") {
@@ -153,7 +154,6 @@ function remarkDefaultLayoutPlugin() {
 export default defineConfig({
 	site: "https://rome.tools",
 	output: "static",
-	outDir: "build",
 
 	integrations: [
 		react(),
@@ -183,6 +183,8 @@ export default defineConfig({
 		],
 		extendDefaultPlugins: true,
 	},
+
+	adapter: vercel(),
 
 	vite: {
 		plugins: [],
