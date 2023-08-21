b56c80860edc11e57281beabc7b6371571fcb443
Victorien Elvinger <victorien@elvinger.fr>
2023-08-21 18:20:49 +0200

    refactor(favicon): use background-less favicon

--- website/src/playground/components/RomeHeader.tsx
+++ website/src/playground/components/RomeHeader.tsx
@@ -1,16 +1,9 @@
 import biomeIcon from "../../svg/logomark.svg";
-import biomeDarkIcon from "../../svg/logomark_white_yellow.svg";
-import { useTheme } from "../utils";
 
 export default function RomeHeader() {
-	const theme = useTheme();
-
 	return (
 		<>
-			<img
-				alt="Biome logo"
-				src={theme === "dark" ? biomeDarkIcon : biomeIcon}
-			/>
+			<img alt="Biome logo" src={biomeIcon} />
 			<span>Biome</span>
 		</>
 	);
