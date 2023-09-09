7ede9d51904a1e874874cdb2f2344717b93c4924
Nicolas Hedger <649677+nhedger@users.noreply.github.com>
2023-09-09 13:44:32 +0200

    fix(vscode): disable extension if required config file is missing (#210)

--- editors/vscode/src/main.ts
+++ editors/vscode/src/main.ts
@@ -39,6 +39,27 @@ export async function activate(context: ExtensionContext) {
 	const outputChannel = window.createOutputChannel("Biome");
 	const traceOutputChannel = window.createOutputChannel("Biome Trace");
 
+	const requiresConfiguration = workspace
+		.getConfiguration("biome")
+		.get<boolean>("requireConfiguration");
+
+	// If the extension requires a configuration file to be present, we attempt to
+	// locate it. If a config file cannot be found, we do not go any further.
+	if (requiresConfiguration) {
+		outputChannel.appendLine("Configuration file required, looking for one.");
+		// TODO: Stop looking for rome.json when we reach biome v2.0
+		const configFiles = await workspace.findFiles("**/{biome,rome}.json");
+		if (configFiles.length === 0) {
+			outputChannel.appendLine(
+				"No config file found, disabling Biome extension",
+			);
+			return;
+		}
+		outputChannel.appendLine(
+			`Config file found at ${configFiles[0].fsPath}, enabling Biome extension`,
+		);
+	}
+
 	const command = await getServerPath(context, outputChannel);
 
 	if (!command) {
