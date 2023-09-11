f468911604e2d11d8f1ed73e4f2ce2db8d1d1673
Denis Bezrukov <6227442+denbezrukov@users.noreply.github.com>
2023-09-11 12:02:01 +0300

    fix(analyzer): noRedeclare breaks *.d.ts files (#228)

---
+++ crates/rome_js_analyze/tests/specs/suspicious/noRedeclare/valid-modules.ts
@@ -0,0 +1,15 @@
+declare module '*.gif' {
+	const src: string
+}
+
+declare module '*.bmp' {
+	const src: string
+}
+
+declare module a {
+	const src: string
+}
+
+declare module b {
+	const src: string
+}
