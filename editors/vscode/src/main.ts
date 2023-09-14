84770ca0b284356fad18370659fec0fc54d086a3
Nicolas Hedger <649677+nhedger@users.noreply.github.com>
2023-09-14 18:08:47 +0200

    fix(vscode): server bin path resolution (#227)
    Co-authored-by: Emanuele Stoppa <my.burning@gmail.com>

--- editors/vscode/src/main.ts
+++ editors/vscode/src/main.ts
@@ -71,6 +71,8 @@ export async function activate(context: ExtensionContext) {
 		return;
 	}
 
+	outputChannel.appendLine(`Using Biome from ${command}`);
+
 	const statusBar = new StatusBar();
 
 	const serverOptions: ServerOptions = createMessageTransports.bind(
@@ -241,45 +243,22 @@ async function getWorkspaceRelativePath(path: string) {
 async function getWorkspaceDependency(
 	outputChannel: OutputChannel,
 ): Promise<string | undefined> {
-	const packageName = PLATFORMS[process.platform]?.[process.arch]?.package;
-
-	const manifestName = `${packageName}/package.json`;
-	const binaryName =
-		process.platform === "win32"
-			? `${packageName}/biome.exe`
-			: `${packageName}/biome`;
-
 	for (const workspaceFolder of workspace.workspaceFolders) {
-		try {
-			const options = {
-				basedir: workspaceFolder.uri.fsPath,
-			};
+		const path = Uri.joinPath(
+			workspaceFolder.uri,
+			"node_modules",
+			".bin",
+			"biome",
+		);
 
-			const [manifestPath, binaryPath] = await Promise.all([
-				resolveAsync(manifestName, options),
-				resolveAsync(binaryName, options),
-			]);
-
-			if (!(manifestPath && binaryPath)) {
-				continue;
-			}
-
-			// Load the package.json manifest of the resolved package
-			const manifestUri = Uri.file(manifestPath);
-			const manifestData = await workspace.fs.readFile(manifestUri);
-
-			const { version } = JSON.parse(new TextDecoder().decode(manifestData));
-			if (typeof version !== "string") {
-				continue;
-			}
-
-			return binaryPath;
-		} catch (e) {
-			console.log(e);
-			window.showWarningMessage(`The extension couldn't resolve ${manifestName} or ${binaryName}.
-			If you installed "@biomejs/biome", it's a resolving issue due to your package manager. Check the troubleshooting section of the extension for more information on how to fix the issue.`);
+		if (await fileExists(path)) {
+			return path.fsPath;
 		}
 	}
+
+	window.showWarningMessage(
+		"Unable to resolve the biome server from your dependencies. Make sure it's correctly installed, or untick the `requireConfiguration` setting to use the bundled binary.",
+	);
 
 	return undefined;
 }
@@ -376,6 +355,7 @@ async function getSocket(
 ): Promise<string> {
 	const process = spawn(command, ["__print_socket"], {
 		stdio: [null, "pipe", "pipe"],
+		shell: true,
 	});
 
 	const stdout = { content: "" };
