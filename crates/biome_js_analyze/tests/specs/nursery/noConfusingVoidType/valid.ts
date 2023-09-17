37e91e7b50e1bee7e4b0a2698afb430669d8b249
Mahesh Vagicherla <59088937+b4s36t4@users.noreply.github.com>
2023-09-17 18:49:53 +0530

    fix: void error on type of a function declaration (#309)
    Co-authored-by: Victorien Elvinger <victorien@elvinger.fr>

--- crates/biome_js_analyze/tests/specs/nursery/noConfusingVoidType/valid.ts
+++ crates/biome_js_analyze/tests/specs/nursery/noConfusingVoidType/valid.ts
@@ -5,3 +5,5 @@ logAndReturn<void>(undefined);
 
 let voidPromise: Promise<void> = new Promise<void>(() => { });
 let voidMap: Map<string, void> = new Map<string, void>();
+
+type FallbackHandler = (error?: Error) => void;
