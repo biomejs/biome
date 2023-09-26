98ec1435a882c41b796c2947e224c12b6565d110
Victor Teles <contato@victormesquita.dev>
2023-09-26 01:01:31 -0300

    fix(intellij):  biome format doesn't resolve biome.json

--- editors/intellij/src/main/kotlin/com/github/biomejs/intellijbiome/BiomeUtils.kt
+++ editors/intellij/src/main/kotlin/com/github/biomejs/intellijbiome/BiomeUtils.kt
@@ -16,71 +16,57 @@ import com.intellij.javascript.nodejs.interpreter.wsl.WslNodeInterpreter
 import com.intellij.execution.ExecutionException
 
 object BiomeUtils {
-    fun isSupportedFileType(file: VirtualFile): Boolean = when (file.extension) {
-        "js", "mjs", "cjs", "jsx", "ts", "mts", "cts", "tsx", "d.ts", "json", "jsonc" -> true
-        else -> false
-    }
-
-    fun getBiomeVersion(project: Project, binaryPath: String): String? {
-        if (binaryPath.isEmpty()) {
-            return null
-        }
-
-        val versionRegex = Regex("\\d{1,2}\\.\\d{1,2}\\.\\d{1,3}")
-
-        val commandLine = createNodeCommandLine(project, binaryPath).apply {
-					addParameter("--version")
-				}
-
-        val output = ExecUtil.execAndGetOutput(commandLine)
-
-        val matchResult = versionRegex.find(output.stdout)
-
-        return matchResult?.value
-    }
-
-		fun attachConfigPath(params: SmartList<String>, project: Project, logger: Logger) {
-			project.basePath?.let {
-					try {
-							val file = File(it)
-							if (file.exists()) {
-									params.add("--config-path")
-									params.add(project.basePath)
-							}
-					} catch (error: Exception) {
-							logger.error(error.message)
-					}
-			}
+	fun isSupportedFileType(file: VirtualFile): Boolean = when (file.extension) {
+		"js", "mjs", "cjs", "jsx", "ts", "mts", "cts", "tsx", "d.ts", "json", "jsonc" -> true
+		else -> false
 	}
 
-    fun getBiomeExecutablePath(project: Project): String? {
-        val directoryManager = NodeModulesDirectoryManager.getInstance(project)
-        val executablePath = BiomeSettings.getInstance(project).executablePath
-        val biomeBinFile = directoryManager.nodeModulesDirs
-            .asSequence()
-            .mapNotNull { it.findFileByRelativePath(".bin/biome") }
-            .filter { it.isValid }
-            .firstOrNull()
-
-        if (executablePath.isEmpty()) {
-            return biomeBinFile?.path
-        }
-
-        return executablePath
-    }
-
-		fun createNodeCommandLine(project: Project, executable: String): GeneralCommandLine {
-			val interpreter = NodeJsInterpreterManager.getInstance(project).interpreter
-			if (interpreter !is NodeJsLocalInterpreter && interpreter !is WslNodeInterpreter) {
-				throw ExecutionException(BiomeBundle.message("biome.interpreter.not.configured"))
-			}
-
-			return GeneralCommandLine().apply {
-				withParentEnvironmentType(GeneralCommandLine.ParentEnvironmentType.CONSOLE)
-				addParameter(executable)
-
-				NodeCommandLineConfigurator.find(interpreter)
-				.configure(this, NodeCommandLineConfigurator.defaultOptions(project))
-			}
+	fun getBiomeVersion(project: Project, binaryPath: String): String? {
+		if (binaryPath.isEmpty()) {
+			return null
 		}
+
+		val versionRegex = Regex("\\d{1,2}\\.\\d{1,2}\\.\\d{1,3}")
+
+		val commandLine = createNodeCommandLine(project, binaryPath).apply {
+			addParameter("--version")
+		}
+
+		val output = ExecUtil.execAndGetOutput(commandLine)
+
+		val matchResult = versionRegex.find(output.stdout)
+
+		return matchResult?.value
+	}
+
+	fun getBiomeExecutablePath(project: Project): String? {
+		val directoryManager = NodeModulesDirectoryManager.getInstance(project)
+		val executablePath = BiomeSettings.getInstance(project).executablePath
+		val biomeBinFile = directoryManager.nodeModulesDirs
+			.asSequence()
+			.mapNotNull { it.findFileByRelativePath(".bin/biome") }
+			.filter { it.isValid }
+			.firstOrNull()
+
+		if (executablePath.isEmpty()) {
+			return biomeBinFile?.path
+		}
+
+		return executablePath
+	}
+
+	fun createNodeCommandLine(project: Project, executable: String): GeneralCommandLine {
+		val interpreter = NodeJsInterpreterManager.getInstance(project).interpreter
+		if (interpreter !is NodeJsLocalInterpreter && interpreter !is WslNodeInterpreter) {
+			throw ExecutionException(BiomeBundle.message("biome.interpreter.not.configured"))
+		}
+
+		return GeneralCommandLine().apply {
+			withParentEnvironmentType(GeneralCommandLine.ParentEnvironmentType.CONSOLE)
+			addParameter(executable)
+
+			NodeCommandLineConfigurator.find(interpreter)
+				.configure(this, NodeCommandLineConfigurator.defaultOptions(project))
+		}
+	}
 }
