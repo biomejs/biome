79e6955c825955d41612f3349e02bc2b1088e610
Emanuele Stoppa <my.burning@gmail.com>
2023-09-27 11:59:11 +0200

    chore: more deprecations

--- packages/@biomejs/backend-jsonrpc/src/workspace.ts
+++ packages/@biomejs/backend-jsonrpc/src/workspace.ts
@@ -216,6 +216,10 @@ export interface JavascriptFormatter {
 	 */
 	indentStyle?: PlainIndentStyle;
 	/**
+	 * The size of the indentation applied to JavaScript (and its super languages) files. Default to 2.
+	 */
+	indentWidth?: number;
+	/**
 	 * The type of quotes used in JSX. Defaults to double.
 	 */
 	jsxQuoteStyle?: QuoteStyle;
@@ -265,6 +269,10 @@ export interface JsonFormatter {
 	 * The indent style applied to JSON (and its super languages) files.
 	 */
 	indentStyle?: PlainIndentStyle;
+	/**
+	 * The size of the indentation applied to JSON (and its super languages) files. Default to 2.
+	 */
+	indentWidth?: number;
 	/**
 	 * What's the max width of a line, applied to JSON (and its super languages) files. Defaults to 80.
 	 */
