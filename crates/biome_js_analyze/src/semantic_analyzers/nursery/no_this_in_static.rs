808205cbe684c88c86874d7c69ebb711eddd9c8e
Juan Di Toro <ditorojuanf@gmail.com>
2023-10-25 22:36:47 +0200

    fix: better information and tips for the user to understand the error

--- crates/biome_js_analyze/src/semantic_analyzers/nursery/no_this_in_static.rs
+++ crates/biome_js_analyze/src/semantic_analyzers/nursery/no_this_in_static.rs
@@ -1,6 +1,8 @@
 use biome_analyze::{context::RuleContext, declare_rule, Ast, Rule, RuleDiagnostic};
 use biome_console::markup;
-use biome_js_syntax::{JsMethodClassMember, JsSuperExpression, JsThisExpression};
+use biome_js_syntax::{
+    JsCallExpression, JsClassDeclaration, JsMethodClassMember, JsSuperExpression, JsThisExpression,
+};
 use biome_rowan::{declare_node_union, AstNode, AstNodeList};
 
 declare_rule! {
@@ -115,12 +117,44 @@ impl Rule for NoThisInStatic {
     fn diagnostic(ctx: &RuleContext<Self>, _: &Self::State) -> Option<RuleDiagnostic> {
         let this_super_expression = ctx.query();
 
-        Some(RuleDiagnostic::new(
-            rule_category!(),
-            this_super_expression.range(),
-            markup! {
-                "Unexpected "<Emphasis>{this_super_expression.text()}</Emphasis>"."
-            },
-        ))
+        let call_expression = this_super_expression
+            .syntax()
+            .ancestors()
+            .find_map(JsCallExpression::cast)?;
+
+        let class_declaration = this_super_expression
+            .syntax()
+            .ancestors()
+            .find_map(JsClassDeclaration::cast)?;
+
+        let class_name_str = class_declaration.id().ok()?.text();
+        let call_expression_str = call_expression.text();
+
+        let extended_class_name = class_declaration
+            .extends_clause()
+            .and_then(|with_extends_clause| with_extends_clause.super_class().ok())
+            .map(|node| node.text());
+
+        let mut recommendation_str = call_expression_str.replace("this", &class_name_str);
+
+        if let Some(extended_class_name_str) = extended_class_name {
+            recommendation_str = recommendation_str.replace("super", &extended_class_name_str);
+        }
+
+        Some(
+            RuleDiagnostic::new(
+                rule_category!(),
+                this_super_expression.range(),
+                markup! {
+                    "Unexpected "<Emphasis>{this_super_expression.text()}</Emphasis>"."
+                },
+            )
+            .note(markup! {
+                            "Function "<Emphasis>{call_expression_str}</Emphasis>" is static, so `"<Emphasis>{this_super_expression.text()}"."</Emphasis>"` refers to the class (the constructor) instance."
+            })
+            .note(markup! {
+                            "Instead of "<Emphasis>{call_expression_str}</Emphasis>" use "<Emphasis>{recommendation_str}</Emphasis>"."
+                        }),
+        )
     }
 }
