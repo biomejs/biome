0d602f64d7c2da9f05d7ea26fee56a1f234c9abb
Emanuele Stoppa <my.burning@gmail.com>
2023-10-31 12:11:48 +0000

    refactor(diagnostics): use `ParseDiagnostic` instead

--- crates/biome_js_parser/src/syntax/jsx/jsx_parse_errors.rs
+++ crates/biome_js_parser/src/syntax/jsx/jsx_parse_errors.rs
@@ -4,15 +4,15 @@ use biome_js_syntax::TextRange;
 use biome_parser::diagnostic::{expected_any, expected_node};
 
 pub(crate) fn jsx_expected_attribute(p: &JsParser, range: TextRange) -> ParseDiagnostic {
-    expected_node("JSX attribute", range).into_diagnostic(p)
+    expected_node("JSX attribute", range, p)
 }
 
 pub(crate) fn jsx_expected_attribute_value(p: &JsParser, range: TextRange) -> ParseDiagnostic {
-    expected_node("JSX attribute value", range).into_diagnostic(p)
+    expected_node("JSX attribute value", range, p)
 }
 
 pub(crate) fn jsx_expected_children(p: &JsParser, range: TextRange) -> ParseDiagnostic {
-    expected_any(&["JSX Expression", "Element", "text"], range).into_diagnostic(p)
+    expected_any(&["JSX Expression", "Element", "text"], range, p)
 }
 
 pub(crate) fn jsx_expected_closing_tag(
