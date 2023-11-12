28bf90c7320914cbc19731d41fd4bd3848538506
2-NOW <kj109888@gmail.com>
2023-11-13 00:56:00 +0900

    refactor: Remove unnecessary comments and code in use_await.rs

--- crates/biome_js_analyze/src/analyzers/nursery/use_await.rs
+++ crates/biome_js_analyze/src/analyzers/nursery/use_await.rs
@@ -69,25 +69,19 @@ impl Visitor for MissingAwaitVisitor {
     ) {
         match event {
             WalkEvent::Enter(node) => {
-                // When the visitor enters an `async` function node, push a new entry on the stack
                 if let Some(node) = AnyFunctionLike::cast_ref(node) {
                     if node.is_async() {
                         self.stack.push((node.range().start(), false));
                     }
                 }
 
-                // Replace JsYieldExpression with the appropriate check for `await`
                 if JsAwaitExpression::can_cast(node.kind()) {
-                    // When the visitor enters an `await` expression, set the
-                    // `has_await` flag for the top entry on the stack to `true`
                     if let Some((_, has_await)) = self.stack.last_mut() {
                         *has_await = true;
                     }
                 }
             }
             WalkEvent::Leave(node) => {
-                // When the visitor exits a function, if it matches the node of the top-most
-                // entry of the stack and the `has_await` flag is `false`, emit a query match
                 if let Some(node) = AnyFunctionLike::cast_ref(node) {
                     if let Some((function_start_range, has_await)) = self.stack.pop() {
                         if function_start_range == node.range().start()
@@ -107,12 +101,10 @@ pub(crate) struct MissingAwait(AnyFunctionLike);
 
 impl QueryMatch for MissingAwait {
     fn text_range(&self) -> TextRange {
-        // Return the range of the function that's missing an `await` statement
         self.0.range()
     }
 }
 
-// Implement the Queryable for MissingAwait
 impl Queryable for MissingAwait {
     type Input = Self;
     type Language = JsLanguage;
@@ -123,12 +115,10 @@ impl Queryable for MissingAwait {
         analyzer: &mut impl AddVisitor<Self::Language>,
         _: &<Self::Language as Language>::Root,
     ) {
-        // Register the MissingAwaitVisitor to run during the Syntax phase
         analyzer.add_visitor(Phases::Syntax, MissingAwaitVisitor::default);
     }
 
     fn unwrap_match(_: &ServiceBag, query: &Self::Input) -> Self::Output {
-        // Extract the function that's missing an `await` statement from the input query match
         query.0.clone()
     }
 }
@@ -141,7 +131,6 @@ impl Rule for UseAwait {
 
     fn run(ctx: &RuleContext<Self>) -> Self::Signals {
         let query = ctx.query();
-        // Don't emit a diagnostic for `async` functions with an empty body
         if query.statements()?.is_empty() {
             return None;
         }
