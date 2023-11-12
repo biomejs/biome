43d32a8f5d6fb8b729cf587c77b5ff4af1edd361
Jon Egeland <jon.egeland@discordapp.com>
2023-11-12 21:03:07 +0000

    parenthesize typeof type expressions in array and indexed access types

--- crates/biome_js_formatter/tests/specs/ts/parenthesis.ts
+++ crates/biome_js_formatter/tests/specs/ts/parenthesis.ts
@@ -1,3 +1,10 @@
 const a = (c && b) as boolean;
 const a = <any>(c && b) as boolean;
 const a = !(c && b) as boolean;
+
+// typeof operator precedence
+type T1 = typeof obj[number];
+type T2 = (typeof obj)[number];
+type T3 = keyof typeof obj[number];
+type T4 = keyof (typeof obj)[number];
+type T5 = (keyof typeof obj)['toString'];
