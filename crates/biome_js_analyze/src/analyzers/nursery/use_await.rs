4b549afa311e55aa80e11694325a890d95566a34
2-NOW <kj109888@gmail.com>
2023-11-13 10:11:08 +0900

    chore: update version to 1.3.0

--- crates/biome_js_analyze/src/analyzers/nursery/use_await.rs
+++ crates/biome_js_analyze/src/analyzers/nursery/use_await.rs
@@ -46,7 +46,7 @@ declare_rule! {
     /// async function noop() { }
     /// ```
     pub(crate) UseAwait {
-        version: "1.0.0",
+        version: "1.3.0",
         name: "useAwait",
         recommended: true,
     }
