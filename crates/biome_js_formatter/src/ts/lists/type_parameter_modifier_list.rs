ee07cbe6688b924fb848057a08f56a09b03c5d54
Victorien Elvinger <victorien@elvinger.fr>
2023-11-14 15:14:22 +0100

    challenge(prettier-compat): predictable order of type parameter modifiers

--- crates/biome_js_formatter/src/ts/lists/type_parameter_modifier_list.rs
+++ crates/biome_js_formatter/src/ts/lists/type_parameter_modifier_list.rs
@@ -1,5 +1,9 @@
 use crate::prelude::*;
-use biome_js_syntax::TsTypeParameterModifierList;
+use biome_js_syntax::{
+    AnyTsTypeParameterModifier, TsTypeParameterModifierList, TypeParameterModifiers,
+};
+use biome_rowan::AstNodeList;
+use smallvec::SmallVec;
 
 #[derive(Debug, Clone, Default)]
 pub(crate) struct FormatTsTypeParameterModifierList;
@@ -7,8 +11,18 @@ pub(crate) struct FormatTsTypeParameterModifierList;
 impl FormatRule<TsTypeParameterModifierList> for FormatTsTypeParameterModifierList {
     type Context = JsFormatContext;
     fn fmt(&self, node: &TsTypeParameterModifierList, f: &mut JsFormatter) -> FormatResult<()> {
+        let modifiers = sort_modifiers_by_precedence(node);
         f.join_with(&space())
-            .entries(node.iter().formatted())
+            .entries(modifiers.into_iter().formatted())
             .finish()
     }
+}
+
+/// This function consumes a list of modifiers and applies a predictable sorting.
+fn sort_modifiers_by_precedence(
+    list: &TsTypeParameterModifierList,
+) -> SmallVec<[AnyTsTypeParameterModifier; 3]> {
+    let mut result = list.iter().collect::<SmallVec<_>>();
+    result.sort_unstable_by_key(|node| TypeParameterModifiers::from(node));
+    result
 }
