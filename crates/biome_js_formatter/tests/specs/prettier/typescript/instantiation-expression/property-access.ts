8d1e8b3c60a4720ac0c3c30dc65ecd960d4e27a7
Victorien Elvinger <victorien@elvinger.fr>
2023-11-16 00:15:56 +0100

    challenge(formatter): add parens around instantiation expr when needed (#738)

--- crates/biome_js_formatter/tests/specs/prettier/typescript/instantiation-expression/property-access.ts
+++ crates/biome_js_formatter/tests/specs/prettier/typescript/instantiation-expression/property-access.ts
@@ -1,16 +1,14 @@
-// FIXME
-// TODO: parse issue
-// (Array<string>).name;
-// (fn1<string>).bind(obj);
-// (fn2<string, number>).bind(obj);
-// a[(Array<string>)];
-// a[(Array<string>).name];
-// (Array<string>).a;
-// (Array<string>)?.a;
-// (Array<string>)[a];
-// (Array<string>)?.[a];
-// (Array<string>)["a"];
-// (Array<string>)?.["a"];
-// (Array<string>)[`a`];
-// (Array<string>)?.[`a`];
-// (Array<string>)[(Array<string>)];
+(Array<string>).name;
+(fn1<string>).bind(obj);
+(fn2<string, number>).bind(obj);
+a[(Array<string>)];
+a[(Array<string>).name];
+(Array<string>).a;
+(Array<string>)?.a;
+(Array<string>)[a];
+(Array<string>)?.[a];
+(Array<string>)["a"];
+(Array<string>)?.["a"];
+(Array<string>)[`a`];
+(Array<string>)?.[`a`];
+(Array<string>)[(Array<string>)];
