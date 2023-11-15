8c88c98d7dfc5cc0661939b968e93c3ef39900ac
Emanuele Stoppa <my.burning@gmail.com>
2023-11-15 17:45:24 -0500

    one more test

--- crates/biome_cli/tests/cases/overrides_linter.rs
+++ crates/biome_cli/tests/cases/overrides_linter.rs
@@ -265,3 +265,65 @@ fn does_include_file_with_different_overrides() {
         result,
     ));
 }
+
+#[test]
+fn does_override_the_rules() {
+    let mut console = BufferConsole::default();
+    let mut fs = MemoryFileSystem::default();
+    let file_path = Path::new("biome.json");
+    fs.insert(
+        file_path.into(),
+        r#"{
+    "overrides": [
+        {
+            "include": [
+                "test.js"
+            ],
+            "linter": {
+                "rules": {
+                    "suspicious": {
+                        "noDebugger": "off"
+                    }
+                }
+            }
+        }
+    ]
+}
+
+"#
+        .as_bytes(),
+    );
+
+    let test = Path::new("test.js");
+    fs.insert(test.into(), DEBUGGER_BEFORE.as_bytes());
+
+    let test2 = Path::new("test2.js");
+    fs.insert(test2.into(), DEBUGGER_BEFORE.as_bytes());
+
+    let result = run_cli(
+        DynRef::Borrowed(&mut fs),
+        &mut console,
+        Args::from(
+            [
+                ("lint"),
+                ("--apply-unsafe"),
+                test.as_os_str().to_str().unwrap(),
+                test2.as_os_str().to_str().unwrap(),
+            ]
+            .as_slice(),
+        ),
+    );
+
+    assert!(result.is_ok(), "run_cli returned {result:?}");
+
+    assert_file_contents(&fs, test, DEBUGGER_BEFORE);
+    assert_file_contents(&fs, test2, DEBUGGER_AFTER);
+
+    assert_cli_snapshot(SnapshotPayload::new(
+        module_path!(),
+        "does_override_the_rules",
+        fs,
+        console,
+        result,
+    ));
+}
