e258b36205cf96aff68f1318d2ea98c372d20341
Vasu Singh <vasucp1207@gmail.com>
2023-11-18 04:15:21 +0530

    feat(lint): add unsafe code fix to `useAriaActivedescendantWithTabindex (#761)

--- crates/biome_js_analyze/src/aria_analyzers/nursery/use_aria_activedescendant_with_tabindex.rs
+++ crates/biome_js_analyze/src/aria_analyzers/nursery/use_aria_activedescendant_with_tabindex.rs
@@ -1,7 +1,18 @@
-use crate::aria_services::Aria;
-use biome_analyze::{context::RuleContext, declare_rule, Rule, RuleDiagnostic};
-use biome_js_syntax::jsx_ext::AnyJsxElement;
-use biome_rowan::AstNode;
+use crate::{aria_services::Aria, JsRuleAction};
+use biome_analyze::{
+    context::RuleContext, declare_rule, ActionCategory, FixKind, Rule, RuleDiagnostic,
+};
+use biome_console::markup;
+use biome_diagnostics::Applicability;
+use biome_js_factory::make::{
+    jsx_attribute, jsx_attribute_initializer_clause, jsx_attribute_list, jsx_ident, jsx_name,
+    jsx_string, jsx_string_literal, token,
+};
+use biome_js_syntax::{
+    jsx_ext::AnyJsxElement, AnyJsxAttribute, AnyJsxAttributeName, AnyJsxAttributeValue,
+    JsxAttributeList, T,
+};
+use biome_rowan::{AstNode, AstNodeList, BatchMutationExt, TriviaPieceKind};
 
 declare_rule! {
     /// Enforce that `tabIndex` is assigned to non-interactive HTML elements with `aria-activedescendant`.
@@ -43,6 +54,7 @@ declare_rule! {
         version: "1.3.0",
         name: "useAriaActivedescendantWithTabindex",
         recommended: false,
+        fix_kind: FixKind::Unsafe,
     }
 }
 
@@ -85,5 +97,37 @@ impl Rule for UseAriaActivedescendantWithTabindex {
                 "Add the tabIndex attribute to the element with a value greater than or equal to -1."
             ),
         )
+    }
+
+    fn action(ctx: &RuleContext<Self>, _: &Self::State) -> Option<JsRuleAction> {
+        let node = ctx.query();
+        let mut mutation = ctx.root().begin();
+        let descendant_attribute = node.find_attribute_by_name("aria-activedescendant")?;
+
+        let old_attribute_list = descendant_attribute
+            .syntax()
+            .ancestors()
+            .find_map(JsxAttributeList::cast)?;
+
+        let new_attribute = jsx_attribute(AnyJsxAttributeName::JsxName(jsx_name(
+            jsx_ident("tabIndex").with_leading_trivia([(TriviaPieceKind::Whitespace, " ")]),
+        )))
+        .with_initializer(jsx_attribute_initializer_clause(
+            token(T![=]),
+            AnyJsxAttributeValue::JsxString(jsx_string(jsx_string_literal("0"))),
+        ))
+        .build();
+
+        let mut new_attribute_list: Vec<_> = old_attribute_list.iter().collect();
+        new_attribute_list.push(AnyJsxAttribute::JsxAttribute(new_attribute));
+
+        mutation.replace_node(old_attribute_list, jsx_attribute_list(new_attribute_list));
+
+        Some(JsRuleAction {
+            category: ActionCategory::QuickFix,
+            applicability: Applicability::MaybeIncorrect,
+            message: markup! { "Add the tabIndex attribute." }.to_owned(),
+            mutation,
+        })
     }
 }
