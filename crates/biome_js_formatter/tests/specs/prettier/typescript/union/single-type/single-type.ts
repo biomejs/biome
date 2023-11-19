a7b978dbcefeaa9dc0da27a6ed85ba8eea8c84f0
Yash Singh <saiansh2525@gmail.com>
2023-11-18 19:19:16 -0800

    fix: typescript/union/inlining for prettier challenge

--- crates/biome_js_formatter/tests/specs/prettier/typescript/union/single-type/single-type.ts
+++ crates/biome_js_formatter/tests/specs/prettier/typescript/union/single-type/single-type.ts
@@ -1,8 +1,6 @@
-// FIXME
-// TODO: reformat issue
-// type A1 =
-//   /* 1 */ | /* 2 */ (
-//     /* 3 */ | /* 4 */ {
-//         key: string;
-//       }
-//   );
+type A1 =
+  /* 1 */ | /* 2 */ (
+    /* 3 */ | /* 4 */ {
+        key: string;
+      }
+  );
