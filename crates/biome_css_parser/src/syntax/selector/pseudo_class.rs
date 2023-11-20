660af7a3cf97a813505ad7f3bafe1ffd6ab77622
Denis Bezrukov <6227442+denbezrukov@users.noreply.github.com>
2023-11-20 12:42:15 +0200

    feat(css_parser): CSS Parser pseudo element selector #268 (#763)

--- crates/biome_css_parser/src/syntax/selector/pseudo_class.rs
+++ crates/biome_css_parser/src/syntax/selector/pseudo_class.rs
@@ -1,12 +1,17 @@
+use crate::lexer::CssLexContext;
 use crate::parser::CssParser;
 use crate::syntax::parse_error::{
-    expect_any_pseudo_class, expect_any_selector, expected_identifier,
+    expect_any_pseudo_class, expect_any_selector, expected_identifier, expected_number,
+    expected_pseudo_class_nth,
 };
 use crate::syntax::selector::{
     is_at_compound_selector, parse_compound_selector, parse_selector,
     parse_selector_function_close_token, parse_selector_identifier, CssSelectorList,
 };
-use crate::syntax::{is_at_identifier, parse_css_string, parse_regular_identifier};
+use crate::syntax::{
+    is_at_identifier, parse_css_string, parse_number, parse_regular_identifier,
+    parse_regular_number,
+};
 use biome_css_syntax::CssSyntaxKind::*;
 use biome_css_syntax::{CssSyntaxKind, T};
 use biome_parser::parse_lists::ParseSeparatedList;
@@ -49,6 +54,8 @@ fn parse_pseudo_class(p: &mut CssParser) -> ParsedSyntax {
         parse_pseudo_class_function_relative_selector_list(p)
     } else if is_at_pseudo_class_function_value_list(p) {
         parse_pseudo_class_function_value_list(p)
+    } else if is_at_pseudo_class_function_nth(p) {
+        parse_pseudo_class_function_nth(p)
     } else {
         parse_pseudo_class_identifier(p)
     }
@@ -360,6 +367,128 @@ fn parse_pseudo_value(p: &mut CssParser) -> ParsedSyntax {
     } else {
         parse_regular_identifier(p)
     }
+}
+
+const PSEUDO_CLASS_FUNCTION_NTH_SET: TokenSet<CssSyntaxKind> = token_set![
+    NTHCHILD_KW,
+    NTHLASTCHILD_KW,
+    NTHOFTYPE_KW,
+    NTHLASTOFTYPE_KW,
+    NTHCOL_KW,
+    NTHLASTCOL_KW
+];
+
+#[inline]
+fn is_at_pseudo_class_function_nth(p: &mut CssParser) -> bool {
+    p.at_ts(PSEUDO_CLASS_FUNCTION_NTH_SET) && p.nth_at(1, T!['('])
+}
+
+#[inline]
+fn parse_pseudo_class_function_nth(p: &mut CssParser) -> ParsedSyntax {
+    if !is_at_pseudo_class_function_nth(p) {
+        return Absent;
+    }
+
+    let m = p.start();
+
+    parse_regular_identifier(p).or_add_diagnostic(p, expected_identifier);
+
+    p.bump_with_context(T!['('], CssLexContext::PseudoNthSelector);
+    parse_pseudo_class_nth_selector(p).or_add_diagnostic(p, expect_any_selector);
+    parse_selector_function_close_token(p);
+
+    Present(m.complete(p, CSS_PSEUDO_CLASS_FUNCTION_NTH))
+}
+
+const PSEUDO_CLASS_FUNCTION_NTH_CLASS_SET: TokenSet<CssSyntaxKind> =
+    token_set![T![odd], T![even], T![n], CSS_NUMBER_LITERAL, T![+], T![-],];
+#[inline]
+fn is_at_pseudo_class_nth_selector(p: &mut CssParser) -> bool {
+    p.at_ts(PSEUDO_CLASS_FUNCTION_NTH_CLASS_SET)
+}
+
+#[inline]
+fn parse_pseudo_class_nth_selector(p: &mut CssParser) -> ParsedSyntax {
+    if !is_at_pseudo_class_nth_selector(p) {
+        return Absent;
+    }
+
+    let m = p.start();
+
+    parse_pseudo_class_nth(p).or_add_diagnostic(p, expected_pseudo_class_nth);
+    parse_pseudo_class_of_nth_selector(p).ok();
+
+    Present(m.complete(p, CSS_PSEUDO_CLASS_NTH_SELECTOR))
+}
+
+#[inline]
+fn parse_pseudo_class_nth(p: &mut CssParser) -> ParsedSyntax {
+    if !is_at_pseudo_class_nth_selector(p) {
+        return Absent;
+    }
+
+    let m = p.start();
+
+    let kind = if matches!(p.cur(), T![odd] | T![even]) {
+        p.bump_any();
+        CSS_PSEUDO_CLASS_NTH_IDENTIFIER
+    } else {
+        // +123
+        // +n + 3
+        // -n + 3
+        //  n + 3
+        // -2n + 1
+        // +2n- 1
+        // 123
+        // 2n + 4
+        // 2n- 4
+        if matches!(p.cur(), T![+] | T![-]) {
+            p.bump_with_context(p.cur(), CssLexContext::PseudoNthSelector);
+        }
+
+        parse_number(p, CssLexContext::PseudoNthSelector).ok();
+
+        if p.eat_with_context(T![n], CssLexContext::PseudoNthSelector) {
+            parse_nth_offset(p).ok();
+
+            CSS_PSEUDO_CLASS_NTH
+        } else {
+            CSS_PSEUDO_CLASS_NTH_NUMBER
+        }
+    };
+
+    Present(m.complete(p, kind))
+}
+
+#[inline]
+fn parse_nth_offset(p: &mut CssParser) -> ParsedSyntax {
+    if !matches!(p.cur(), T![+] | T![-]) {
+        return Absent;
+    }
+
+    let m = p.start();
+
+    p.bump_any();
+    parse_regular_number(p).or_add_diagnostic(p, expected_number);
+
+    Present(m.complete(p, CSS_NTH_OFFSET))
+}
+
+#[inline]
+fn parse_pseudo_class_of_nth_selector(p: &mut CssParser) -> ParsedSyntax {
+    if !p.at(OF_KW) {
+        return Absent;
+    }
+
+    let m = p.start();
+
+    p.bump(OF_KW);
+
+    CssSelectorList::default()
+        .with_end_kind(T![')'])
+        .parse_list(p);
+
+    Present(m.complete(p, CSS_PSEUDO_CLASS_OF_NTH_SELECTOR))
 }
 
 #[inline]
