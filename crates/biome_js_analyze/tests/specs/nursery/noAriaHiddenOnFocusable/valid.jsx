cc60a55e14d2388ecf3590b96e8cab9d74e33c74
Vasu Singh <vasucp1207@gmail.com>
2023-11-24 01:22:26 +0530

    feat: `noAriaHiddenOnFocusable` rule (#822)

---
+++ crates/biome_js_analyze/tests/specs/nursery/noAriaHiddenOnFocusable/valid.jsx
@@ -0,0 +1,9 @@
+<>
+  <div aria-hidden="true" />
+  <img aria-hidden="true" />
+  <a aria-hidden="false" href="#" />
+  <button aria-hidden="true" tabIndex="-1" />
+  <a href="/" />
+  <div aria-hidden="true"><a href="#"></a></div>
+  <button />
+</>
