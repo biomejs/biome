36a94dccd87f9f4b7e80c043a5e472a488542c93
Yan Thomas <61414485+Yan-Thomas@users.noreply.github.com>
2023-11-28 09:26:16 -0300

    feat(website): Move blog to CC and fix RSS

--- website/src/content/config.ts
+++ website/src/content/config.ts
@@ -1,7 +1,41 @@
 import { docsSchema } from "@astrojs/starlight/schema";
 // src/content/config.ts
-import { defineCollection } from "astro:content";
+import { z, defineCollection } from "astro:content";
+
+const blogSchema = z.object({
+	title: z.string(),
+	subtitle: z.string().optional(),
+	pubDate: z
+		.string()
+		.or(z.date())
+		.transform((val) => new Date(val)),
+	summary: z.string(),
+	description: z.string(),
+	authors: z.array(z.string()),
+	coverImage: z
+		.object({
+			src: z.string(),
+			caption: z.string().optional(),
+			alt: z.string(),
+		})
+		.optional(),
+	socialImage: z.string(),
+});
+
+const authorsSchema = z.object({
+	name: z.string(),
+	avatar: z.string(),
+	url: z.string().optional(),
+});
 
 export const collections = {
 	docs: defineCollection({ schema: docsSchema() }),
+	blog: defineCollection({
+		type: "content",
+		schema: blogSchema,
+	}),
+	authors: defineCollection({
+		type: "data",
+		schema: authorsSchema,
+	}),
 };
