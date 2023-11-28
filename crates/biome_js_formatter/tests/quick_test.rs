fbe62cece9f57f2f6a6845401f461e32d5ae99d2
Jon <jonegeland@gmail.com>
2023-11-28 13:40:29 -0800

    fix(js_formatter): Avoid double-printing the first leading comments in arrow chains (#951)

--- crates/biome_js_formatter/tests/quick_test.rs
+++ crates/biome_js_formatter/tests/quick_test.rs
@@ -14,30 +14,7 @@ mod language {
 // use this test check if your snippet prints as you wish, without using a snapshot
 fn quick_test() {
     let src = r#"
-    /**
- * Curried function that ends with a BEM CSS Selector
- *
- * @param {String} block - the BEM Block you'd like to select.
- * @returns {Function}
- */
-export const bem =
-	(block) =>
-	/**
-	 * @param {String} [element] - the BEM Element within that block; if undefined, selects the block itself.
-	 * @returns {Function}
-	 */
-	(element) =>
-	/**
-	 * @param {?String} [modifier] - the BEM Modifier for the Block or Element; if undefined, selects the Block or Element unmodified.
-	 * @returns {String}
-	 */
-	(modifier) =>
-		[
-			".",
-			css(block),
-			element ? `__${css(element)}` : "",
-			modifier ? `--${css(modifier)}` : "",
-		].join("");
+    foo( /*hi*/(a) => (b) => {})
     "#;
     let syntax = JsFileSource::tsx();
     let tree = parse(
