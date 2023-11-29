198196cddeaa2923fbbebabdf442061f3db6f737
Keita Nonaka <iKonnyaku40@gmail.com>
2023-11-29 01:20:31 -0800

    doc(ja): translate 404 page into Japanese

--- website/src/content/config.ts
+++ website/src/content/config.ts
@@ -1,7 +1,8 @@
-import { docsSchema } from "@astrojs/starlight/schema";
+import { docsSchema, i18nSchema } from "@astrojs/starlight/schema";
 // src/content/config.ts
 import { defineCollection } from "astro:content";
 
 export const collections = {
 	docs: defineCollection({ schema: docsSchema() }),
+	i18n: defineCollection({ type: "data", schema: i18nSchema() }),
 };
