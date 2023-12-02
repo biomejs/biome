6d1180be5c79703f50fb94685c55678119ed434e
vasu <vasucp1207@gmail.com>
2023-12-02 07:44:15 +0530

    home revamp

---
+++ website/src/playground/components/Progress.tsx
@@ -0,0 +1,47 @@
+import { useState, useEffect } from "react";
+import "./progress.css";
+
+const ProgressBar = ({
+	duration,
+	label,
+}: {
+	duration: number;
+	label: string;
+}) => {
+	const [progress, setProgress] = useState(0);
+
+	useEffect(() => {
+		const startTime = Date.now();
+
+		const updateProgress = () => {
+			const elapsed = Date.now() - startTime;
+			const newProgress = elapsed / 1000;
+			console.log(newProgress, "p");
+
+			if (Math.floor(newProgress) >= duration) {
+				clearInterval(interval);
+			} else {
+				setProgress(newProgress);
+			}
+		};
+
+		const interval = setInterval(updateProgress, 100);
+
+		return () => {
+			clearInterval(interval);
+		};
+	}, [duration]);
+
+	return (
+		<div className="prog-cont">
+			<span className="label">{label}</span>
+			<div className="progress-bar-container">
+				<div className="bar" style={{ width: `calc(3 * ${progress}%)` }}>
+					<span className="time">{progress.toFixed(2)}s</span>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default ProgressBar;
