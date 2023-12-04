c34008facc7aa96e30915b3a9617b323ea722131
Jon Egeland <jon.egeland@discordapp.com>
2023-12-04 05:57:21 +0000

    fix(js_formatter): don't hug blocks in case clauses with multiple statements

--- crates/biome_js_formatter/tests/specs/js/module/statement/switch.js
+++ crates/biome_js_formatter/tests/specs/js/module/statement/switch.js
@@ -17,3 +17,17 @@ switch (key) {
 switch ("test") {
   case "test": {}
 }
+
+switch (key) {
+	case blockBody: {
+		const a = 1;
+		break;
+	}
+
+	// The block is not the only statement in the case body,
+	// so it doesn't hug the same line as the case here.
+	case separateBlockBody: {
+		const a = 1;
+	}
+	break;
+}
