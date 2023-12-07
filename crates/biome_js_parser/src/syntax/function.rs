199eb36f59cd1ff33404f79a654950cecdf91d6b
Karl Persson <kalle.persson@grafana.com>
2023-12-07 23:04:24 +0100

    revert unintended change

--- crates/biome_js_parser/src/syntax/function.rs
+++ crates/biome_js_parser/src/syntax/function.rs
@@ -1252,7 +1252,6 @@ pub(super) fn parse_parameter_list(
     if !p.at(T!['(']) {
         return Absent;
     }
-
     let m = p.start();
     parse_parameters_list(
         p,
@@ -1327,6 +1326,7 @@ pub(super) fn parse_parameter_list(
                     })
                     .into()
             };
+
             parse_any_parameter(
                 p,
                 decorator_list,
