88cb1769b5ef65820b9345af4d2741beaad013ce
Karl Persson <kalle.persson@grafana.com>
2023-12-12 10:30:11 +0100

    Use app console to output documentation

--- crates/biome_cli/src/commands/explain.rs
+++ crates/biome_cli/src/commands/explain.rs
@@ -1,30 +1,21 @@
 use biome_analyze::RuleMetadata;
+use biome_console::{markup, ConsoleExt};
 use biome_service::explanations::Explanations;
 
 use crate::{CliDiagnostic, CliSession};
 
 #[allow(dead_code)]
-fn print_rule(metadata: &RuleMetadata) {
-    println!("# {}", metadata.name);
-    println!();
-
-    print!("This rule is ");
-    if metadata.recommended {
-        println!("recommended.");
-    } else {
-        println!("not recommended.");
-    }
-    println!();
-
-    if let Some(fix) = &metadata.fix_kind {
-        println!("Fix is {}.", fix.to_string());
-    } else {
-        println!("No fix available.")
-    }
-    println!();
-
-    println!("# Description");
-    println!("{}", metadata.docs);
+fn print_rule(session: CliSession, metadata: &RuleMetadata) {
+    session.app.console.log(markup! {
+        "# "{metadata.name}
+        "\n\n"
+        {if let Some(fix) = &metadata.fix_kind {format!("Fix is {}.", fix.to_string())} else {"No fix available.".to_string()}}
+        "\n\n"
+        "This rule is "{if metadata.recommended {"recommended."} else {"not recommended."}}
+        "\n\n"
+        "# Description\n"
+        {metadata.docs}
+    });
 }
 
 #[allow(dead_code)]
@@ -33,13 +24,13 @@ pub(crate) struct ExplainCommandPayload {
 }
 
 pub(crate) fn explain(
-    _session: CliSession,
+    session: CliSession,
     payload: ExplainCommandPayload,
 ) -> Result<(), CliDiagnostic> {
     let ExplainCommandPayload { explanations } = payload;
 
     if let Some(metadata) = explanations.rule {
-        print_rule(&metadata);
+        print_rule(session, &metadata);
     }
 
     Ok(())
