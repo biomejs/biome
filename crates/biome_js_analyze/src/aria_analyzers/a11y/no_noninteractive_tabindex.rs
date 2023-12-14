82c7ac43ddbbe0def8569525f3eb500f2b171441
vasu <vasucp1207@gmail.com>
2023-12-14 08:09:26 +0530

    fix: noNoninteractiveTabindex code action

--- crates/biome_js_analyze/src/aria_analyzers/a11y/no_noninteractive_tabindex.rs
+++ crates/biome_js_analyze/src/aria_analyzers/a11y/no_noninteractive_tabindex.rs
@@ -1,5 +1,7 @@
 use crate::{aria_services::Aria, JsRuleAction};
-use biome_analyze::{context::RuleContext, declare_rule, Rule, RuleDiagnostic, ActionCategory, FixKind};
+use biome_analyze::{
+    context::RuleContext, declare_rule, ActionCategory, FixKind, Rule, RuleDiagnostic,
+};
 use biome_aria::AriaRoles;
 use biome_console::markup;
 use biome_diagnostics::Applicability;
@@ -164,7 +166,8 @@ impl Rule for NoNoninteractiveTabindex {
         Some(JsRuleAction {
             category: ActionCategory::QuickFix,
             applicability: Applicability::MaybeIncorrect,
-            message: markup! { "Remove the "<Emphasis>"tabIndex"</Emphasis>" attribute." }.to_owned(),
+            message: markup! { "Remove the "<Emphasis>"tabIndex"</Emphasis>" attribute." }
+                .to_owned(),
             mutation,
         })
     }
