105e46e70a80ff436c48dcfcc65e1917b6d1436d
Imamuzzaki Abu Salam <imamuzzaki@gmail.com>
2023-12-14 16:55:03 +0700

    test(useShorthandFunctionType): add more test cases (#1185)

--- crates/biome_js_analyze/tests/specs/nursery/useShorthandFunctionType/valid.ts
+++ crates/biome_js_analyze/tests/specs/nursery/useShorthandFunctionType/valid.ts
@@ -1,30 +1,54 @@
 type Example = () => string;
 
 function foo(example: () => number): number {
- return bar();
+	return bar();
 }
 
 // returns the function itself, not the `this` argument.
 type ReturnsSelf = (arg: string) => ReturnsSelf;
 
 interface Foo {
- bar: string;
+	bar: string;
 }
 
 interface Bar extends Foo {
- (): void;
+	(): void;
 }
 
 // multiple call signatures (overloads) is allowed:
 interface Overloaded {
- (data: string): number;
- (id: number): string;
+	(data: string): number;
+	(id: number): string;
 }
 
 // this is equivelent to Overloaded interface.
 type Intersection = ((data: string) => number) & ((id: number) => string);
 
 interface ReturnsSelf {
- // returns the function itself, not the `this` argument.
- (arg: string): this;
+	// returns the function itself, not the `this` argument.
+	(arg: string): this;
 }
+
+// Simple shorthand function type with no arguments
+type NoArgFunction = () => void;
+
+// Function type with multiple arguments
+type MultiArgFunction = (arg1: string, arg2: number) => boolean;
+
+// Function type with rest parameters
+type RestParamsFunction = (...args: number[]) => number;
+
+// Nested function types
+type NestedFunction = () => () => number;
+
+// Function type with tuple types as parameters
+type TupleFunction = ([a, b]: [string, number]) => boolean;
+
+// Function type in a type union
+type UnionFunction = (() => string) | (() => number);
+
+// Using shorthand function type in a generic type
+type GenericFunction<T> = (arg: T) => T;
+
+// Function type with optional parameter
+type OptionalParamFunction = (arg?: string) => void;
