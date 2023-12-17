420b767da53d02726221dfa3d2618c5bfdda7033
bestmike007 <i@bestmike007.com>
2023-12-17 11:36:18 -0600

    chore: use cache dir instead of data dir
    Signed-off-by: bestmike007 <i@bestmike007.com>

--- crates/biome_fs/src/dir.rs
+++ crates/biome_fs/src/dir.rs
@@ -1,15 +1,15 @@
 use directories::ProjectDirs;
 use std::{env, fs, path::PathBuf};
 
-pub fn ensure_data_dir() -> PathBuf {
+pub fn ensure_cache_dir() -> PathBuf {
     let project_data_dir = if let Some(proj_dirs) = ProjectDirs::from("dev", "biomejs", "biome") {
-        // Linux: /home/alice/.local/share/biome
-        // Win: C:\Users\Alice\AppData\Roaming\biomejs\biome\Data
-        // Mac: /Users/Alice/Library/Application Support/dev.biomejs.biome
-        proj_dirs.data_local_dir().to_path_buf()
+        // Linux: /home/alice/.cache/biome
+        // Win: C:\Users\Alice\AppData\Local\biomejs\biome\cache
+        // Mac: /Users/Alice/Library/Caches/dev.biomejs.biome
+        proj_dirs.cache_dir().to_path_buf()
     } else {
         env::temp_dir()
     };
-    fs::create_dir_all(&project_data_dir).expect("failed to create biome data directory");
+    fs::create_dir_all(&project_data_dir).expect("failed to create biome cache directory");
     project_data_dir
 }
