1df4b94376c3241bfae4adf306ffdee6c26f9bb2
bestmike007 <i@bestmike007.com>
2023-12-18 01:41:39 -0600

    chore: skip creating dir if fallback
    Signed-off-by: bestmike007 <i@bestmike007.com>

--- crates/biome_fs/src/dir.rs
+++ crates/biome_fs/src/dir.rs
@@ -2,14 +2,14 @@ use directories::ProjectDirs;
 use std::{env, fs, path::PathBuf};
 
 pub fn ensure_cache_dir() -> PathBuf {
-    let project_data_dir = if let Some(proj_dirs) = ProjectDirs::from("dev", "biomejs", "biome") {
+    if let Some(proj_dirs) = ProjectDirs::from("dev", "biomejs", "biome") {
         // Linux: /home/alice/.cache/biome
         // Win: C:\Users\Alice\AppData\Local\biomejs\biome\cache
         // Mac: /Users/Alice/Library/Caches/dev.biomejs.biome
-        proj_dirs.cache_dir().to_path_buf()
+        let cache_dir = proj_dirs.cache_dir().to_path_buf();
+        fs::create_dir_all(&cache_dir).expect("failed to create biome cache directory");
+        cache_dir
     } else {
         env::temp_dir()
-    };
-    fs::create_dir_all(&project_data_dir).expect("failed to create biome cache directory");
-    project_data_dir
+    }
 }
