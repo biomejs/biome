6832256865e51b4358d5b49b1c98a58dd15a66aa
Victorien Elvinger <victorien@elvinger.fr>
2023-12-18 13:28:18 +0100

    feat(parse/noTypeOnlyImportAttributes): report type-only imports/exports with attributes (#1239)

--- packages/@biomejs/backend-jsonrpc/src/workspace.ts
+++ packages/@biomejs/backend-jsonrpc/src/workspace.ts
@@ -1529,6 +1529,7 @@ export type Category =
 	| "lint/nursery/noEmptyBlockStatements"
 	| "lint/nursery/noImplicitAnyLet"
 	| "lint/nursery/noMisleadingCharacterClass"
+	| "lint/nursery/noTypeOnlyImportAttributes"
 	| "lint/nursery/noUnusedImports"
 	| "lint/nursery/noUnusedPrivateClassMembers"
 	| "lint/nursery/noUselessLoneBlockStatements"
