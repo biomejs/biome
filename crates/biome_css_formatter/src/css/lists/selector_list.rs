b50b85a90a879eec2e30840c54e1cc349f239553
Jon Egeland <jon.egeland@discordapp.com>
2023-12-19 20:55:57 +0000

    add namespace support and tests

--- crates/biome_css_formatter/src/css/lists/selector_list.rs
+++ crates/biome_css_formatter/src/css/lists/selector_list.rs
@@ -5,8 +5,20 @@ pub(crate) struct FormatCssSelectorList;
 impl FormatRule<CssSelectorList> for FormatCssSelectorList {
     type Context = CssFormatContext;
     fn fmt(&self, node: &CssSelectorList, f: &mut CssFormatter) -> FormatResult<()> {
-        f.join_with(&hard_line_break())
-            .entries(node.format_separated(","))
-            .finish()
+        let mut joiner = f.join_nodes_with_hardline();
+
+        for (rule, formatted) in node.elements().zip(node.format_separated(",")) {
+            // Each selector gets `indent` added in case it breaks over multiple
+            // lines. The break is added here rather than in each selector both
+            // for simplicity and to avoid recursively adding indents when
+            // selectors are nested within other rules.
+            //
+            // For example, a selector like `div span a` is structured like
+            // `[div, [span, [a]]]`, so `a` would end up double-indented if it
+            // was handled by the selector rather than here.
+            joiner.entry(rule.node()?.syntax(), &indent(&formatted));
+        }
+
+        joiner.finish()
     }
 }
