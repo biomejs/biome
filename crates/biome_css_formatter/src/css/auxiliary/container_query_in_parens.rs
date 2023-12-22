d70092e9ffcc7551ac3bff4359081613d1a016dd
Jon Egeland <jon.egeland@discordapp.com>
2023-12-22 06:11:38 +0000

    container queries

--- crates/biome_css_formatter/src/css/auxiliary/container_query_in_parens.rs
+++ crates/biome_css_formatter/src/css/auxiliary/container_query_in_parens.rs
@@ -1,6 +1,7 @@
 use crate::prelude::*;
-use biome_css_syntax::CssContainerQueryInParens;
-use biome_rowan::AstNode;
+use biome_css_syntax::{CssContainerQueryInParens, CssContainerQueryInParensFields};
+use biome_formatter::{format_args, write};
+
 #[derive(Debug, Clone, Default)]
 pub(crate) struct FormatCssContainerQueryInParens;
 impl FormatNodeRule<CssContainerQueryInParens> for FormatCssContainerQueryInParens {
@@ -9,6 +10,19 @@ impl FormatNodeRule<CssContainerQueryInParens> for FormatCssContainerQueryInPare
         node: &CssContainerQueryInParens,
         f: &mut CssFormatter,
     ) -> FormatResult<()> {
-        format_verbatim_node(node.syntax()).fmt(f)
+        let CssContainerQueryInParensFields {
+            l_paren_token,
+            query,
+            r_paren_token,
+        } = node.as_fields();
+
+        write!(
+            f,
+            [group(&format_args![
+                l_paren_token.format(),
+                soft_block_indent(&query.format()),
+                r_paren_token.format()
+            ])]
+        )
     }
 }
