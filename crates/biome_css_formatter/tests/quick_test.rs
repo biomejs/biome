9537ea77c62d940748a5fccb7becf57a6f25e95c
suxin2017 <1107178482@qq.com>
2023-12-22 13:00:54 +0800

    update snapshot

--- crates/biome_css_formatter/tests/quick_test.rs
+++ crates/biome_css_formatter/tests/quick_test.rs
@@ -14,7 +14,16 @@ mod language {
 fn quick_test() {
     let src = r#"
     div {
-        a: 4px;
+        color: rgba(255, 255, 255, 1);
+        color:   rgba   (
+            0,
+            1,
+            255,
+            1
+        );
+        color: arbitrary(really long list, of complex parameter values, each one on its own line);
+        color: more-arbitrary(just, has, lots, of, individual, parameters, breaking, over, lines);
+        color: arbitrary(one really long parameter value that itself will break over multiple lines and fill together);
     }
 "#;
     let parse = parse_css(src, CssParserOptions::default());
