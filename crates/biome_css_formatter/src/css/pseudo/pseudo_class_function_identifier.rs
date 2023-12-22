bf1c3eb5c579b2e7de56d232faa872d59d510323
Jon <jonegeland@gmail.com>
2023-12-22 10:31:14 -0800

    feat(css_formatter): support for pseudo selectors (#1291)

--- crates/biome_css_formatter/src/css/pseudo/pseudo_class_function_identifier.rs
+++ crates/biome_css_formatter/src/css/pseudo/pseudo_class_function_identifier.rs
@@ -1,6 +1,7 @@
 use crate::prelude::*;
-use biome_css_syntax::CssPseudoClassFunctionIdentifier;
-use biome_rowan::AstNode;
+use biome_css_syntax::{CssPseudoClassFunctionIdentifier, CssPseudoClassFunctionIdentifierFields};
+use biome_formatter::{format_args, write};
+
 #[derive(Debug, Clone, Default)]
 pub(crate) struct FormatCssPseudoClassFunctionIdentifier;
 impl FormatNodeRule<CssPseudoClassFunctionIdentifier> for FormatCssPseudoClassFunctionIdentifier {
@@ -9,6 +10,23 @@ impl FormatNodeRule<CssPseudoClassFunctionIdentifier> for FormatCssPseudoClassFu
         node: &CssPseudoClassFunctionIdentifier,
         f: &mut CssFormatter,
     ) -> FormatResult<()> {
-        format_verbatim_node(node.syntax()).fmt(f)
+        let CssPseudoClassFunctionIdentifierFields {
+            name_token,
+            l_paren_token,
+            ident,
+            r_paren_token,
+        } = node.as_fields();
+
+        write!(
+            f,
+            [
+                name_token.format(),
+                group(&format_args![
+                    l_paren_token.format(),
+                    soft_block_indent(&ident.format()),
+                    r_paren_token.format()
+                ])
+            ]
+        )
     }
 }
