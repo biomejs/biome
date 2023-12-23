8cf04878e326e066fe9d0612b7f7e9b4b691440c
Jon <jonegeland@gmail.com>
2023-12-23 06:36:38 -0800

    fix(css_formatter): don't preserve empty lines for selector lists (#1309)

--- crates/biome_css_formatter/src/css/lists/parameter_list.rs
+++ crates/biome_css_formatter/src/css/lists/parameter_list.rs
@@ -6,10 +6,14 @@ pub(crate) struct FormatCssParameterList;
 impl FormatRule<CssParameterList> for FormatCssParameterList {
     type Context = CssFormatContext;
     fn fmt(&self, node: &CssParameterList, f: &mut CssFormatter) -> FormatResult<()> {
-        let mut joiner = f.join_nodes_with_soft_line();
+        // Using `join_with` instead of `join_nodes_with_soft_line` to avoid
+        // preserving empty lines from the input source. See the comment in
+        // [FormatCssSelectorList] for more information.
+        let separator = soft_line_break_or_space();
+        let mut joiner = f.join_with(&separator);
 
-        for (rule, formatted) in node.elements().zip(node.format_separated(",")) {
-            joiner.entry(rule.node()?.syntax(), &formatted);
+        for formatted in node.format_separated(",") {
+            joiner.entry(&formatted);
         }
 
         joiner.finish()
--- crates/biome_css_formatter/src/css/lists/pseudo_value_list.rs
+++ crates/biome_css_formatter/src/css/lists/pseudo_value_list.rs
@@ -6,10 +6,14 @@ pub(crate) struct FormatCssPseudoValueList;
 impl FormatRule<CssPseudoValueList> for FormatCssPseudoValueList {
     type Context = CssFormatContext;
     fn fmt(&self, node: &CssPseudoValueList, f: &mut CssFormatter) -> FormatResult<()> {
-        let mut joiner = f.join_nodes_with_soft_line();
+        // Using `join_with` instead of `join_nodes_with_soft_line` to avoid
+        // preserving empty lines from the input source. See the comment in
+        // [FormatCssSelectorList] for more information.
+        let separator = soft_line_break_or_space();
+        let mut joiner = f.join_with(&separator);
 
-        for (rule, formatted) in node.elements().zip(node.format_separated(",")) {
-            joiner.entry(rule.node()?.syntax(), &formatted);
+        for formatted in node.format_separated(",") {
+            joiner.entry(&formatted);
         }
 
         joiner.finish()
