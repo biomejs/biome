6f94d0c7f3d7ba9c869e7d63da6699f36eea540b
togami2864 <togami2864@gmail.com>
2023-12-26 23:41:53 +0900

    feat: fmt empty params

--- crates/biome_js_formatter/src/ts/bindings/type_empty_parameters.rs
+++ crates/biome_js_formatter/src/ts/bindings/type_empty_parameters.rs
@@ -1,10 +1,21 @@
 use crate::prelude::*;
-use biome_js_syntax::TsTypeEmptyParameters;
-use biome_rowan::AstNode;
+use biome_formatter::{format_args, write};
+use biome_js_syntax::{TsTypeEmptyParameters, TsTypeEmptyParametersFields};
 #[derive(Debug, Clone, Default)]
 pub(crate) struct FormatTsTypeEmptyParameters;
 impl FormatNodeRule<TsTypeEmptyParameters> for FormatTsTypeEmptyParameters {
     fn fmt_fields(&self, node: &TsTypeEmptyParameters, f: &mut JsFormatter) -> FormatResult<()> {
-        format_verbatim_node(node.syntax()).fmt(f)
+        let TsTypeEmptyParametersFields {
+            l_angle_token,
+            r_angle_token,
+        } = node.as_fields();
+
+        write!(
+            f,
+            [group(&format_args![
+                l_angle_token.format(),
+                r_angle_token.format()
+            ])]
+        )
     }
 }
--- crates/biome_js_formatter/src/ts/declarations/type_alias_declaration.rs
+++ crates/biome_js_formatter/src/ts/declarations/type_alias_declaration.rs
@@ -9,12 +9,19 @@ pub struct FormatTsTypeAliasDeclaration;
 impl FormatNodeRule<TsTypeAliasDeclaration> for FormatTsTypeAliasDeclaration {
     fn fmt_fields(&self, node: &TsTypeAliasDeclaration, f: &mut JsFormatter) -> FormatResult<()> {
         let type_token = node.type_token()?;
+        let type_params = format_with(|f| {
+            if let Some(empty_type_params) = &node.ts_type_empty_parameters() {
+                write!(f, [empty_type_params.format()])?;
+            }
+            Ok(())
+        });
         let semicolon = node.semicolon_token();
         let assignment_like = format_with(|f| write!(f, [AnyJsAssignmentLike::from(node.clone())]));
         write!(
             f,
             [
                 type_token.format(),
+                type_params,
                 space(),
                 group(&assignment_like),
                 FormatStatementSemicolon::new(semicolon.as_ref())
