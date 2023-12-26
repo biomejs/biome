33b7a171b7a6d4978df74555b04eb8229ea987cc
Jon Egeland <jon.egeland@discordapp.com>
2023-12-26 22:02:38 +0000

    feat(css_formatter): formatting support for supports at-rules

--- crates/biome_css_formatter/src/css/auxiliary/supports_and_condition.rs
+++ crates/biome_css_formatter/src/css/auxiliary/supports_and_condition.rs
@@ -1,10 +1,26 @@
 use crate::prelude::*;
-use biome_css_syntax::CssSupportsAndCondition;
-use biome_rowan::AstNode;
+use biome_css_syntax::{CssSupportsAndCondition, CssSupportsAndConditionFields};
+use biome_formatter::write;
+
 #[derive(Debug, Clone, Default)]
 pub(crate) struct FormatCssSupportsAndCondition;
 impl FormatNodeRule<CssSupportsAndCondition> for FormatCssSupportsAndCondition {
     fn fmt_fields(&self, node: &CssSupportsAndCondition, f: &mut CssFormatter) -> FormatResult<()> {
-        format_verbatim_node(node.syntax()).fmt(f)
+        let CssSupportsAndConditionFields {
+            left,
+            and_token,
+            right,
+        } = node.as_fields();
+
+        write!(
+            f,
+            [
+                left.format(),
+                space(),
+                and_token.format(),
+                soft_line_break_or_space(),
+                right.format()
+            ]
+        )
     }
 }
--- crates/biome_css_formatter/src/css/selectors/supports_feature_selector.rs
+++ crates/biome_css_formatter/src/css/selectors/supports_feature_selector.rs
@@ -1,6 +1,7 @@
 use crate::prelude::*;
-use biome_css_syntax::CssSupportsFeatureSelector;
-use biome_rowan::AstNode;
+use biome_css_syntax::{CssSupportsFeatureSelector, CssSupportsFeatureSelectorFields};
+use biome_formatter::{format_args, write};
+
 #[derive(Debug, Clone, Default)]
 pub(crate) struct FormatCssSupportsFeatureSelector;
 impl FormatNodeRule<CssSupportsFeatureSelector> for FormatCssSupportsFeatureSelector {
@@ -9,6 +10,21 @@ impl FormatNodeRule<CssSupportsFeatureSelector> for FormatCssSupportsFeatureSele
         node: &CssSupportsFeatureSelector,
         f: &mut CssFormatter,
     ) -> FormatResult<()> {
-        format_verbatim_node(node.syntax()).fmt(f)
+        let CssSupportsFeatureSelectorFields {
+            selector_token,
+            l_paren_token,
+            selector,
+            r_paren_token,
+        } = node.as_fields();
+
+        write!(
+            f,
+            [group(&format_args![
+                selector_token.format(),
+                l_paren_token.format(),
+                soft_block_indent(&selector.format()),
+                r_paren_token.format()
+            ])]
+        )
     }
 }
