6c56ad5366cc29ca9bc5caf8e97e68d2a0f1415e
Emanuele Stoppa <my.burning@gmail.com>
2023-12-29 15:05:39 +0000

    ci: remove deprecated dependencies

--- xtask/bench/src/lib.rs
+++ xtask/bench/src/lib.rs
@@ -6,6 +6,22 @@ pub use crate::language::Parse;
 pub use crate::test_case::TestCase;
 use biome_formatter::Printed;
 
+#[cfg(all(target_os = "windows"))]
+#[global_allocator]
+static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
+
+#[cfg(all(
+    any(target_os = "macos", target_os = "linux"),
+    not(target_env = "musl"),
+))]
+#[global_allocator]
+static GLOBAL: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
+
+// Jemallocator does not work on aarch64 with musl, so we'll use the system allocator instead
+#[cfg(all(target_env = "musl", target_os = "linux", target_arch = "aarch64"))]
+#[global_allocator]
+static GLOBAL: std::alloc::System = std::alloc::System;
+
 pub fn run_format(format_node: &FormatNode) -> Printed {
     let formatted = format_node.format_node().unwrap();
     let printed = formatted.print();
