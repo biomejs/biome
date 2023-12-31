cbc5cd7b2daceed88e3d9293113cfaf0201c1bca
Emanuele Stoppa <my.burning@gmail.com>
2023-12-31 08:41:24 +0000

    ci: use codspeed criterion (#1387)

--- xtask/bench/benches/css_formatter.rs
+++ xtask/bench/benches/css_formatter.rs
@@ -1,7 +1,21 @@
-use criterion::{criterion_group, criterion_main, Criterion};
 use std::collections::HashMap;
 use xtask_bench::{bench_formatter_group, TestCase};
+use xtask_bench::{criterion_group, criterion_main, Criterion};
+#[cfg(target_os = "windows")]
+#[global_allocator]
+static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
 
+#[cfg(all(
+    any(target_os = "macos", target_os = "linux"),
+    not(target_env = "musl"),
+))]
+#[global_allocator]
+static GLOBAL: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
+
+// Jemallocator does not work on aarch64 with musl, so we'll use the system allocator instead
+#[cfg(all(target_env = "musl", target_os = "linux", target_arch = "aarch64"))]
+#[global_allocator]
+static GLOBAL: std::alloc::System = std::alloc::System;
 fn bench_css_formatter(criterion: &mut Criterion) {
     let mut all_suites = HashMap::new();
     all_suites.insert("css", include_str!("libs-css.txt"));
