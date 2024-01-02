ad20d09d460b09e1b32ca8a91ec1c09fd5068c22
Jon Egeland <jon.egeland@discordapp.com>
2024-01-02 18:58:56 +0000

    fix moved formatter nodes

--- crates/biome_css_formatter/src/css/properties/generic_property.rs
+++ crates/biome_css_formatter/src/css/properties/generic_property.rs
@@ -1,10 +1,20 @@
 use crate::prelude::*;
-use biome_css_syntax::CssGenericProperty;
-use biome_rowan::AstNode;
+use biome_css_syntax::{CssGenericProperty, CssGenericPropertyFields};
+use biome_formatter::write;
+
 #[derive(Debug, Clone, Default)]
 pub(crate) struct FormatCssGenericProperty;
 impl FormatNodeRule<CssGenericProperty> for FormatCssGenericProperty {
     fn fmt_fields(&self, node: &CssGenericProperty, f: &mut CssFormatter) -> FormatResult<()> {
-        format_verbatim_node(node.syntax()).fmt(f)
+        let CssGenericPropertyFields {
+            name,
+            colon_token,
+            value,
+        } = node.as_fields();
+
+        write!(
+            f,
+            [name.format(), colon_token.format(), space(), value.format()]
+        )
     }
 }
