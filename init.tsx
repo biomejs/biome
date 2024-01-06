7189c88b573a73871e655b8137766243b0b3f0ec
Emanuele Stoppa <my.burning@gmail.com>
2024-01-06 17:40:13 +0000

    fix design

---
+++ init.tsx
@@ -0,0 +1,8 @@
+ function Reproduction () {
+    return (
+        <a href="https://vitejs.dev" target="_blank">
+            <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+    )
+}
+
