01ada2e0a543392cb70c71ecd10b0aa94091b6a3
Jon <jonegeland@gmail.com>
2024-01-05 21:24:58 -0800

    fix(js_formatter): fix invalid formatting of expanded bindings for AsNeeded arrow parens (#1449)

---
+++ crates/biome_js_formatter/tests/specs/js/module/arrow/assignment_binding_line_break.js
@@ -0,0 +1,4 @@
+const foo = bar => `This is a string that's long enough to wrap and contains an interpolated value ${bar}
+and then a newline`
+
+const foo = bar => `This is a string that's long enough to wrap and contains an interpolated value ${bar} and then a newline`
