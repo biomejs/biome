b4f51fffdea37d96a03c28a9844c366a767693b2
Emanuele Stoppa <my.burning@gmail.com>
2024-01-07 10:35:34 +0000

    chore: fix mobile styles

--- init.tsx
+++
@@ -1,8 +0,0 @@
- function Reproduction () {
-    return (
-        <a href="https://vitejs.dev" target="_blank">
-            <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-    )
-}
-
