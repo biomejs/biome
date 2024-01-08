cc3a75670a3172a06b619ba1c20cd66f29750170
ty <62130798+togami2864@users.noreply.github.com>
2024-01-08 21:26:05 +0900

    feat(lint/noEmptyTypeParameters): add rule (#1458)

---
+++ crates/biome_js_analyze/src/syntax/nursery/no_empty_type_parameters.rs
@@ -0,0 +1,73 @@
+use biome_analyze::Ast;
+use biome_analyze::{context::RuleContext, declare_rule, Rule, RuleDiagnostic};
+use biome_console::markup;
+use biome_js_syntax::{JsSyntaxKind, TextRange, TsTypeParameters};
+use biome_rowan::{AstNode, AstSeparatedList, SyntaxNodeOptionExt};
+
+declare_rule! {
+    /// Disallow empty type parameters in type aliases and interfaces.
+    ///
+    /// TypeScript permits the use of empty type parameter lists in type alias and interface declarations; however, this practice is generally discouraged.
+    /// Allowing empty type parameter lists can lead to unclear or ambiguous code, where the intention of the generic type is not self-evident.
+    /// This rule disallows empty type parameter lists in type alias and interface declarations.
+    ///
+    /// ## Examples
+    ///
+    /// ### Invalid
+    ///
+    /// ```ts,expect_diagnostic
+    /// interface Foo<> {}
+    /// ```
+    /// ```ts,expect_diagnostic
+    /// type Foo<> = {}
+    /// ```
+    ///
+    /// ## Valid
+    ///
+    /// ```ts
+    /// interface Foo{}
+    /// ```
+    ///
+    /// ```ts
+    /// type Foo<T> = {
+    ///  bar: T;
+    /// }
+    /// ```
+    ///
+    pub(crate) NoEmptyTypeParameters {
+        version: "next",
+        name: "noEmptyTypeParameters",
+        recommended: true,
+    }
+}
+
+impl Rule for NoEmptyTypeParameters {
+    type Query = Ast<TsTypeParameters>;
+    type State = TextRange;
+    type Signals = Option<Self::State>;
+    type Options = ();
+
+    fn run(ctx: &RuleContext<Self>) -> Self::Signals {
+        let node = ctx.query();
+        if node.items().is_empty()
+            && matches!(
+                node.syntax().parent().kind(),
+                Some(
+                    JsSyntaxKind::TS_INTERFACE_DECLARATION
+                        | JsSyntaxKind::TS_TYPE_ALIAS_DECLARATION
+                )
+            )
+        {
+            return Some(node.items().range());
+        }
+        None
+    }
+
+    fn diagnostic(_: &RuleContext<Self>, reference: &Self::State) -> Option<RuleDiagnostic> {
+        Some(RuleDiagnostic::new(
+            rule_category!(),
+            reference,
+            markup! {"Empty type parameter list "<Emphasis>"<>"</Emphasis>" is not recommended"},
+        ))
+    }
+}
