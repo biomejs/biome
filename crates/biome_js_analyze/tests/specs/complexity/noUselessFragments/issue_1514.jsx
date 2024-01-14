73d57fce6fdddaa14c79c2127448fe2e7783b41a
ty <62130798+togami2864@users.noreply.github.com>
2024-01-14 21:58:55 +0900

    fix(lint/noUselessFragments): trim trivia to avoid suggesting invalid code fixes (#1553)

---
+++ crates/biome_js_analyze/tests/specs/complexity/noUselessFragments/issue_1514.jsx
@@ -0,0 +1,8 @@
+// https://github.com/biomejs/biome/issues/1514
+const SomeComponent = () => {
+    return (
+        <React.Fragment>
+            Your Data Dispute Request was sent to your current Employer
+        </React.Fragment>
+    );
+}
