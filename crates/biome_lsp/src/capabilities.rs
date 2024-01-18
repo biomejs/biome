db34750edb546d441116574b698fa6656549c6f7
Nicolas Hedger <nicolas@hedger.ch>
2024-01-18 12:50:59 +0100

    fix(biome_lsp): use dynamic formatter registration when possible

--- crates/biome_lsp/src/capabilities.rs
+++ crates/biome_lsp/src/capabilities.rs
@@ -8,6 +8,27 @@ use tower_lsp::lsp_types::{
 ///
 /// [`InitializeResult`]: lspower::lsp::InitializeResult
 pub(crate) fn server_capabilities(capabilities: &ClientCapabilities) -> ServerCapabilities {
+    let supports_formatting_dynamic_registration = capabilities
+        .text_document
+        .as_ref()
+        .and_then(|text_document| text_document.formatting.as_ref())
+        .and_then(|formatting| formatting.dynamic_registration)
+        .unwrap_or(false);
+
+    let supports_range_formatting_dynamic_registration = capabilities
+        .text_document
+        .as_ref()
+        .and_then(|text_document| text_document.range_formatting.as_ref())
+        .and_then(|range_formatting| range_formatting.dynamic_registration)
+        .unwrap_or(false);
+
+    let supports_on_type_formatting_dynamic_registration = capabilities
+        .text_document
+        .as_ref()
+        .and_then(|text_document| text_document.on_type_formatting.as_ref())
+        .and_then(|on_type_formatting| on_type_formatting.dynamic_registration)
+        .unwrap_or(false);
+
     ServerCapabilities {
         position_encoding: Some(match negotiated_encoding(capabilities) {
             PositionEncoding::Utf8 => PositionEncodingKind::UTF8,
@@ -19,12 +40,24 @@ pub(crate) fn server_capabilities(capabilities: &ClientCapabilities) -> ServerCa
         text_document_sync: Some(TextDocumentSyncCapability::Kind(
             TextDocumentSyncKind::INCREMENTAL,
         )),
-        document_formatting_provider: Some(OneOf::Left(true)),
-        document_range_formatting_provider: Some(OneOf::Left(true)),
-        document_on_type_formatting_provider: Some(DocumentOnTypeFormattingOptions {
-            first_trigger_character: String::from("}"),
-            more_trigger_character: Some(vec![String::from("]"), String::from(")")]),
-        }),
+        document_formatting_provider: if supports_formatting_dynamic_registration {
+            None
+        } else {
+            Some(OneOf::Left(true))
+        },
+        document_range_formatting_provider: if supports_range_formatting_dynamic_registration {
+            None
+        } else {
+            Some(OneOf::Left(true))
+        },
+        document_on_type_formatting_provider: if supports_on_type_formatting_dynamic_registration {
+            None
+        } else {
+            Some(DocumentOnTypeFormattingOptions {
+                first_trigger_character: String::from("}"),
+                more_trigger_character: Some(vec![String::from("]"), String::from(")")]),
+            })
+        },
         code_action_provider: Some(CodeActionProviderCapability::Simple(true)),
         rename_provider: None,
         ..Default::default()
