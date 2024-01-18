c4fabd0fb3c9e8bc48fbb9e6a729085205ecf5a8
Nicolas Hedger <nicolas@hedger.ch>
2024-01-18 12:55:08 +0100

    rename variables

--- crates/biome_lsp/src/capabilities.rs
+++ crates/biome_lsp/src/capabilities.rs
@@ -8,21 +8,21 @@ use tower_lsp::lsp_types::{
 ///
 /// [`InitializeResult`]: lspower::lsp::InitializeResult
 pub(crate) fn server_capabilities(capabilities: &ClientCapabilities) -> ServerCapabilities {
-    let supports_formatting_dynamic_registration = capabilities
+    let supports_formatter_dynamic_registration = capabilities
         .text_document
         .as_ref()
         .and_then(|text_document| text_document.formatting.as_ref())
         .and_then(|formatting| formatting.dynamic_registration)
         .unwrap_or(false);
 
-    let supports_range_formatting_dynamic_registration = capabilities
+    let supports_range_formatter_dynamic_registration = capabilities
         .text_document
         .as_ref()
         .and_then(|text_document| text_document.range_formatting.as_ref())
         .and_then(|range_formatting| range_formatting.dynamic_registration)
         .unwrap_or(false);
 
-    let supports_on_type_formatting_dynamic_registration = capabilities
+    let supports_on_type_formatter_dynamic_registration = capabilities
         .text_document
         .as_ref()
         .and_then(|text_document| text_document.on_type_formatting.as_ref())
@@ -40,17 +40,17 @@ pub(crate) fn server_capabilities(capabilities: &ClientCapabilities) -> ServerCa
         text_document_sync: Some(TextDocumentSyncCapability::Kind(
             TextDocumentSyncKind::INCREMENTAL,
         )),
-        document_formatting_provider: if supports_formatting_dynamic_registration {
+        document_formatting_provider: if supports_formatter_dynamic_registration {
             None
         } else {
             Some(OneOf::Left(true))
         },
-        document_range_formatting_provider: if supports_range_formatting_dynamic_registration {
+        document_range_formatting_provider: if supports_range_formatter_dynamic_registration {
             None
         } else {
             Some(OneOf::Left(true))
         },
-        document_on_type_formatting_provider: if supports_on_type_formatting_dynamic_registration {
+        document_on_type_formatting_provider: if supports_on_type_formatter_dynamic_registration {
             None
         } else {
             Some(DocumentOnTypeFormattingOptions {
