aa3c86d5526f45c8d59c0d6f7e9c597c4227dddd
Arend van Beelen jr. <arend@arendjr.nl>
2024-01-22 20:57:59 +0100

    Fix schema

--- xtask/codegen/src/generate_schema.rs
+++ xtask/codegen/src/generate_schema.rs
@@ -1,6 +1,6 @@
 use biome_json_formatter::context::JsonFormatOptions;
 use biome_json_parser::{parse_json, JsonParserOptions};
-use biome_service::Configuration;
+use biome_service::PartialConfiguration;
 use schemars::schema_for;
 use serde_json::to_string;
 use xtask::{project_root, Mode, Result};
@@ -9,7 +9,7 @@ use xtask_codegen::update;
 pub(crate) fn generate_configuration_schema(mode: Mode) -> Result<()> {
     let schema_path_npm = project_root().join("packages/@biomejs/biome/configuration_schema.json");
 
-    let schema = schema_for!(Configuration);
+    let schema = schema_for!(PartialConfiguration);
     let json_schema = to_string(&schema)?;
 
     let parsed = parse_json(&json_schema, JsonParserOptions::default());
