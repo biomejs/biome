74865f0c9ae227666f3444b6546fecb7a446139a
ty <62130798+togami2864@users.noreply.github.com>
2024-01-23 01:13:24 +0900

    fix: remove unneeded semicolon in the autofix suggestion (#1638)

--- crates/biome_js_analyze/tests/specs/complexity/noUselessThisAlias/invalid.js
+++ crates/biome_js_analyze/tests/specs/complexity/noUselessThisAlias/invalid.js
@@ -23,3 +23,31 @@ function f() {
         self.g();
     }
 }
+
+
+// https://github.com/biomejs/biome/issues/1633
+class FetchServersMetrics extends React.Component {
+    _fetchServersMetrics() {
+      const { onStatusChange, appIds, filters, time } = this.props;
+      const component = this;
+
+      fetchServers(appIds, time)
+        .then(servers => {
+          const data = isEmpty(filters) ? servers : applyFilters(servers, filters);
+          component.setState({
+            timestamp: new Date().getTime(),
+            isLoading: false,
+            data: data.filter(identity),
+          });
+        })
+        .catch((err) => {
+          component.setState({ serverError: throwOrResolve(err) });
+        })
+        .then(() => component.setState({ isLoading: false }));
+    }
+
+    render() {
+      const { children } = this.props;
+      return children(this._fetchServerMetrics);
+    }
+  }
