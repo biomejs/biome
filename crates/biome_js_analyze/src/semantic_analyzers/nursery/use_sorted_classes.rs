72ee1f117ede86bc4b4f06c4fc827ac264d0d67e
Dani Guardiola <hi@daniguardio.la>
2024-01-23 04:05:37 +0100

    improve docs

--- crates/biome_js_analyze/src/semantic_analyzers/nursery/use_sorted_classes.rs
+++ crates/biome_js_analyze/src/semantic_analyzers/nursery/use_sorted_classes.rs
@@ -31,7 +31,28 @@ declare_rule! {
     ///
     /// It is analogous to [`prettier-plugin-tailwindcss`](https://github.com/tailwindlabs/prettier-plugin-tailwindcss).
     ///
-    /// NOTE: this rule is only partially implemented. Progress is being tracked in the following GitHub issue: https://github.com/biomejs/biome/issues/1274
+    ///
+    /// :::caution
+    /// ## Important notes
+    ///
+    /// This rule is a work in progress, and is only partially implemented. Progress is being tracked in the following GitHub issue: https://github.com/biomejs/biome/issues/1274
+    ///
+    /// Currently, utility class sorting is **not part of the formatter**, and is implemented as a linter rule instead, with an automatic fix. The fix is, at this stage, classified as unsafe. This means that **it won't be applied automatically** as part of IDE actions such as "fix on save".
+    ///
+    /// We appreciate any feedback on this rule, and encourage you to try it out and report any issues you find.
+    ///
+    /// **Please read this entire documentation page before reporting an issue.**
+    ///
+    /// Notably, keep in mind that the following features are not supported yet:
+    ///
+    /// - Variant sorting.
+    /// - Custom utilitites and variants (such as ones introduced by Tailwind CSS plugins). Only the default Tailwind CSS configuration is supported.
+    /// - Options such as `prefix` and `separator`.
+    /// - Tagged template literals.
+    /// - Object properties (e.g. in `clsx` calls).
+    ///
+    /// Please don't report issues about these features.
+    /// :::
     ///
     /// ## Examples
     ///
@@ -62,6 +83,12 @@ declare_rule! {
     ///
     /// If specified, strings in the indicated functions will be sorted. This is useful when working with libraries like [`clsx`](https://github.com/lukeed/clsx) or [`cva`](https://cva.style/).
     ///
+    /// ```js
+    /// clsx("px-2 foo p-4 bar", {
+    ///     "block mx-4": condition,
+    /// });
+    /// ```
+    ///
     /// Tagged template literals are also supported, for example:
     ///
     /// ```js
@@ -69,21 +96,28 @@ declare_rule! {
     /// tw.div`px-2`;
     /// ```
     ///
-    /// NOTE: tagged template literal support has not been implemented yet.
+    /// :::caution
+    /// Tagged template literal support has not been implemented yet.
+    /// :::
     ///
     /// ### Sort-related
     ///
-    /// NOTE: at the moment, this rule does not support customizing the sort options. Instead, the default Tailwind CSS configuration is hard-coded.
+    /// :::caution
+    /// At the moment, this rule does not support customizing the sort options. Instead, the default Tailwind CSS configuration is hard-coded.
+    /// :::
     ///
     /// ## Differences with [Prettier](https://github.com/tailwindlabs/prettier-plugin-tailwindcss)
     ///
-    /// The main key difference is that Tailwind CSS and its Prettier plugin read the `tailwind.config.js` file, which Biome can't access. Instead, Biome implements a simpler version of the configuration. The trade-offs are explained below.
+    /// The main key difference is that Tailwind CSS and its Prettier plugin read and execute the `tailwind.config.js` JavaScript file, which Biome can't do. Instead, Biome implements a simpler version of the configuration. The trade-offs are explained below.
     ///
     /// ### Values are not known
     ///
     /// The rule has no knowledge of values such as colors, font sizes, or spacing values, which are normally defined in a configuration file like `tailwind.config.js`. Instead, the rule matches utilities that support values in a simpler way: if they start with a known utility prefix, such as `px-` or `text-`, they're considered valid.
     ///
-    /// This can result in false positives, i.e. classes that are wrongly recognized as utilities even though their values are incorrect. For example, if there's a `px-` utility defined in the configuration, it will match all of the following classes: `px-2`, `px-1337`, `px-[not-actually-valid]`, `px-literally-anything`.
+    /// This has two implications:
+    ///
+    /// - False positives: classes can be wrongly recognized as utilities even though their values are incorrect. For example, if there's a `px-` utility defined in the configuration, it will match all of the following classes: `px-2`, `px-1337`, `px-[not-actually-valid]`, `px-literally-anything`.
+    /// - No distinction between different utilities that share the same prefix: for example, `text-red-500` and `text-lg` are both interpreted as the same type of utility by this rule, even though the former refers to a color and the latter to a font size. This results in all utilities that share the same prefix being sorted together, regardless of their actual values.
     ///
     /// ### Custom additions must be specified
     ///
@@ -96,6 +130,8 @@ declare_rule! {
     /// ### Whitespace is collapsed
     ///
     /// The Tailwind CSS Prettier plugin preserves all original whitespace. This rule, however, collapses all whitespace (including newlines) into single spaces.
+    ///
+    /// This is a deliberate decision. We're unsure about this behavior, and would appreciate feedback on it. If this is a problem for you, please share a detailed explanation of your use case in [the GitHub issue](https://github.com/biomejs/biome/issues/1274).
     ///
     pub(crate) UseSortedClasses {
         version: "next",
