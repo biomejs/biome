cff390d636d9b64f9003baaf2a45eee57408eb4f
Victorien Elvinger <victorien@elvinger.fr>
2024-01-26 14:39:17 +0100

    refactor(biome_service): cleanup get_parse

--- crates/biome_service/src/workspace.rs
+++ crates/biome_service/src/workspace.rs
@@ -355,7 +355,7 @@ impl SupportKind {
     }
 }
 
-#[derive(Debug, Clone, Hash, serde::Serialize, serde::Deserialize, Eq, PartialEq)]
+#[derive(Debug, Copy, Clone, Hash, serde::Serialize, serde::Deserialize, Eq, PartialEq)]
 #[cfg_attr(feature = "schema", derive(schemars::JsonSchema))]
 pub enum FeatureName {
     Format,
