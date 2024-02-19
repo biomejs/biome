f8fd0e13226577211a4a1a32298057555de87de4
Yan Thomas <61414485+Yan-Thomas@users.noreply.github.com>
2024-02-19 17:58:11 -0300

    fix(website): generate RSS feed again

--- website/src/pages/feed.xml.js
+++ website/src/pages/feed.xml.js
@@ -1,7 +1,7 @@
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
 
-export async function get(context) {
+export async function GET(context) {
 	const posts = (await getCollection("blog")).sort(
 		(a, b) =>
 			new Date(b.data.pubDate).valueOf() - new Date(a.data.pubDate).valueOf(),
