2dd6128258adeb3b779a5ac2f6a6ebe15daf7c98
Zheyu Zhang <zheyuzhang03@gmail.com>
2024-02-21 12:48:18 +0800

    implementation

--- crates/biome_js_analyze/src/analyzers/nursery/no_exports_in_test.rs
+++ crates/biome_js_analyze/src/analyzers/nursery/no_exports_in_test.rs
@@ -1,8 +1,10 @@
-use crate::semantic_services::Semantic;
-use biome_analyze::{context::RuleContext, declare_rule, Rule, RuleDiagnostic};
+use biome_analyze::{
+    context::RuleContext, declare_rule, AddVisitor, Phases, QueryMatch, Queryable, Rule,
+    RuleDiagnostic, ServiceBag, Visitor,
+};
 use biome_console::markup;
-use biome_js_semantic::{Reference, ReferencesExtensions};
-use biome_js_syntax::JsIdentifierBinding;
+use biome_js_syntax::{AnyJsRoot, JsCallExpression, JsExport, JsLanguage};
+use biome_rowan::{AstNode, Language, TextRange, WalkEvent};
 
 declare_rule! {
     /// Succinct description of the rule.
@@ -32,38 +34,94 @@ declare_rule! {
     pub(crate) NoExportsInTest {
         version: "next",
         name: "noExportsInTest",
-        recommended: false,
+        recommended: true,
+    }
+}
+
+#[derive(Default)]
+
+struct ExportClauseInTestVisitor {
+    has_test: bool,
+    exports: Vec<JsExport>,
+}
+
+impl Visitor for ExportClauseInTestVisitor {
+    type Language = JsLanguage;
+
+    fn visit(
+        &mut self,
+        event: &biome_rowan::WalkEvent<biome_rowan::SyntaxNode<Self::Language>>,
+        mut ctx: biome_analyze::VisitorContext<Self::Language>,
+    ) {
+        match event {
+            WalkEvent::Enter(node) => {
+                // TODO(ah-yu): CommonJs export
+                if let Some(export) = JsExport::cast_ref(node) {
+                    self.exports.push(export)
+                }
+
+                if !self.has_test {
+                    if let Some(call_expr) = JsCallExpression::cast_ref(node) {
+                        self.has_test = call_expr.is_test_call_expression().ok().unwrap_or(false);
+                    }
+                }
+            }
+            WalkEvent::Leave(node) => {
+                if let Some(_) = AnyJsRoot::cast_ref(node) {
+                    if self.has_test {
+                        for export in self.exports.iter() {
+                            ctx.match_query(ExportClauseInTest(export.clone()));
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+pub(crate) struct ExportClauseInTest(JsExport);
+
+impl QueryMatch for ExportClauseInTest {
+    fn text_range(&self) -> TextRange {
+        self.0.range()
+    }
+}
+
+impl Queryable for ExportClauseInTest {
+    type Input = Self;
+    type Language = JsLanguage;
+    type Output = JsExport;
+    type Services = ();
+
+    fn build_visitor(
+        analyzer: &mut impl AddVisitor<Self::Language>,
+        _: &<Self::Language as Language>::Root,
+    ) {
+        analyzer.add_visitor(Phases::Syntax, ExportClauseInTestVisitor::default);
+    }
+
+    fn unwrap_match(_: &ServiceBag, query: &Self::Input) -> Self::Output {
+        query.0.clone()
     }
 }
 
 impl Rule for NoExportsInTest {
-    type Query = Semantic<JsIdentifierBinding>;
-    type State = Reference;
-    type Signals = Vec<Self::State>;
+    type Query = ExportClauseInTest;
+    type State = ();
+    type Signals = Option<Self::State>;
     type Options = ();
 
-    fn run(ctx: &RuleContext<Self>) -> Self::Signals {
-        let binding = ctx.query();
-        let model = ctx.model();
-        binding.all_references(model).collect()
+    fn run(_: &RuleContext<Self>) -> Self::Signals {
+        Some(())
     }
 
-    fn diagnostic(_: &RuleContext<Self>, reference: &Self::State) -> Option<RuleDiagnostic> {
-        //
-        // Read our guidelines to write great diagnostics:
-        // https://docs.rs/biome_analyze/latest/biome_analyze/#what-a-rule-should-say-to-the-user
-        //
-        Some(
-            RuleDiagnostic::new(
-                rule_category!(),
-                reference.range(),
-                markup! {
-                    "Variable is read here."
-                },
-            )
-            .note(markup! {
-                "This note will give you more information."
-            }),
-        )
+    fn diagnostic(ctx: &RuleContext<Self>, _: &Self::State) -> Option<RuleDiagnostic> {
+        Some(RuleDiagnostic::new(
+            rule_category!(),
+            ctx.query().range(),
+            markup! {
+                "Don't export in a test file."
+            },
+        ))
     }
 }
--- crates/biome_js_analyze/tests/specs/nursery/noExportsInTest/invalid.js
+++ crates/biome_js_analyze/tests/specs/nursery/noExportsInTest/invalid.js
@@ -1,3 +1,3 @@
 describe("a test", () => { expect(1).toBe(1); })
-export const myThing = "invalid"; test("a test", () => { expect(1).toBe(1); });
+export const myThing = "invalid";
 export default function () { }
