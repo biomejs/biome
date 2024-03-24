72830fc3a8c7a10bdda59d446f41291d4ba4c563
Denis Bezrukov <6227442+denbezrukov@users.noreply.github.com>
2024-03-25 00:55:07 +0200

    feat(css_formatter): CSS parser parses sequence of custom idents for @font-feature-values #1743

--- crates/biome_css_formatter/src/css/lists/custom_identifier_list.rs
+++ crates/biome_css_formatter/src/css/lists/custom_identifier_list.rs
@@ -5,6 +5,8 @@ pub(crate) struct FormatCssCustomIdentifierList;
 impl FormatRule<CssCustomIdentifierList> for FormatCssCustomIdentifierList {
     type Context = CssFormatContext;
     fn fmt(&self, node: &CssCustomIdentifierList, f: &mut CssFormatter) -> FormatResult<()> {
-        f.join_with(&space()).entries(node.iter().formatted()).finish()
+        f.join_with(&space())
+            .entries(node.iter().formatted())
+            .finish()
     }
 }
