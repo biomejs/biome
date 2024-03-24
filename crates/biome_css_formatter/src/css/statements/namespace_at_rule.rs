70bc75a76ad84f445bb5ea0514d478f9fb8be2aa
Denis Bezrukov <6227442+denbezrukov@users.noreply.github.com>
2024-03-24 22:03:33 +0200

    feat(css_formatter): CssNamespaceAtRule formatting

--- crates/biome_css_formatter/src/css/statements/namespace_at_rule.rs
+++ crates/biome_css_formatter/src/css/statements/namespace_at_rule.rs
@@ -1,10 +1,27 @@
 use crate::prelude::*;
-use biome_css_syntax::CssNamespaceAtRule;
-use biome_rowan::AstNode;
+use biome_css_syntax::{CssNamespaceAtRule, CssNamespaceAtRuleFields};
+use biome_formatter::write;
 #[derive(Debug, Clone, Default)]
 pub(crate) struct FormatCssNamespaceAtRule;
 impl FormatNodeRule<CssNamespaceAtRule> for FormatCssNamespaceAtRule {
     fn fmt_fields(&self, node: &CssNamespaceAtRule, f: &mut CssFormatter) -> FormatResult<()> {
-        format_verbatim_node(node.syntax()).fmt(f)
+        let CssNamespaceAtRuleFields {
+            namespace_token,
+            prefix,
+            url,
+            semicolon_token,
+        } = node.as_fields();
+
+        write!(
+            f,
+            [
+                namespace_token.format(),
+                space(),
+                prefix.format(),
+                space(),
+                url.format(),
+                semicolon_token.format(),
+            ]
+        )
     }
 }
