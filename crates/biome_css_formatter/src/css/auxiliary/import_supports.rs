3fdf1ee69ab3cc3bf2496c6a5ceee8f2bffcb5ce
Denis Bezrukov <6227442+denbezrukov@users.noreply.github.com>
2024-03-25 22:21:44 +0200

    feat(css_formatter): CssImportAtRule formatting (#2180)

--- crates/biome_css_formatter/src/css/auxiliary/import_supports.rs
+++ crates/biome_css_formatter/src/css/auxiliary/import_supports.rs
@@ -1,10 +1,28 @@
 use crate::prelude::*;
-use biome_css_syntax::CssImportSupports;
-use biome_rowan::AstNode;
+use biome_css_syntax::{CssImportSupports, CssImportSupportsFields};
+use biome_formatter::{format_args, write};
+
 #[derive(Debug, Clone, Default)]
 pub(crate) struct FormatCssImportSupports;
 impl FormatNodeRule<CssImportSupports> for FormatCssImportSupports {
     fn fmt_fields(&self, node: &CssImportSupports, f: &mut CssFormatter) -> FormatResult<()> {
-        format_verbatim_node(node.syntax()).fmt(f)
+        let CssImportSupportsFields {
+            supports_token,
+            l_paren_token,
+            condition,
+            r_paren_token,
+        } = node.as_fields();
+
+        write!(
+            f,
+            [
+                supports_token.format(),
+                group(&format_args![
+                    l_paren_token.format(),
+                    soft_block_indent(&condition.format()),
+                    r_paren_token.format()
+                ])
+            ]
+        )
     }
 }
