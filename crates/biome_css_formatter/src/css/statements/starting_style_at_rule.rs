86aaac86066ce3d16d8ccb6a7d9c0d7659834667
Denis Bezrukov <6227442+denbezrukov@users.noreply.github.com>
2024-03-25 22:41:03 +0200

    feat(css_formatter): Starting style at rule

--- crates/biome_css_formatter/src/css/statements/starting_style_at_rule.rs
+++ crates/biome_css_formatter/src/css/statements/starting_style_at_rule.rs
@@ -1,10 +1,23 @@
 use crate::prelude::*;
-use biome_css_syntax::CssStartingStyleAtRule;
-use biome_rowan::AstNode;
+use biome_css_syntax::{CssStartingStyleAtRule, CssStartingStyleAtRuleFields};
+use biome_formatter::write;
+
 #[derive(Debug, Clone, Default)]
 pub(crate) struct FormatCssStartingStyleAtRule;
 impl FormatNodeRule<CssStartingStyleAtRule> for FormatCssStartingStyleAtRule {
     fn fmt_fields(&self, node: &CssStartingStyleAtRule, f: &mut CssFormatter) -> FormatResult<()> {
-        format_verbatim_node(node.syntax()).fmt(f)
+        let CssStartingStyleAtRuleFields {
+            starting_style_token,
+            block,
+        } = node.as_fields();
+
+        write!(
+            f,
+            [
+                starting_style_token.format(),
+                space(),
+                block.format()
+            ]
+        )
     }
 }
