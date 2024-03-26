308a2c53bb998f7060ceebd01af0dcf571ce95fa
zoomdong <1344492820@qq.com>
2024-03-26 18:12:12 +0800

    fix(lint): rule noChildrenProp not valid under children in a new line

--- crates/biome_js_analyze/tests/specs/correctness/noChildrenProp/noChildrenPropInvalid.jsx
+++ crates/biome_js_analyze/tests/specs/correctness/noChildrenProp/noChildrenPropInvalid.jsx
@@ -2,6 +2,13 @@ import { createElement as aliased } from "react";
 
 <>
     <Component children={'foo'}></Component>
+    <Component
+      children={'foo'}
+    />
+    <Child 
+      className='testing'
+      children='Hello'
+    />
 </>
 
 createElement('div', {
