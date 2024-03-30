cd9e6d1b3f59b63d76dee775eb279a8d02568f7d
togami2864 <togami2864@gmail.com>
2024-03-31 07:40:26 +0900

    feat: handle dup family name in font-family

--- crates/biome_css_analyze/src/lint/nursery/no_font_family_duplicate_names.rs
+++ crates/biome_css_analyze/src/lint/nursery/no_font_family_duplicate_names.rs
@@ -1,7 +1,9 @@
-use biome_analyze::{context::RuleContext, declare_rule, Ast, Rule, RuleDiagnostic};
+use std::collections::HashSet;
+
+use biome_analyze::{context::RuleContext, declare_rule, Ast, Rule, RuleDiagnostic, RuleSource};
 use biome_console::markup;
-use biome_css_syntax::CssDeclarationOrRuleBlock;
-use biome_rowan::AstNode;
+use biome_css_syntax::{AnyCssGenericComponentValue, AnyCssValue, CssGenericProperty};
+use biome_rowan::{AstNode, TextRange};
 
 declare_rule! {
     /// Succinct description of the rule.
@@ -32,41 +34,76 @@ declare_rule! {
     pub NoFontFamilyDuplicateNames {
         version: "next",
         name: "noFontFamilyDuplicateNames",
-        recommended: false,
+        recommended: true,
+        source: RuleSource::EslintImportAccess("font-family-no-duplicate-names"),
     }
 }
 
+pub struct RuleState {
+    value: String,
+    span: TextRange,
+}
+
 impl Rule for NoFontFamilyDuplicateNames {
-    type Query = Ast<CssDeclarationOrRuleBlock>;
-    type State = CssDeclarationOrRuleBlock;
+    type Query = Ast<CssGenericProperty>;
+    type State = RuleState;
     type Signals = Option<Self::State>;
     type Options = ();
 
     fn run(ctx: &RuleContext<Self>) -> Option<Self::State> {
         let node = ctx.query();
-        if node.items().into_iter().next().is_none() {
-            return Some(node.clone());
+        let property_name = node.name().ok()?.text();
+        if property_name.to_lowercase() == "font-family" {
+            let mut seen: HashSet<String> = HashSet::new();
+            let value_list = node.value();
+            for v in value_list {
+                match v {
+                    AnyCssGenericComponentValue::CssGenericDelimiter(_) => continue,
+                    AnyCssGenericComponentValue::AnyCssValue(css_value) => match css_value {
+                        // A generic family name like `serif` or `sans-serif``.
+                        AnyCssValue::CssIdentifier(val) => {
+                            let font_name = val.text();
+                            if seen.contains(&font_name) {
+                                return Some(RuleState {
+                                    value: font_name,
+                                    span: val.range(),
+                                });
+                            };
+                            seen.insert(font_name);
+                        }
+                        // A font family name. e.g "Lucida Grande", "Arial".
+                        AnyCssValue::CssString(val) => {
+                            let normalized_val: String = val
+                                .text()
+                                .chars()
+                                .filter(|&c| c != '\'' && c != '\"' && !c.is_whitespace())
+                                .collect();
+                            if seen.contains(&normalized_val) {
+                                return Some(RuleState {
+                                    value: normalized_val.to_string(),
+                                    span: val.range(),
+                                });
+                            }
+                            seen.insert(normalized_val);
+                        }
+                        _ => continue,
+                    },
+                }
+            }
+            None
+        } else {
+            None
         }
-        None
     }
 
-    fn diagnostic(_: &RuleContext<Self>, node: &Self::State) -> Option<RuleDiagnostic> {
-        //
-        // Read our guidelines to write great diagnostics:
-        // https://docs.rs/biome_analyze/latest/biome_analyze/#what-a-rule-should-say-to-the-user
-        //
-        let span = node.range();
-        Some(
-            RuleDiagnostic::new(
-                rule_category!(),
-                span,
-                markup! {
-                    "Unexpected empty block is not allowed"
-                },
-            )
-            .note(markup! {
-                    "This note will give you more information."
-            }),
-        )
+    fn diagnostic(_: &RuleContext<Self>, state: &Self::State) -> Option<RuleDiagnostic> {
+        let span = state.span;
+        Some(RuleDiagnostic::new(
+            rule_category!(),
+            span,
+            markup! {
+                "Unexpected duplicate font name: "<Emphasis>{ state.value }</Emphasis>
+            },
+        ))
     }
 }
