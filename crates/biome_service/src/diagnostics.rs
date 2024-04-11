60671ec3a4c59421583d5d1ec8cfe30d45b27cd7
Emanuele Stoppa <my.burning@gmail.com>
2024-04-11 18:40:44 +0100

    feat(cli): JSON reporter (#2364)

--- crates/biome_service/src/diagnostics.rs
+++ crates/biome_service/src/diagnostics.rs
@@ -94,12 +94,6 @@ impl WorkspaceError {
         })
     }
 
-    pub fn report_not_serializable(reason: impl Into<String>) -> Self {
-        Self::ReportNotSerializable(ReportNotSerializable {
-            reason: reason.into(),
-        })
-    }
-
     pub fn vcs_disabled() -> Self {
         Self::Vcs(VcsDiagnostic::DisabledVcs(DisabledVcs {}))
     }
