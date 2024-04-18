c9c9b6de3282814c47a08ae50fd20400b769738b
Zheyu Zhang <zheyuzhang03@gmail.com>
2024-04-18 15:35:03 +0800

    add parentheses for expr that has leading multiline comments

--- crates/biome_js_formatter/tests/specs/js/module/statement/return.js
+++ crates/biome_js_formatter/tests/specs/js/module/statement/return.js
@@ -5,3 +5,22 @@ function f1() {
 function f2() {
     return 1,3,4
 }
+
+function f3() {
+    return /* commment */'1'
+}
+
+function f4() {
+    return (
+        /* comment */
+        '1'
+    )
+}
+
+function f5() {
+    return (
+        /*
+         * multiline comment 
+         */ '1'
+    )
+}
