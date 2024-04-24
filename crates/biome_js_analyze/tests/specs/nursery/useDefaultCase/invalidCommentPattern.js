8f0a338f50ce8db469fd595d164b4f54d4b309b4
Michell Ocaña <michell.santo@cwi.com.br>
2024-04-24 09:06:34 -0300

    feat(lint/useDefaultSwitchClause): add tests to custom config

---
+++ crates/biome_js_analyze/tests/specs/nursery/useDefaultCase/invalidCommentPattern.js
@@ -0,0 +1,29 @@
+switch (a) {
+	case 1:
+		break;
+}
+
+switch (a) {
+	// skip default
+	case 1:
+		break;
+}
+
+switch (a) {
+	case 1:
+		break;
+	// skip default
+	// nope
+}
+
+switch (a) {
+	case 1:
+		break;
+	// skipdefault
+}
+
+switch (a) {
+	case 1:
+		break;
+	// no default
+}
---
+++ crates/biome_js_analyze/tests/specs/nursery/useDefaultCase/validCommentPattern.js
@@ -0,0 +1,33 @@
+switch (a) {
+	case 1:
+		break;
+	default:
+		break;
+}
+
+switch (a) {
+	case 1:
+		break;
+	// skip default case
+}
+
+switch (a) {
+	case 1:
+		break;
+	// SKIP DEFAULT
+}
+
+switch (a) {
+	case 1:
+		break;
+	// SkIp DeFaUlT
+}
+
+switch (a) {
+	case 1:
+		break;
+	// skip default 123
+}
+
+switch (a) {
+}
