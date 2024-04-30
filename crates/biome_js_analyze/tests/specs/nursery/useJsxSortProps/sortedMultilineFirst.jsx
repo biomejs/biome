a88089ddcd66e3c073879f63684cae43a9e4da31
vohoanglong0107 <vo.hoang.long@outlook.com>
2024-04-30 06:22:56 +0000

    test: multiline first option

---
+++ crates/biome_js_analyze/tests/specs/nursery/useJsxSortProps/sortedMultilineFirst.jsx
@@ -0,0 +1,18 @@
+/* should not generate diagnostics */
+<Hello
+  classes={{
+    greetings: classes.greetings,
+  }}
+  name="John"
+  tel={5555555}
+/>;
+
+<Hello
+  active
+  validate
+  classes={{
+    greetings: classes.greetings,
+  }}
+  name="John"
+  tel={5555555}
+/>;
---
+++ crates/biome_js_analyze/tests/specs/nursery/useJsxSortProps/unsortedMultilineFirst.jsx
@@ -0,0 +1,17 @@
+<Hello
+  name="John"
+  classes={{
+    greetings: classes.greetings,
+  }}
+  tel={5555555}
+/>;
+
+<Hello
+  active
+  classes={{
+    greetings: classes.greetings,
+  }}
+  validate
+  name="John"
+  tel={5555555}
+/>;
