ef8abbe006133adb1fd7627e93f8a73dc0e687d8
Emanuele Stoppa <my.burning@gmail.com>
2024-05-03 17:19:07 +0100

    chore(deps): upgrade oxc_resolver (#2694)

--- crates/biome_fs/src/fs/os.rs
+++ crates/biome_fs/src/fs/os.rs
@@ -33,7 +33,7 @@ impl OsFileSystem {
             working_directory: Some(working_directory),
             configuration_resolver: AssertUnwindSafe(Resolver::new(ResolveOptions {
                 condition_names: vec!["node".to_string(), "import".to_string()],
-                extensions: vec!["*.json".to_string(), "*.jsonc".to_string()],
+                extensions: vec![".json".to_string(), ".jsonc".to_string()],
                 ..ResolveOptions::default()
             })),
         }
@@ -46,7 +46,7 @@ impl Default for OsFileSystem {
             working_directory: env::current_dir().ok(),
             configuration_resolver: AssertUnwindSafe(Resolver::new(ResolveOptions {
                 condition_names: vec!["node".to_string(), "import".to_string()],
-                extensions: vec!["*.json".to_string(), "*.jsonc".to_string()],
+                extensions: vec![".json".to_string(), ".jsonc".to_string()],
                 ..ResolveOptions::default()
             })),
         }
