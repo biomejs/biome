e825c55ee7831c32e5b83154b3cd7c888089e879
tasshi / Masaharu Tashiro <33759872+tasshi-me@users.noreply.github.com>
2024-05-04 15:51:25 +0900

    fix(cli): correctly lint HTML-ish piped from stdin (#2686)

--- crates/biome_cli/tests/cases/handle_svelte_files.rs
+++ crates/biome_cli/tests/cases/handle_svelte_files.rs
@@ -1,6 +1,8 @@
 use crate::run_cli;
-use crate::snap_test::{assert_cli_snapshot, assert_file_contents, SnapshotPayload};
-use biome_console::BufferConsole;
+use crate::snap_test::{
+    assert_cli_snapshot, assert_file_contents, markup_to_string, SnapshotPayload,
+};
+use biome_console::{markup, BufferConsole};
 use biome_fs::MemoryFileSystem;
 use biome_service::DynRef;
 use bpaf::Args;
@@ -32,6 +34,47 @@ const hello: string = "world";
 
 const SVELTE_CARRIAGE_RETURN_LINE_FEED_FILE_UNFORMATTED: &str =
     "<script>\r\n  const a    = \"b\";\r\n</script>\r\n<div></div>";
+
+const SVELTE_TS_FILE_LINT_BEFORE: &str = r#"<script context="module" lang="ts">
+var foo: string = "";
+</script>
+<div></div>"#;
+
+const SVELTE_TS_FILE_LINT_APPLY_AFTER: &str = r#"<script context="module" lang="ts">
+var foo = "";
+</script>
+<div></div>"#;
+
+const SVELTE_TS_FILE_LINT_APPLY_UNSAFE_AFTER: &str = r#"<script context="module" lang="ts">
+const foo = "";
+</script>
+<div></div>"#;
+
+const SVELTE_TS_FILE_CHECK_BEFORE: &str = r#"<script context="module" lang="ts">
+import { Form as   Form }     from './components/Form.svelte' ;
+import     Button     from "./components/Button.svelte";
+debugger;
+statement ( ) ;
+var foo: string = "";
+</script>
+<div></div>"#;
+
+const SVELTE_TS_FILE_CHECK_APPLY_AFTER: &str = r#"<script context="module" lang="ts">
+import Button from "./components/Button.svelte";
+import { Form } from "./components/Form.svelte";
+debugger;
+statement();
+var foo = "";
+</script>
+<div></div>"#;
+
+const SVELTE_TS_FILE_CHECK_APPLY_UNSAFE_AFTER: &str = r#"<script context="module" lang="ts">
+import Button from "./components/Button.svelte";
+import { Form } from "./components/Form.svelte";
+statement();
+const foo = "";
+</script>
+<div></div>"#;
 
 #[test]
 fn sorts_imports_check() {
@@ -213,6 +256,310 @@ fn format_svelte_carriage_return_line_feed_files() {
     assert_cli_snapshot(SnapshotPayload::new(
         module_path!(),
         "format_svelte_carriage_return_line_feed_files",
+        fs,
+        console,
+        result,
+    ));
+}
+
+#[test]
+fn format_stdin_successfully() {
+    let mut fs = MemoryFileSystem::default();
+    let mut console = BufferConsole::default();
+
+    console
+        .in_buffer
+        .push(SVELTE_TS_CONTEXT_MODULE_FILE_UNFORMATTED.to_string());
+
+    let result = run_cli(
+        DynRef::Borrowed(&mut fs),
+        &mut console,
+        Args::from(["format", "--stdin-file-path", "file.svelte"].as_slice()),
+    );
+
+    assert!(result.is_ok(), "run_cli returned {result:?}");
+
+    let message = console
+        .out_buffer
+        .first()
+        .expect("Console should have written a message");
+
+    let content = markup_to_string(markup! {
+        {message.content}
+    });
+
+    assert_eq!(content, SVELTE_TS_CONTEXT_MODULE_FILE_FORMATTED);
+
+    assert_cli_snapshot(SnapshotPayload::new(
+        module_path!(),
+        "format_stdin_successfully",
+        fs,
+        console,
+        result,
+    ));
+}
+
+#[test]
+fn format_stdin_write_successfully() {
+    let mut fs = MemoryFileSystem::default();
+    let mut console = BufferConsole::default();
+
+    console
+        .in_buffer
+        .push(SVELTE_TS_CONTEXT_MODULE_FILE_UNFORMATTED.to_string());
+
+    let result = run_cli(
+        DynRef::Borrowed(&mut fs),
+        &mut console,
+        Args::from(["format", "--write", "--stdin-file-path", "file.svelte"].as_slice()),
+    );
+
+    assert!(result.is_ok(), "run_cli returned {result:?}");
+
+    let message = console
+        .out_buffer
+        .first()
+        .expect("Console should have written a message");
+
+    let content = markup_to_string(markup! {
+        {message.content}
+    });
+
+    assert_eq!(content, SVELTE_TS_CONTEXT_MODULE_FILE_FORMATTED);
+
+    assert_cli_snapshot(SnapshotPayload::new(
+        module_path!(),
+        "format_stdin_write_successfully",
+        fs,
+        console,
+        result,
+    ));
+}
+
+#[test]
+fn lint_stdin_successfully() {
+    let mut fs = MemoryFileSystem::default();
+    let mut console = BufferConsole::default();
+
+    console
+        .in_buffer
+        .push(SVELTE_TS_FILE_LINT_BEFORE.to_string());
+
+    let result = run_cli(
+        DynRef::Borrowed(&mut fs),
+        &mut console,
+        Args::from(["lint", "--stdin-file-path", "file.svelte"].as_slice()),
+    );
+
+    assert!(result.is_ok(), "run_cli returned {result:?}");
+
+    let message = console
+        .out_buffer
+        .first()
+        .expect("Console should have written a message");
+
+    let content = markup_to_string(markup! {
+        {message.content}
+    });
+
+    assert_eq!(content, SVELTE_TS_FILE_LINT_BEFORE);
+
+    assert_cli_snapshot(SnapshotPayload::new(
+        module_path!(),
+        "lint_stdin_successfully",
+        fs,
+        console,
+        result,
+    ));
+}
+
+#[test]
+fn lint_stdin_apply_successfully() {
+    let mut fs = MemoryFileSystem::default();
+    let mut console = BufferConsole::default();
+
+    console
+        .in_buffer
+        .push(SVELTE_TS_FILE_LINT_BEFORE.to_string());
+
+    let result = run_cli(
+        DynRef::Borrowed(&mut fs),
+        &mut console,
+        Args::from(["lint", "--apply", "--stdin-file-path", "file.svelte"].as_slice()),
+    );
+
+    assert!(result.is_ok(), "run_cli returned {result:?}");
+
+    let message = console
+        .out_buffer
+        .first()
+        .expect("Console should have written a message");
+
+    let content = markup_to_string(markup! {
+        {message.content}
+    });
+
+    assert_eq!(content, SVELTE_TS_FILE_LINT_APPLY_AFTER);
+
+    assert_cli_snapshot(SnapshotPayload::new(
+        module_path!(),
+        "lint_stdin_apply_successfully",
+        fs,
+        console,
+        result,
+    ));
+}
+
+#[test]
+fn lint_stdin_apply_unsafe_successfully() {
+    let mut fs = MemoryFileSystem::default();
+    let mut console = BufferConsole::default();
+
+    console
+        .in_buffer
+        .push(SVELTE_TS_FILE_LINT_BEFORE.to_string());
+
+    let result = run_cli(
+        DynRef::Borrowed(&mut fs),
+        &mut console,
+        Args::from(["lint", "--apply-unsafe", "--stdin-file-path", "file.svelte"].as_slice()),
+    );
+
+    assert!(result.is_ok(), "run_cli returned {result:?}");
+
+    let message = console
+        .out_buffer
+        .first()
+        .expect("Console should have written a message");
+
+    let content = markup_to_string(markup! {
+        {message.content}
+    });
+
+    assert_eq!(content, SVELTE_TS_FILE_LINT_APPLY_UNSAFE_AFTER);
+
+    assert_cli_snapshot(SnapshotPayload::new(
+        module_path!(),
+        "lint_stdin_apply_unsafe_successfully",
+        fs,
+        console,
+        result,
+    ));
+}
+
+#[test]
+fn check_stdin_successfully() {
+    let mut fs = MemoryFileSystem::default();
+    let mut console = BufferConsole::default();
+
+    console
+        .in_buffer
+        .push(SVELTE_TS_FILE_CHECK_BEFORE.to_string());
+
+    let result = run_cli(
+        DynRef::Borrowed(&mut fs),
+        &mut console,
+        Args::from(["check", "--stdin-file-path", "file.svelte"].as_slice()),
+    );
+
+    assert!(result.is_ok(), "run_cli returned {result:?}");
+
+    let message = console
+        .out_buffer
+        .first()
+        .expect("Console should have written a message");
+
+    let content = markup_to_string(markup! {
+        {message.content}
+    });
+
+    assert_eq!(content, SVELTE_TS_FILE_CHECK_BEFORE);
+
+    assert_cli_snapshot(SnapshotPayload::new(
+        module_path!(),
+        "check_stdin_successfully",
+        fs,
+        console,
+        result,
+    ));
+}
+
+#[test]
+fn check_stdin_apply_successfully() {
+    let mut fs = MemoryFileSystem::default();
+    let mut console = BufferConsole::default();
+
+    console
+        .in_buffer
+        .push(SVELTE_TS_FILE_CHECK_BEFORE.to_string());
+
+    let result = run_cli(
+        DynRef::Borrowed(&mut fs),
+        &mut console,
+        Args::from(["check", "--apply", "--stdin-file-path", "file.svelte"].as_slice()),
+    );
+
+    assert!(result.is_ok(), "run_cli returned {result:?}");
+
+    let message = console
+        .out_buffer
+        .first()
+        .expect("Console should have written a message");
+
+    let content = markup_to_string(markup! {
+        {message.content}
+    });
+
+    assert_eq!(content, SVELTE_TS_FILE_CHECK_APPLY_AFTER);
+
+    assert_cli_snapshot(SnapshotPayload::new(
+        module_path!(),
+        "check_stdin_apply_successfully",
+        fs,
+        console,
+        result,
+    ));
+}
+
+#[test]
+fn check_stdin_apply_unsafe_successfully() {
+    let mut fs = MemoryFileSystem::default();
+    let mut console = BufferConsole::default();
+
+    console
+        .in_buffer
+        .push(SVELTE_TS_FILE_CHECK_BEFORE.to_string());
+
+    let result = run_cli(
+        DynRef::Borrowed(&mut fs),
+        &mut console,
+        Args::from(
+            [
+                "check",
+                "--apply-unsafe",
+                "--stdin-file-path",
+                "file.svelte",
+            ]
+            .as_slice(),
+        ),
+    );
+
+    assert!(result.is_ok(), "run_cli returned {result:?}");
+
+    let message = console
+        .out_buffer
+        .first()
+        .expect("Console should have written a message");
+
+    let content = markup_to_string(markup! {
+        {message.content}
+    });
+
+    assert_eq!(content, SVELTE_TS_FILE_CHECK_APPLY_UNSAFE_AFTER);
+
+    assert_cli_snapshot(SnapshotPayload::new(
+        module_path!(),
+        "check_stdin_apply_unsafe_successfully",
         fs,
         console,
         result,
