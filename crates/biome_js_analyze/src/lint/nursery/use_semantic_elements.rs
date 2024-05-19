a3ab25be17d5c614b3cbfa8b554cdb5a72b6c247
fujiyamaorange <kaito.bakuman@gmail.com>
2024-05-19 23:36:20 +0900

    fix: add test cases & refactor code

--- crates/biome_js_analyze/src/lint/nursery/use_semantic_elements.rs
+++ crates/biome_js_analyze/src/lint/nursery/use_semantic_elements.rs
@@ -16,15 +16,17 @@ declare_rule! {
     ///
     /// ### Invalid
     ///
-    /// ```js,expect_diagnostic
+    /// ```jsx,expect_diagnostic
     /// <div role="checkbox">
+    /// ```
+    ///
+    /// ```jsx,expect_diagnostic
     /// <div role="img">
     /// ```
     ///
     /// ### Valid
     ///
-    /// ```js
-    /// // var a = 1;
+    /// ```jsx
     /// <div>...</div>
     /// <header>...</header>
     /// <img alt="" src="image.jpg" />
@@ -47,22 +49,17 @@ impl Rule for UseSemanticElements {
 
     fn run(ctx: &RuleContext<Self>) -> Self::Signals {
         let node = ctx.query();
+        let role_attributes = node.find_attribute_by_name("role").unwrap();
 
-        let attributes = node.attributes();
-        for attr in attributes {
-            let attr_value = attr.as_jsx_attribute().unwrap().name_value_token().unwrap();
-            let attr_name = attr_value.text_trimmed();
-            if attr_name == "role" {
-                let extract_attributes = ctx.extract_attributes(&node.attributes());
+        if let Some(attr) = role_attributes {
+            let extract_attributes = ctx.extract_attributes(&node.attributes());
 
-                let element: biome_rowan::SyntaxToken<biome_js_syntax::JsLanguage> =
-                    node.name().ok()?.as_jsx_name()?.value_token().ok()?;
-                let element_name = element.text_trimmed();
-                let is_not_interative =
-                    AriaRoles.is_not_interactive_element(element_name, extract_attributes);
-                if is_not_interative {
-                    return Some(attr);
-                }
+            let element = node.name().ok()?.as_jsx_name()?.value_token().ok()?;
+            let element_name = element.text_trimmed();
+            let is_not_interative =
+                AriaRoles.is_not_interactive_element(element_name, extract_attributes);
+            if is_not_interative {
+                return Some(AnyJsxAttribute::from(attr));
             }
         }
 
@@ -70,22 +67,20 @@ impl Rule for UseSemanticElements {
     }
 
     fn diagnostic(_ctx: &RuleContext<Self>, state: &Self::State) -> Option<RuleDiagnostic> {
-        //
-        // Read our guidelines to write great diagnostics:
-        // https://docs.rs/biome_analyze/latest/biome_analyze/#what-a-rule-should-say-to-the-user
-        //
-
         Some(
             RuleDiagnostic::new(
                 rule_category!(),
                 state.range(),
                 markup! {
-                    "This JSX element uses a `role` attribute. Use a semantic element instead."
+                    "The element with this role can be changed to a DOM element that already this role."
                 },
             )
-            .note(markup! {
-                "Semantic elements like `<button>`, `<input>`, `<textarea>`, `<a>`, `<img>`, `<table>`, `<article>`, `<section>`, `<nav>`, `<aside>`, `<header>`, `<footer>`, `<main>`, `<figure>`, `<figcaption>`, `<details>`, `<summary>`, `<dialog>`, `<menu>`, `<menuitem>`, `<fieldset>`, `<legend>`, `<caption>`, `<colgroup>`, `<col>`, `<optgroup>`, `<option>`, `<select>`, `<datalist>`, `<output>`, `<progress>`, `<meter>`, `<time>`, `<audio>`, `<video>`, `<track>`, `<source>`, `<embed>`, `<object>`, `<param>`, `<iframe>`, `<canvas>`, `<map>`, `<area>`, `<svg>`, `<math>` are more accessible and provide better semantics."
-            }),
+            .footer_list(
+                markup! {
+                    "For examples and more information, see" <Hyperlink href="https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Roles">"WAI-ARIA Roles"</Hyperlink>
+                },
+                &["serif", "sans-serif", "monospace", "etc."],
+            ),
         )
     }
 }
