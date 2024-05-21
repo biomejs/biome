336d5db8a8b326780f281dd1a8ce4341eac709ed
Võ Hoàng Long <78085736+vohoanglong0107@users.noreply.github.com>
2024-05-21 16:32:46 +0900

    feat(biome_graphql_parser): parse scalar extension (#2927)

--- crates/biome_graphql_parser/src/parser/definitions/scalar.rs
+++ crates/biome_graphql_parser/src/parser/definitions/scalar.rs
@@ -1,10 +1,13 @@
 use crate::parser::{
-    directive::DirectiveList, parse_description, parse_error::expected_name, parse_name,
-    GraphqlParser,
+    directive::DirectiveList,
+    parse_description,
+    parse_error::{expected_directive, expected_name},
+    parse_name, GraphqlParser,
 };
 use biome_graphql_syntax::{GraphqlSyntaxKind::*, T};
 use biome_parser::{
-    parse_lists::ParseNodeList, parsed_syntax::ParsedSyntax, prelude::ParsedSyntax::*, Parser,
+    parse_lists::ParseNodeList, parsed_syntax::ParsedSyntax, prelude::ParsedSyntax::*,
+    token_source::TokenSource, Parser,
 };
 
 #[inline]
@@ -19,4 +22,22 @@ pub(crate) fn parse_scalar_type_definition(p: &mut GraphqlParser) -> ParsedSynta
     DirectiveList.parse_list(p);
 
     Present(m.complete(p, GRAPHQL_SCALAR_TYPE_DEFINITION))
+}
+
+#[inline]
+pub(crate) fn parse_scalar_type_extension(p: &mut GraphqlParser) -> ParsedSyntax {
+    let m = p.start();
+
+    p.bump(T![extend]);
+    p.bump(T![scalar]);
+
+    parse_name(p).or_add_diagnostic(p, expected_name);
+    let pos = p.source().position();
+    DirectiveList.parse_list(p);
+    let directive_empty = p.source().position() == pos;
+    if directive_empty {
+        p.error(expected_directive(p, p.cur_range()));
+    }
+
+    Present(m.complete(p, GRAPHQL_SCALAR_TYPE_EXTENSION))
 }
