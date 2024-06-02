1ad43119a58924d2e8fa4d86137375c3f576e114
chansuke <moonset20@gmail.com>
2024-06-02 14:15:43 +0900

    chore: update testcase

--- crates/biome_js_analyze/tests/specs/nursery/useConsistentStringFunctions/invalid.js
+++ crates/biome_js_analyze/tests/specs/nursery/useConsistentStringFunctions/invalid.js
@@ -1,5 +1,32 @@
-const foo = bar.trimLeft();
-const foo = bar.trimRight();
+foo.trimLeft();
+foo.trimRight();
+trimLeft.trimRight();
+foo.trimLeft.trimRight();
+"foo".trimLeft();
+foo
+	// comment
+	.trimRight /* comment */
+	/* comment */
+	();
+foo?.trimLeft();
 
-foo.substr(start, length);
-foo.substring(indexStart, indexEnd);
+// Substring
+foo.substr()
+foo?.substr()
+foo.bar?.substring()
+foo?.[0]?.substring()
+foo.bar.substr?.()
+foo.bar?.substring?.()
+foo.bar?.baz?.substr()
+foo.bar?.baz.substring()
+foo.bar.baz?.substr()
+"foo".substr()
+"foo".substr(bar.length, Math.min(baz, 100)) // "foo".slice(bar.length, bar.length + Math.min(baz, 100))
+"foo".substr(1, "abc".length) // "foo".slice(1, 1 + "abc".length)
+"foo".substr("1", 2)
+"foo".substring(length, 0) // "foo".slice(0, Math.max(0, length))
+foo.substring(start) // foo.slice(Math.max(0, start))
+foo.substring(start, end)
+"foo".substring(1, 3)
+// Extra arguments
+foo.substring(1, 2, 3)
