0f00240447a788f9098e758208ef764b0b376531
chansuke <moonset20@gmail.com>
2024-06-08 10:57:23 +0900

    feat(biome_js_analyzer): `useTrimStartEnd`

---
+++ crates/biome_js_analyze/tests/specs/nursery/useTrimStartEnd/invalid.js
@@ -0,0 +1,11 @@
+foo.trimLeft();
+foo.trimRight();
+trimLeft.trimRight();
+foo.trimLeft.trimRight();
+"foo".trimLeft();
+foo
+	// comment
+	.trimRight /* comment */
+	/* comment */
+	();
+foo?.trimLeft();
--- packages/@biomejs/backend-jsonrpc/src/workspace.ts
+++ packages/@biomejs/backend-jsonrpc/src/workspace.ts
@@ -1175,9 +1175,9 @@ export interface Nursery {
 	 */
 	useTopLevelRegex?: RuleConfiguration_for_Null;
 	/**
-	 * Use valid values for the autocomplete attribute on input elements.
+	 * Enforce the use of trimStart() over trimLeft() and trimeEnd() over trimRight().
 	 */
-	useValidAutocomplete?: RuleConfiguration_for_UseValidAutocompleteOptions;
+	useTrimStartEnd?: RuleFixConfiguration_for_Null;
 }
 /**
  * A list of rules that belong to this group
@@ -2429,7 +2429,7 @@ export type Category =
 	| "lint/nursery/useThrowNewError"
 	| "lint/nursery/useThrowOnlyError"
 	| "lint/nursery/useTopLevelRegex"
-	| "lint/nursery/useValidAutocomplete"
+	| "lint/nursery/useTrimStartEnd"
 	| "lint/performance/noAccumulatingSpread"
 	| "lint/performance/noBarrelFile"
 	| "lint/performance/noDelete"
