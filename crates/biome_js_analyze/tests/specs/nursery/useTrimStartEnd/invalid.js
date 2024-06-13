36e46b7dcaf2470c7281ca232175673a552df3d9
chansuke <moonset20@gmail.com>
2024-06-14 01:53:17 +0900

    fix: remove illegal syntax test cases

--- crates/biome_js_analyze/tests/specs/nursery/useTrimStartEnd/invalid.js
+++ crates/biome_js_analyze/tests/specs/nursery/useTrimStartEnd/invalid.js
@@ -1,11 +1,11 @@
-foo.trimLeft();
-foo.trimRight();
-trimLeft.trimRight();
-foo.trimLeft.trimRight();
-"foo".trimLeft();
+foo.trimLeft()
+foo.trimRight()
+trimLeft.trimRight()
+foo.trimLeft.trimRight()
+"foo".trimLeft()
 foo
 	// comment
 	.trimRight /* comment */
 	/* comment */
-	();
-foo?.trimLeft();
+	()
+foo?.trimLeft()
