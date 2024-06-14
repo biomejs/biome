7d690a8e1813c0ce6099752be43e4011318d6a9c
chansuke <moonset20@gmail.com>
2024-06-14 12:58:13 +0900

    fix: add logic for parameters

--- crates/biome_js_analyze/src/lint/nursery/use_trim_start_end.rs
+++ crates/biome_js_analyze/src/lint/nursery/use_trim_start_end.rs
@@ -5,7 +5,7 @@ use biome_analyze::{
 use biome_console::markup;
 use biome_js_factory::make;
 use biome_js_syntax::JsCallExpression;
-use biome_rowan::{BatchMutationExt, TextRange, TokenText};
+use biome_rowan::{AstSeparatedList, BatchMutationExt, TextRange, TokenText};
 
 use crate::JsRuleAction;
 
@@ -14,6 +14,11 @@ declare_rule! {
     ///
     /// While `String.trimLeft()` and `String.trimRight()` are aliases for `String.trimStart()` and `String.trimEnd()`,
     /// only using the latter pair ensures consistency and is preferable for their direction-independent wording.
+    ///
+    /// Note that `String.trimStart()` and `String.trimEnd()` methods do not take any parameters. Any arguments passed to these methods will be ignored.
+    /// See the MDN documentation for more details:
+    /// - [String.prototype.trimStart()](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/trimStart)
+    /// - [String.prototype.trimEnd()](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/trimEnd)
     ///
     /// ## Examples
     ///
@@ -62,6 +67,17 @@ impl Rule for UseTrimStartEnd {
 
     fn run(ctx: &RuleContext<Self>) -> Self::Signals {
         let node = ctx.query();
+        let arguments = node.arguments().ok()?;
+        let args = arguments.args();
+
+        if !args.is_empty() {
+            // If arguments are present, it suggests this function call may not be intended for `String.trimStart()` or `String.trimEnd()`,
+            // as these methods do not accept parameters according to the specification:
+            // - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/trimStart#parameters
+            // - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/trimEnd#parameters
+            return None;
+        }
+
         let callee = node.callee().ok()?;
         let expression = callee.as_js_static_member_expression()?;
         let value_token = expression.member().ok()?.value_token().ok()?;
