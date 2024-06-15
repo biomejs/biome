f68bd87a0feb481e82cda869da2d36b40a65f2c9
ryo-ebata <r.ebata@hajimari.inc>
2024-06-15 21:28:39 +0900

    refactor: Improved event handler mappings by using simple arrays and added explanatory comments for better clarity and performance.

--- crates/biome_js_analyze/src/lint/nursery/no_static_element_interactions.rs
+++ crates/biome_js_analyze/src/lint/nursery/no_static_element_interactions.rs
@@ -4,7 +4,6 @@ use biome_analyze::{declare_rule, Rule, RuleDiagnostic, RuleSource};
 use biome_console::markup;
 use biome_js_syntax::jsx_ext::AnyJsxElement;
 use biome_rowan::AstNode;
-use rustc_hash::FxHashMap;
 
 declare_rule! {
     /// Enforce that non-interactive, visible elements (such as `<div>`) that have click handlers use the role attribute.
@@ -52,40 +51,226 @@ declare_rule! {
     }
 }
 
-// These are interactions defined by eslint-plugin-jsx-a11y.
-// ref: https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/974275353598e9407c76bd4a50c331a953755cee/src/rules/no-static-element-interactions.js#L33-L37
-// ref: https://github.com/jsx-eslint/jsx-ast-utils/blob/main/src/eventHandlers.js
-lazy_static::lazy_static! {
-    static ref EVENT_TO_HANDLERS: FxHashMap<&'static str, Vec<&'static str>> = {
-        let mut m = FxHashMap::default();
-        // m.insert("clipboard", vec!["onCopy", "onCut", "onPaste"]);
-        // m.insert("composition", vec!["onCompositionEnd", "onCompositionStart", "onCompositionUpdate"]);
-        m.insert("keyboard", vec!["onKeyDown", "onKeyPress", "onKeyUp"]);
-        m.insert("focus", vec!["onFocus", "onBlur"]);
-        // m.insert("form", vec!["onChange", "onInput", "onSubmit"]);
-        m.insert("mouse", vec![
-            "onClick", "onContextMenu", "onDblClick", "onDoubleClick", "onDrag", "onDragEnd",
-            "onDragEnter", "onDragExit", "onDragLeave", "onDragOver", "onDragStart", "onDrop",
-            "onMouseDown", "onMouseEnter", "onMouseLeave", "onMouseMove", "onMouseOut",
-            "onMouseOver", "onMouseUp"
-        ]);
-        // m.insert("selection", vec!["onSelect"]);
-        // m.insert("touch", vec!["onTouchCancel", "onTouchEnd", "onTouchMove", "onTouchStart"]);
-        // m.insert("ui", vec!["onScroll"]);
-        // m.insert("wheel", vec!["onWheel"]);
-        // m.insert("media", vec![
-        //     "onAbort", "onCanPlay", "onCanPlayThrough", "onDurationChange", "onEmptied",
-        //     "onEncrypted", "onEnded", "onError", "onLoadedData", "onLoadedMetadata", "onLoadStart",
-        //     "onPause", "onPlay", "onPlaying", "onProgress", "onRateChange", "onSeeked", "onSeeking",
-        //     "onStalled", "onSuspend", "onTimeUpdate", "onVolumeChange", "onWaiting"
-        // ]);
-        // m.insert("image", vec!["onLoad", "onError"]);
-        // m.insert("animation", vec!["onAnimationStart", "onAnimationEnd", "onAnimationIteration"]);
-        // m.insert("transition", vec!["onTransitionEnd"]);
-        m
-    };
-}
+// EVENT_TO_HANDLERS is a mapping of event categories to their corresponding event handlers.
+// For example, the "keyboard" category includes handlers like "onKeyDown", "onKeyPress", and "onKeyUp".
+const EVENT_TO_HANDLERS: &[(&str, &[&str])] = &[
+    // (
+    //     // ref: https://developer.mozilla.org/en-US/docs/Web/API/ClipboardEvent
+    //     "clipboard",
+    //     &[
+    //         // ref: https://developer.mozilla.org/en-US/docs/Web/API/Element/copy_event
+    //         "onCopy",
+    //         //ref: https://developer.mozilla.org/en-US/docs/Web/API/Element/cut_event
+    //         "onCut",
+    //         // ref: https://developer.mozilla.org/en-US/docs/Web/API/Element/paste_event
+    //         "onPaste",
+    //     ],
+    // ),
+    // (
+    //     // ref: https://developer.mozilla.org/en-US/docs/Web/API/CompositionEvent
+    //     "composition",
+    //     &[
+    //         // ref: https://developer.mozilla.org/en-US/docs/Web/API/Element/compositionstart_event
+    //         "onCompositionStart",
+    //         // ref: https://developer.mozilla.org/en-US/docs/Web/API/Element/compositionend_event
+    //         "onCompositionEnd",
+    //         // ref: https://developer.mozilla.org/en-US/docs/Web/API/Element/compositionupdate_event
+    //         "onCompositionUpdate",
+    //     ],
+    // ),
+    (
+        // ref https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent
+        "keyboard",
+        &[
+            // ref: https://developer.mozilla.org/en-US/docs/Web/API/Element/keydown_event
+            "onKeyDown",
+            // ref: https://developer.mozilla.org/en-US/docs/Web/API/Element/keyup_event
+            "onKeyUp",
+            //ref: https://developer.mozilla.org/en-US/docs/Web/API/Element/keypress_event
+            "onKeyPress",
+        ],
+    ),
+    (
+        // ref: https://developer.mozilla.org/en-US/docs/Web/API/FocusEvent
+        "focus",
+        &[
+            // ref: https://developer.mozilla.org/en-US/docs/Web/API/Element/focus_event
+            "onFocus",
+            // ref: https://developer.mozilla.org/en-US/docs/Web/API/Element/blur_event
+            "onBlur",
+        ],
+    ),
+    // (
+    //     "form",
+    //     &[
+    //         /// ref: https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/change_event
+    //         "onChange",
+    //         // ref: https://developer.mozilla.org/en-US/docs/Web/API/Element/input_event
+    //         "onInput",
+    //         // https://developer.mozilla.org/en-US/docs/Web/API/HTMLFormElement/submit_event
+    //         "onSubmit",
+    //     ],
+    // ),
+    (
+        // ref: https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent
+        "mouse",
+        &[
+            // ref: https://developer.mozilla.org/en-US/docs/Web/API/Element/click_event
+            "onClick",
+            // ref: https://developer.mozilla.org/en-US/docs/Web/API/Element/contextmenu_event
+            "onContextMenu",
+            // ref: https://developer.mozilla.org/en-US/docs/Web/API/Element/dblclick_event
+            "onDblClick",
+            "onDoubleClick",
+            // ref: https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/drag_event
+            "onDrag",
+            // ref: https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/dragend_event
+            "onDragEnd",
+            // ref: https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/dragenter_event
+            "onDragEnter",
+            // ref: https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/dragleave_event
+            "onDragLeave",
+            "onDragExit",
+            // ref: https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/dragover_event
+            "onDragOver",
+            // ref: https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/dragstart_event
+            "onDragStart",
+            // ref: https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/drop_event
+            "onDrop",
+            // ref: https://developer.mozilla.org/en-US/docs/Web/API/Element/mousedown_event
+            "onMouseDown",
+            // ref: https://developer.mozilla.org/en-US/docs/Web/API/Element/mouseenter_event
+            "onMouseEnter",
+            // ref: https://developer.mozilla.org/en-US/docs/Web/API/Element/mouseleave_event
+            "onMouseLeave",
+            // ref: https://developer.mozilla.org/en-US/docs/Web/API/Element/mousemove_event
+            "onMouseMove",
+            // ref: https://developer.mozilla.org/en-US/docs/Web/API/Element/mouseout_event
+            "onMouseOut",
+            // ref: https://developer.mozilla.org/en-US/docs/Web/API/Element/mouseover_event
+            "onMouseOver",
+            // ref: https://developer.mozilla.org/en-US/docs/Web/API/Element/mouseup_event
+            "onMouseUp",
+        ],
+    ),
+    // (
+    //     "selection",
+    //     &[
+    //         // ref: https://developer.mozilla.org/en-US/docs/Web/API/HTMLInputElement/select_event
+    //         "onSelect",
+    //     ],
+    // ),
+    // (
+    //     // ref: https://developer.mozilla.org/en-US/docs/Web/API/TouchEvent
+    //     "touch",
+    //     &[
+    //         // ref: https://developer.mozilla.org/en-US/docs/Web/API/Element/touchcancel_event
+    //         "onTouchCancel",
+    //         // ref: https://developer.mozilla.org/en-US/docs/Web/API/Element/touchend_event
+    //         "onTouchEnd",
+    //         // ref: https://developer.mozilla.org/en-US/docs/Web/API/Element/touchmove_event
+    //         "onTouchMove",
+    //         // ref: https://developer.mozilla.org/en-US/docs/Web/API/Element/touchstart_event
+    //         "onTouchStart",
+    //     ],
+    // ),
+    // (
+    //     // ref: https://developer.mozilla.org/en-US/docs/Web/API/UIEvent
+    //     "ui",
+    //     &[
+    //         // ref: https://developer.mozilla.org/en-US/docs/Web/API/Element/scroll_event
+    //         "onScroll",
+    //     ],
+    // ),
+    // (
+    //     // ref: https://developer.mozilla.org/en-US/docs/Web/API/WheelEvent
+    //     "wheel",
+    //     &[
+    //         // ref: https://developer.mozilla.org/en-US/docs/Web/API/Element/wheel_event
+    //         "onWheel",
+    //     ],
+    // ),
+    // (
+    //     "media",
+    //     &[
+    //         // ref: https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement/abort_event
+    //         "onAbort",
+    //         // ref: https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement/canplay_event
+    //         "onCanPlay",
+    //         // ref: https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement/canplaythrough_event
+    //         "onCanPlayThrough",
+    //         // ref: https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement/durationchange_event
+    //         "onDurationChange",
+    //         // ref: https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement/emptied_event
+    //         "onEmptied",
+    //         // ref: https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement/encrypted_event
+    //         "onEncrypted",
+    //         // ref: https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement/ended_event
+    //         "onEnded",
+    //         // ref: https://developer.mozilla.org/en-US/docs/Web/API/ErrorEvent
+    //         "onError",
+    //         // ref: https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement/loadeddata_event
+    //         "onLoadedData",
+    //         // ref: https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement/loadedmetadata_event
+    //         "onLoadedMetadata",
+    //         // ref: https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement/loadstart_event
+    //         "onLoadStart",
+    //         // ref: https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement/pause_event
+    //         "onPause",
+    //         // ref: https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement/play_event
+    //         "onPlay",
+    //         // ref: https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement/playing_event
+    //         "onPlaying",
+    //         // ref: https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement/progress_event
+    //         "onProgress",
+    //         // ref: https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement/ratechange_event
+    //         "onRateChange",
+    //         // ref: https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement/seeked_event
+    //         "onSeeked",
+    //         // ref: https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement/seeking_event
+    //         "onSeeking",
+    //         // ref: https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement/stalled_event
+    //         "onStalled",
+    //         // ref: https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement/suspend_event
+    //         "onSuspend",
+    //         // ref: https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement/timeupdate_event
+    //         "onTimeUpdate",
+    //         // ref: https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement/volumechange_event
+    //         "onVolumeChange",
+    //         // ref: https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement/waiting_event
+    //         "onWaiting",
+    //     ],
+    // ),
+    // (
+    //     "image",
+    //     &[
+    //         // ref: https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/load_event
+    //         "onLoad",
+    //     ],
+    // ),
+    // (
+    //     // ref: https://developer.mozilla.org/en-US/docs/Web/API/AnimationEvent
+    //     "animation",
+    //     &[
+    //         // ref: https://developer.mozilla.org/en-US/docs/Web/API/Element/animationstart_event
+    //         "onAnimationStart",
+    //         // ref: https://developer.mozilla.org/en-US/docs/Web/API/Element/animationend_event
+    //         "onAnimationEnd",
+    //         // ref: https://developer.mozilla.org/en-US/docs/Web/API/Element/animationiteration_event
+    //         "onAnimationIteration",
+    //     ],
+    // ),
+    // (
+    //     // ref: https://developer.mozilla.org/en-US/docs/Web/API/TransitionEvent
+    //     "transition",
+    //     &[
+    //         // ref: https://developer.mozilla.org/en-US/docs/Web/API/Element/transitionend_event
+    //         "onTransitionEnd",
+    //     ],
+    // ),
+];
 
+// no-static-element-interactions rule checks only focus, keyboard and mouse categories.
 const CATEGORIES_TO_CHECK: &[&str] = &["focus", "keyboard", "mouse"];
 
 impl Rule for NoStaticElementInteractions {
@@ -103,7 +288,11 @@ impl Rule for NoStaticElementInteractions {
 
         if let Some(attributes_ref) = attributes.as_ref() {
             let has_interactive_handler = CATEGORIES_TO_CHECK.iter().any(|&category| {
-                if let Some(handlers) = EVENT_TO_HANDLERS.get(category) {
+                if let Some(handlers) = EVENT_TO_HANDLERS
+                    .iter()
+                    .find(|&&(cat, _)| cat == category)
+                    .map(|&(_, handlers)| handlers)
+                {
                     handlers.iter().any(|&handler| {
                         if let Some(values) = attributes_ref.get(handler) {
                             values.iter().any(|value| value != "null")
