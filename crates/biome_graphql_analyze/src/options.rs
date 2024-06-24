0c253b66b27168770c8cc8b07c1d8a2e1b6aad87
Emanuele Stoppa <my.burning@gmail.com>
2024-06-24 13:43:06 +0100

    chore(lint): initialise `biome_graphql_analyze` (#3276)

---
+++ crates/biome_graphql_analyze/src/options.rs
@@ -0,0 +1,6 @@
+//! Generated file, do not edit by hand, see `xtask/codegen`
+
+use crate::lint;
+
+pub type UseDummyRule =
+    <lint::nursery::use_dummy_rule::UseDummyRule as biome_analyze::Rule>::Options;
