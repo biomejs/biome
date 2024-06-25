436d44b992dae21813b63c632d3cc9babad55d5a
Arend van Beelen jr <arendjr@gmail.com>
2024-06-25 19:48:38 +0200

    chore(grit): implement node-like compilers + fixes (#3253)

--- crates/biome_grit_patterns/src/grit_context.rs
+++ crates/biome_grit_patterns/src/grit_context.rs
@@ -8,11 +8,13 @@ use crate::grit_target_node::GritTargetNode;
 use crate::grit_tree::GritTargetTree;
 use anyhow::Result;
 use grit_pattern_matcher::context::{ExecContext, QueryContext};
-use grit_pattern_matcher::file_owners::FileOwners;
+use grit_pattern_matcher::file_owners::{FileOwner, FileOwners};
 use grit_pattern_matcher::pattern::{
     CallBuiltIn, GritFunctionDefinition, Pattern, PatternDefinition, PredicateDefinition, State,
 };
-use grit_util::AnalysisLogs;
+use grit_util::{AnalysisLogs, FileOrigin};
+use path_absolutize::Absolutize;
+use std::path::PathBuf;
 
 #[derive(Clone, Debug, PartialEq)]
 pub struct GritQueryContext;
@@ -21,7 +23,7 @@ impl QueryContext for GritQueryContext {
     type Node<'a> = GritTargetNode<'a>;
     type NodePattern = GritNodePattern;
     type LeafNodePattern = GritLeafNodePattern;
-    type ExecContext<'a> = GritExecContext;
+    type ExecContext<'a> = GritExecContext<'a>;
     type Binding<'a> = GritBinding<'a>;
     type CodeSnippet = GritCodeSnippet;
     type ResolvedPattern<'a> = GritResolvedPattern<'a>;
@@ -30,32 +32,47 @@ impl QueryContext for GritQueryContext {
     type Tree<'a> = GritTargetTree;
 }
 
-#[derive(Debug)]
-pub struct GritExecContext {
+pub struct GritExecContext<'a> {
     lang: GritTargetLanguage,
+    loadable_files: &'a [GritTargetFile],
+    files: &'a FileOwners<GritTargetTree>,
+    functions: Vec<GritFunctionDefinition<GritQueryContext>>,
+    patterns: Vec<PatternDefinition<GritQueryContext>>,
+    predicates: Vec<PredicateDefinition<GritQueryContext>>,
 }
 
-impl GritExecContext {
-    pub fn new(lang: GritTargetLanguage) -> Self {
-        Self { lang }
+impl<'a> GritExecContext<'a> {
+    pub fn new(
+        lang: GritTargetLanguage,
+        loadable_files: &'a [GritTargetFile],
+        files: &'a FileOwners<GritTargetTree>,
+    ) -> Self {
+        Self {
+            lang,
+            loadable_files,
+            files,
+            functions: Vec::new(),
+            patterns: Vec::new(),
+            predicates: Vec::new(),
+        }
     }
 }
 
-impl<'a> ExecContext<'a, GritQueryContext> for GritExecContext {
+impl<'a> ExecContext<'a, GritQueryContext> for GritExecContext<'a> {
     fn pattern_definitions(&self) -> &[PatternDefinition<GritQueryContext>] {
-        todo!()
+        &self.patterns
     }
 
     fn predicate_definitions(&self) -> &[PredicateDefinition<GritQueryContext>] {
-        todo!()
+        &self.predicates
     }
 
     fn function_definitions(&self) -> &[GritFunctionDefinition<GritQueryContext>] {
-        todo!()
+        &self.functions
     }
 
     fn ignore_limit_pattern(&self) -> bool {
-        todo!()
+        false
     }
 
     fn call_built_in(
@@ -65,11 +82,11 @@ impl<'a> ExecContext<'a, GritQueryContext> for GritExecContext {
         _state: &mut State<'a, GritQueryContext>,
         _logs: &mut AnalysisLogs,
     ) -> Result<GritResolvedPattern<'a>> {
-        todo!()
+        unimplemented!("built-in functions are still TODO")
     }
 
     fn files(&self) -> &FileOwners<GritTargetTree> {
-        todo!()
+        self.files
     }
 
     fn language(&self) -> &GritTargetLanguage {
@@ -92,10 +109,68 @@ impl<'a> ExecContext<'a, GritQueryContext> for GritExecContext {
 
     fn load_file(
         &self,
-        _file: &<GritQueryContext as QueryContext>::File<'a>,
-        _state: &mut State<'a, GritQueryContext>,
-        _logs: &mut AnalysisLogs,
+        file: &GritFile<'a>,
+        state: &mut State<'a, GritQueryContext>,
+        logs: &mut AnalysisLogs,
     ) -> Result<bool> {
-        todo!()
+        match file {
+            GritFile::Resolved(_) => {
+                // Assume the file is already loaded
+            }
+            GritFile::Ptr(ptr) => {
+                if state.files.is_loaded(ptr) {
+                    return Ok(true);
+                }
+
+                let index = ptr.file;
+                let file = &self.loadable_files[index as usize];
+
+                // TODO: Verify the workspace's maximum file size.
+
+                let file = file_owner_from_matches(&file.path, &file.content, &self.lang, logs)?;
+                if let Some(file) = file {
+                    self.files.push(file);
+                    state.files.load_file(ptr, self.files.last().unwrap());
+                }
+            }
+        }
+        Ok(true)
     }
+}
+
+fn file_owner_from_matches(
+    name: impl Into<PathBuf>,
+    source: &str,
+    language: &GritTargetLanguage,
+    logs: &mut AnalysisLogs,
+) -> Result<Option<FileOwner<GritTargetTree>>> {
+    let name = name.into();
+
+    let Some(tree) = language
+        .get_parser()
+        .parse_file(source, Some(&name), logs, FileOrigin::Fresh)
+    else {
+        return Ok(None);
+    };
+
+    let absolute_path = name.absolutize()?.to_path_buf();
+    Ok(Some(FileOwner {
+        name,
+        absolute_path,
+        tree,
+        matches: Default::default(),
+        new: false,
+    }))
+}
+
+/// Simple wrapper for target files so that we can avoid doing file I/O inside
+/// the Grit engine.
+///
+/// This should suffice as long as we only do single-file queries, but when we
+/// want to support multifile queries, we probably need to implement a solution
+/// that can use the Biome workspace.
+#[derive(Clone, Debug)]
+pub struct GritTargetFile {
+    pub path: PathBuf,
+    pub content: String,
 }
