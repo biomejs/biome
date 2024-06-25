6c83d94fd33f048b9209ec010ec751b382b8f9d8
vohoanglong0107 <vo.hoang.long@outlook.com>
2024-06-25 22:26:34 +0000

    feat(graphql_analyze): useDeprecatedReason

--- crates/biome_graphql_analyze/src/lint/nursery.rs
+++ crates/biome_graphql_analyze/src/lint/nursery.rs
@@ -2,13 +2,13 @@
 
 use biome_analyze::declare_lint_group;
 
-pub mod use_dummy_rule;
+pub mod use_deprecated_reason;
 
 declare_lint_group! {
     pub Nursery {
         name : "nursery" ,
         rules : [
-            self :: use_dummy_rule :: UseDummyRule ,
+            self :: use_deprecated_reason :: UseDeprecatedReason ,
         ]
      }
 }
