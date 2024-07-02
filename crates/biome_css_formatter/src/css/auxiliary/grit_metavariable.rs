9cb8a4fed941f6cdd310200fbe01717c5651b4b4
Zheyu Zhang <zheyuzhang03@gmail.com>
2024-07-02 10:45:46 +0800

    implement metavar formatting

--- crates/biome_css_formatter/src/css/auxiliary/grit_metavariable.rs
+++ crates/biome_css_formatter/src/css/auxiliary/grit_metavariable.rs
@@ -1,10 +1,12 @@
 use crate::prelude::*;
-use biome_css_syntax::CssGritMetavariable;
-use biome_rowan::AstNode;
+use biome_css_syntax::{CssGritMetavariable, CssGritMetavariableFields};
+use biome_formatter::write;
+
 #[derive(Debug, Clone, Default)]
 pub(crate) struct FormatCssGritMetavariable;
 impl FormatNodeRule<CssGritMetavariable> for FormatCssGritMetavariable {
     fn fmt_fields(&self, node: &CssGritMetavariable, f: &mut CssFormatter) -> FormatResult<()> {
-        format_verbatim_node(node.syntax()).fmt(f)
+        let CssGritMetavariableFields { value_token } = node.as_fields();
+        write!(f, [value_token.format()])
     }
 }
