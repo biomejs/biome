8834e26290323a7028e5f43daf49f721fb712736
Denis Bezrukov <6227442+denbezrukov@users.noreply.github.com>
2024-07-04 11:01:36 +0300

    chore(graphql_formatter): performance tests (#3323)

--- xtask/bench/benches/graphql_formatter.rs
+++ xtask/bench/benches/graphql_formatter.rs
@@ -1,5 +1,5 @@
 use std::collections::HashMap;
-use xtask_bench::{bench_parser_group, TestCase};
+use xtask_bench::{bench_formatter_group, TestCase};
 use xtask_bench::{criterion_group, criterion_main, Criterion};
 #[cfg(target_os = "windows")]
 #[global_allocator]
@@ -16,19 +16,19 @@ static GLOBAL: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
 #[cfg(all(target_env = "musl", target_os = "linux", target_arch = "aarch64"))]
 #[global_allocator]
 static GLOBAL: std::alloc::System = std::alloc::System;
-fn bench_graphql_parser(criterion: &mut Criterion) {
+fn bench_graphql_formatter(criterion: &mut Criterion) {
     let mut all_suites = HashMap::new();
     all_suites.insert("graphql", include_str!("libs-graphql.txt"));
     let mut libs = vec![];
     libs.extend(all_suites.values().flat_map(|suite| suite.lines()));
+    let mut group = criterion.benchmark_group("graphql_formatter");
 
-    let mut group = criterion.benchmark_group("graphql_parser");
     for lib in libs {
         let test_case = TestCase::try_from(lib);
 
         match test_case {
             Ok(test_case) => {
-                bench_parser_group(&mut group, test_case);
+                bench_formatter_group(&mut group, test_case);
             }
             Err(e) => println!("{e:?}"),
         }
@@ -36,5 +36,5 @@ fn bench_graphql_parser(criterion: &mut Criterion) {
     group.finish();
 }
 
-criterion_group!(graphql_parser, bench_graphql_parser);
-criterion_main!(graphql_parser);
+criterion_group!(graphql_formatter, bench_graphql_formatter);
+criterion_main!(graphql_formatter);
