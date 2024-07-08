f2b49c0c847db661b187392c4dcd42ba775cb963
Emanuele Stoppa <my.burning@gmail.com>
2024-07-08 08:45:21 +0100

    feat: enable GraphQL linting (#3350)
    Co-authored-by: Ze-Zheng Wu <zezhengwu@proton.me>

--- packages/@biomejs/backend-jsonrpc/src/workspace.ts
+++ packages/@biomejs/backend-jsonrpc/src/workspace.ts
@@ -179,6 +179,7 @@ export interface PartialGraphqlConfiguration {
 	 * GraphQL formatter options
 	 */
 	formatter?: PartialGraphqlFormatter;
+	linter?: PartialGraphqlLinter;
 }
 /**
  * A set of options applied to the JavaScript files
@@ -379,6 +380,15 @@ export interface PartialGraphqlFormatter {
 	 * The type of quotes used in GraphQL code. Defaults to double.
 	 */
 	quoteStyle?: QuoteStyle;
+}
+/**
+ * Options that changes how the GraphQL linter behaves
+ */
+export interface PartialGraphqlLinter {
+	/**
+	 * Control the formatter for GraphQL files.
+	 */
+	enabled?: boolean;
 }
 /**
  * Formatting options specific to the JavaScript files
