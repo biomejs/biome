f80f505351046912149120a264b7a3902f14f9a3
Carson McManus <dyc3@users.noreply.github.com>
2024-07-13 17:15:01 -0400

    feat(yaml): more comprehensive grammar (#3400)

--- crates/biome_yaml_syntax/src/generated/macros.rs
+++ crates/biome_yaml_syntax/src/generated/macros.rs
@@ -16,20 +16,64 @@ macro_rules! map_syntax_node {
     ($ node : expr , $ pattern : pat => $ body : expr) => {
         match $node {
             node => match $crate::YamlSyntaxNode::kind(&node) {
+                $crate::YamlSyntaxKind::YAML_ARRAY => {
+                    let $pattern = unsafe { $crate::YamlArray::new_unchecked(node) };
+                    $body
+                }
+                $crate::YamlSyntaxKind::YAML_ARRAY_INLINE => {
+                    let $pattern = unsafe { $crate::YamlArrayInline::new_unchecked(node) };
+                    $body
+                }
+                $crate::YamlSyntaxKind::YAML_ARRAY_ITEM => {
+                    let $pattern = unsafe { $crate::YamlArrayItem::new_unchecked(node) };
+                    $body
+                }
+                $crate::YamlSyntaxKind::YAML_BLOCK_FOLDED => {
+                    let $pattern = unsafe { $crate::YamlBlockFolded::new_unchecked(node) };
+                    $body
+                }
+                $crate::YamlSyntaxKind::YAML_BLOCK_LITERAL => {
+                    let $pattern = unsafe { $crate::YamlBlockLiteral::new_unchecked(node) };
+                    $body
+                }
+                $crate::YamlSyntaxKind::YAML_BLOCK_VALUE => {
+                    let $pattern = unsafe { $crate::YamlBlockValue::new_unchecked(node) };
+                    $body
+                }
+                $crate::YamlSyntaxKind::YAML_BOOLEAN_VALUE => {
+                    let $pattern = unsafe { $crate::YamlBooleanValue::new_unchecked(node) };
+                    $body
+                }
                 $crate::YamlSyntaxKind::YAML_DOCUMENT => {
                     let $pattern = unsafe { $crate::YamlDocument::new_unchecked(node) };
+                    $body
+                }
+                $crate::YamlSyntaxKind::YAML_IDENTIFIER => {
+                    let $pattern = unsafe { $crate::YamlIdentifier::new_unchecked(node) };
+                    $body
+                }
+                $crate::YamlSyntaxKind::YAML_NULL_VALUE => {
+                    let $pattern = unsafe { $crate::YamlNullValue::new_unchecked(node) };
+                    $body
+                }
+                $crate::YamlSyntaxKind::YAML_NUMBER_VALUE => {
+                    let $pattern = unsafe { $crate::YamlNumberValue::new_unchecked(node) };
+                    $body
+                }
+                $crate::YamlSyntaxKind::YAML_OBJECT => {
+                    let $pattern = unsafe { $crate::YamlObject::new_unchecked(node) };
+                    $body
+                }
+                $crate::YamlSyntaxKind::YAML_OBJECT_MEMBER => {
+                    let $pattern = unsafe { $crate::YamlObjectMember::new_unchecked(node) };
                     $body
                 }
                 $crate::YamlSyntaxKind::YAML_ROOT => {
                     let $pattern = unsafe { $crate::YamlRoot::new_unchecked(node) };
                     $body
                 }
-                $crate::YamlSyntaxKind::YAML_SCALAR => {
-                    let $pattern = unsafe { $crate::YamlScalar::new_unchecked(node) };
-                    $body
-                }
-                $crate::YamlSyntaxKind::YAML_STRING_LITERAL => {
-                    let $pattern = unsafe { $crate::YamlStringLiteral::new_unchecked(node) };
+                $crate::YamlSyntaxKind::YAML_STRING_VALUE => {
+                    let $pattern = unsafe { $crate::YamlStringValue::new_unchecked(node) };
                     $body
                 }
                 $crate::YamlSyntaxKind::YAML_BOGUS => {
@@ -40,12 +84,20 @@ macro_rules! map_syntax_node {
                     let $pattern = unsafe { $crate::YamlBogusValue::new_unchecked(node) };
                     $body
                 }
-                $crate::YamlSyntaxKind::YAML_CONTENT_LIST => {
-                    let $pattern = unsafe { $crate::YamlContentList::new_unchecked(node) };
+                $crate::YamlSyntaxKind::YAML_ARRAY_INLINE_LIST => {
+                    let $pattern = unsafe { $crate::YamlArrayInlineList::new_unchecked(node) };
+                    $body
+                }
+                $crate::YamlSyntaxKind::YAML_ARRAY_ITEM_LIST => {
+                    let $pattern = unsafe { $crate::YamlArrayItemList::new_unchecked(node) };
                     $body
                 }
                 $crate::YamlSyntaxKind::YAML_DOCUMENT_LIST => {
                     let $pattern = unsafe { $crate::YamlDocumentList::new_unchecked(node) };
+                    $body
+                }
+                $crate::YamlSyntaxKind::YAML_OBJECT_MEMBER_LIST => {
+                    let $pattern = unsafe { $crate::YamlObjectMemberList::new_unchecked(node) };
                     $body
                 }
                 _ => unreachable!(),
