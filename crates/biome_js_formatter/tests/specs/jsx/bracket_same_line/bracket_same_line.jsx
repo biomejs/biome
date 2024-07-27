e7529da5325d41187a7cb25f3202d7a0ca857a7d
satojin219 <renzuoteng142@gmail.com>
2024-07-27 21:29:49 +0900

    test: bracket_same_line.jsx

--- crates/biome_js_formatter/tests/specs/jsx/bracket_same_line/bracket_same_line.jsx
+++ crates/biome_js_formatter/tests/specs/jsx/bracket_same_line/bracket_same_line.jsx
@@ -1,17 +1,24 @@
 const a = <div></div>;
 
 <Foo
-    className={style}
-    reallyLongAttributeName1={longComplexValue}
-    reallyLongAttributeName2={anotherLongValue}
+	className={style}
+	reallyLongAttributeName1={longComplexValue}
+	reallyLongAttributeName2={anotherLongValue}
 />;
 
 <Foo
-    className={style}
-    reallyLongAttributeName1={longComplexValue}
-    reallyLongAttributeName2={anotherLongValue}
+		className={style}
+		reallyLongAttributeName1={longComplexValue}
+		reallyLongAttributeName2={anotherLongValue} // this is comment
+/>;
+
+<Foo
+		className={style}
+		reallyLongAttributeName1={longComplexValue}
+		reallyLongAttributeName2={anotherLongValue}
+		// this is comment
 >
-    Hi
+	Hi
 </Foo>;
 
 <div className="hi" />;
