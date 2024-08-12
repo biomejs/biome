05b42b9a8b3a51afb9f5c2c7ba94a2854fbbe2d2
ryo-ebata <r.ebata@hajimari.inc>
2024-08-12 21:28:31 +0900

    copied testcase from eslint-plugin-jsx-a11y

---
+++ crates/biome_js_analyze/tests/specs/nursery/useAriaPropsSupportedByRole/invalid.jsx
@@ -0,0 +1,39 @@
+<>
+<a href="#" aria-checked />
+<area href="#" aria-checked />
+<link href="#" aria-checked />
+<img alt="foobar" aria-checked />
+<menu type="toolbar" aria-checked />
+<aside aria-checked />
+<ul aria-expanded />
+<details aria-expanded />
+<dialog aria-expanded />
+<aside aria-expanded />
+<article aria-expanded />
+<body aria-expanded />
+<li aria-expanded />
+<nav aria-expanded />
+<ol aria-expanded />
+<output aria-expanded />
+<section aria-expanded />
+<tbody aria-expanded />
+<tfoot aria-expanded />
+<thead aria-expanded />
+<input type="radio" aria-invalid />
+<input type="radio" aria-selected />
+<input type="radio" aria-haspopup />
+<input type="checkbox" aria-haspopup />
+<input type="reset" aria-invalid />
+<input type="submit" aria-invalid />
+<input type="image" aria-invalid />
+<input type="button" aria-invalid />
+<menuitem type="command" aria-invalid />
+<menuitem type="radio" aria-selected />
+<menu type="toolbar" aria-haspopup />
+<menu type="toolbar" aria-invalid />
+<menu type="toolbar" aria-expanded />
+<link href="#" aria-invalid />
+<area href="#" aria-invalid />
+<a href="#" aria-invalid />
+<Link href="#" aria-checked />
+</>
