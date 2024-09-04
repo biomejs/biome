d851ed07331e3b7e595e307eab585412820f7ae7
Victorien Elvinger <victorien@elvinger.fr>
2024-09-04 19:00:34 +0200

    fix(useAdjacentOverloadSignatures): fix version

--- crates/biome_js_analyze/src/lint/nursery/use_adjacent_overload_signatures.rs
+++ crates/biome_js_analyze/src/lint/nursery/use_adjacent_overload_signatures.rs
@@ -87,7 +87,7 @@ declare_lint_rule! {
     /// ```
     ///
     pub UseAdjacentOverloadSignatures {
-        version: "1.8.0",
+        version: "next",
         name: "useAdjacentOverloadSignatures",
         language: "js",
         sources: &[
