0c1e86c1f8042c12ecad2cacdfd960c1bb38a611
Carson McManus <carson.mcmanus1@gmail.com>
2024-09-04 10:14:57 -0400

    feat(format/html): impl non-verbatim formatting for directive and element tags

--- crates/biome_html_formatter/src/html/auxiliary/element.rs
+++ crates/biome_html_formatter/src/html/auxiliary/element.rs
@@ -1,10 +1,25 @@
 use crate::prelude::*;
-use biome_html_syntax::HtmlElement;
-use biome_rowan::AstNode;
+use biome_formatter::write;
+use biome_html_syntax::{HtmlElement, HtmlElementFields};
 #[derive(Debug, Clone, Default)]
 pub(crate) struct FormatHtmlElement;
 impl FormatNodeRule<HtmlElement> for FormatHtmlElement {
     fn fmt_fields(&self, node: &HtmlElement, f: &mut HtmlFormatter) -> FormatResult<()> {
-        format_verbatim_node(node.syntax()).fmt(f)
+        let HtmlElementFields {
+            opening_element,
+            children,
+            closing_element,
+        } = node.as_fields();
+
+        write!(
+            f,
+            [
+                opening_element.format(),
+                children.format(),
+                closing_element.format(),
+            ]
+        )?;
+
+        Ok(())
     }
 }
--- crates/biome_html_formatter/src/html/auxiliary/self_closing_element.rs
+++ crates/biome_html_formatter/src/html/auxiliary/self_closing_element.rs
@@ -1,10 +1,28 @@
 use crate::prelude::*;
-use biome_html_syntax::HtmlSelfClosingElement;
-use biome_rowan::AstNode;
+use biome_formatter::write;
+use biome_html_syntax::{HtmlSelfClosingElement, HtmlSelfClosingElementFields};
 #[derive(Debug, Clone, Default)]
 pub(crate) struct FormatHtmlSelfClosingElement;
 impl FormatNodeRule<HtmlSelfClosingElement> for FormatHtmlSelfClosingElement {
     fn fmt_fields(&self, node: &HtmlSelfClosingElement, f: &mut HtmlFormatter) -> FormatResult<()> {
-        format_verbatim_node(node.syntax()).fmt(f)
+        let HtmlSelfClosingElementFields {
+            l_angle_token,
+            name,
+            attributes,
+            slash_token,
+            r_angle_token,
+        } = node.as_fields();
+
+        write!(
+            f,
+            [
+                l_angle_token.format(),
+                name.format(),
+                attributes.format(),
+                soft_line_break_or_space(),
+                slash_token.format(),
+                r_angle_token.format()
+            ]
+        )
     }
 }
