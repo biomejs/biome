647653aa3d03cd214ed1dc19ec47e14fe2650c7d
Carson McManus <dyc3@users.noreply.github.com>
2024-09-10 09:19:32 -0400

    feat(parser/html): emit diagnostics for parsing errors (#3846)

--- crates/biome_html_parser/src/syntax/mod.rs
+++ crates/biome_html_parser/src/syntax/mod.rs
@@ -1,14 +1,9 @@
 mod parse_error;
 
 use crate::parser::HtmlParser;
-use crate::syntax::parse_error::{expected_attribute, expected_child};
+use crate::syntax::parse_error::*;
 use crate::token_source::HtmlLexContext;
-use biome_html_syntax::HtmlSyntaxKind::{
-    HTML_ATTRIBUTE, HTML_ATTRIBUTE_INITIALIZER_CLAUSE, HTML_ATTRIBUTE_LIST, HTML_BOGUS_ELEMENT,
-    HTML_CLOSING_ELEMENT, HTML_CONTENT, HTML_DIRECTIVE, HTML_ELEMENT, HTML_ELEMENT_LIST,
-    HTML_LITERAL, HTML_NAME, HTML_OPENING_ELEMENT, HTML_ROOT, HTML_SELF_CLOSING_ELEMENT,
-    HTML_STRING, HTML_STRING_LITERAL, UNICODE_BOM,
-};
+use biome_html_syntax::HtmlSyntaxKind::*;
 use biome_html_syntax::{HtmlSyntaxKind, T};
 use biome_parser::parse_lists::ParseNodeList;
 use biome_parser::parse_recovery::{ParseRecoveryTokenSet, RecoveryResult};
@@ -59,21 +54,19 @@ fn parse_element(p: &mut HtmlParser) -> ParsedSyntax {
     let m = p.start();
 
     p.bump(T![<]);
-    // TODO: handle error
-    parse_literal(p).ok();
+    parse_literal(p).or_add_diagnostic(p, expected_element_name);
 
     AttributeList.parse_list(p);
 
     if p.at(T![/]) {
         p.bump(T![/]);
-        p.bump(T![>]);
+        p.expect(T![>]);
         Present(m.complete(p, HTML_SELF_CLOSING_ELEMENT))
     } else {
-        p.bump_with_context(T![>], HtmlLexContext::ElementList);
+        p.expect_with_context(T![>], HtmlLexContext::ElementList);
         let opening = m.complete(p, HTML_OPENING_ELEMENT);
         ElementList.parse_list(p);
-        // TODO: handle error
-        parse_closing_element(p).ok();
+        parse_closing_element(p).or_add_diagnostic(p, expected_closing_tag);
         let previous = opening.precede(p);
 
         Present(previous.complete(p, HTML_ELEMENT))
@@ -81,6 +74,9 @@ fn parse_element(p: &mut HtmlParser) -> ParsedSyntax {
 }
 
 fn parse_closing_element(p: &mut HtmlParser) -> ParsedSyntax {
+    if !p.at(T![<]) || !p.nth_at(1, T![/]) {
+        return Absent;
+    }
     let m = p.start();
     p.bump(T![<]);
     p.bump(T![/]);
@@ -112,7 +108,8 @@ impl ParseNodeList for ElementList {
     fn is_at_list_end(&self, p: &mut Self::Parser<'_>) -> bool {
         let at_l_angle0 = p.at(T![<]);
         let at_slash1 = p.nth_at(1, T![/]);
-        at_l_angle0 && at_slash1
+        let at_eof = p.at(EOF);
+        at_l_angle0 && at_slash1 || at_eof
     }
 
     fn recover(
@@ -142,7 +139,7 @@ impl ParseNodeList for AttributeList {
     }
 
     fn is_at_list_end(&self, p: &mut Self::Parser<'_>) -> bool {
-        p.at(T![>]) || p.at(T![/])
+        p.at(T![>]) || p.at(T![/]) || p.at(EOF)
     }
 
     fn recover(
@@ -163,8 +160,7 @@ fn parse_attribute(p: &mut HtmlParser) -> ParsedSyntax {
         return Absent;
     }
     let m = p.start();
-    // TODO: handle error
-    parse_literal(p).ok();
+    parse_literal(p).or_add_diagnostic(p, expected_attribute);
     if p.at(T![=]) {
         parse_attribute_initializer(p).ok();
         Present(m.complete(p, HTML_ATTRIBUTE))
@@ -201,7 +197,6 @@ fn parse_attribute_initializer(p: &mut HtmlParser) -> ParsedSyntax {
     }
     let m = p.start();
     p.bump(T![=]);
-    // TODO: handle error
-    parse_string_literal(p).ok();
+    parse_string_literal(p).or_add_diagnostic(p, expected_initializer);
     Present(m.complete(p, HTML_ATTRIBUTE_INITIALIZER_CLAUSE))
 }
--- crates/biome_html_parser/src/syntax/parse_error.rs
+++ crates/biome_html_parser/src/syntax/parse_error.rs
@@ -10,3 +10,32 @@ pub(crate) fn expected_attribute(p: &HtmlParser, range: TextRange) -> ParseDiagn
 pub(crate) fn expected_child(p: &HtmlParser, range: TextRange) -> ParseDiagnostic {
     expect_one_of(&["element", "text"], range).into_diagnostic(p)
 }
+
+/// The parser was expecting a value for an attribute initializer clause.
+///
+/// ```html
+/// <div id= />
+///         ^ expected initializer
+/// ```
+pub(crate) fn expected_initializer(p: &HtmlParser, range: TextRange) -> ParseDiagnostic {
+    expected_node("initializer", range, p).into_diagnostic(p)
+}
+
+/// The parser encountered a tag that does not have a corresponding closing tag.
+///
+/// ```html
+/// <div>foo
+/// ```
+pub(crate) fn expected_closing_tag(p: &HtmlParser, range: TextRange) -> ParseDiagnostic {
+    expected_node("closing tag", range, p).into_diagnostic(p)
+}
+
+/// The parser was encountered a tag that does not have a name.
+///
+/// ```html
+/// <>
+/// ^ expected element name
+/// ```
+pub(crate) fn expected_element_name(p: &HtmlParser, range: TextRange) -> ParseDiagnostic {
+    expected_node("element name", range, p).into_diagnostic(p)
+}
