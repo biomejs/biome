9543f15cbd3dff1abf2dd8f1f19144ff159f2ea1
kaykdm <kay.kodama43@gmail.com>
2024-09-16 22:41:09 +0900

    Add rule description and examples

--- crates/biome_js_analyze/src/lint/nursery/use_explicit_function_return_type.rs
+++ crates/biome_js_analyze/src/lint/nursery/use_explicit_function_return_type.rs
@@ -1,36 +1,108 @@
-use biome_analyze::{context::RuleContext, declare_lint_rule, Ast, Rule, RuleDiagnostic};
+use biome_analyze::{
+    context::RuleContext, declare_lint_rule, Ast, Rule, RuleDiagnostic, RuleSource,
+};
 use biome_console::markup;
 use biome_js_syntax::JsIdentifierBinding;
 use biome_rowan::AstNode;
 
 declare_lint_rule! {
-    /// Succinct description of the rule.
+    /// Require explicit return types on functions and class methods.
     ///
-    /// Put context and details about the rule.
-    /// As a starting point, you can take the description of the corresponding _ESLint_ rule (if any).
+    /// Functions in TypeScript often don't need to be given an explicit return type annotation.
+    /// Leaving off the return type is less code to read or write and allows the compiler to infer it from the contents of the function.
     ///
-    /// Try to stay consistent with the descriptions of implemented rules.
+    /// However, explicit return types do make it visually more clear what type is returned by a function.
+    /// They can also speed up TypeScript type checking performance in large codebases with many large functions.
+    ///
+    /// This rule enforces that functions do have an explicit return type annotation.
     ///
     /// ## Examples
     ///
     /// ### Invalid
     ///
-    /// ```js,expect_diagnostic
-    /// var a = 1;
-    /// a = 2;
+    /// ```ts,expect_diagnostic
+    /// // Should indicate that no value is returned (void)
+    /// function test() {
+    ///   return;
+    /// }
+    /// ```
+    ///
+    /// ```ts,expect_diagnostic
+    /// // Should indicate that a number is returned
+    /// var fn = function () {
+    ///    return 1;
+    /// };
+    /// ```
+    ///
+    /// ```ts,expect_diagnostic
+    /// // Should indicate that a string is returned
+    /// var arrowFn = () => 'test';
+    /// ```
+    ///
+    /// ```ts,expect_diagnostic
+    /// class Test {
+    ///   // Should indicate that no value is returned (void)
+    ///   method() {
+    ///     return;
+    ///   }
+    /// }
     /// ```
     ///
     /// ### Valid
+    /// ```ts
+    /// // No return value should be expected (void)
+    /// function test(): void {
+    ///   return;
+    /// }
+    /// ```
     ///
-    /// ```js
-    /// // var a = 1;
+    /// ```ts
+    /// // A return value of type number
+    /// var fn = function (): number {
+    ///   return 1;
+    /// }
+    /// ```
+    ///
+    /// ```ts
+    /// // A return value of type string
+    /// var arrowFn = (): string => 'test';
+    /// ```
+    ///
+    /// ```ts
+    /// class Test {
+    ///   // No return value should be expected (void)
+    ///   method(): void {
+    ///     return;
+    ///   }
+    /// }
+    /// ```
+    ///
+    /// ## Options
+    ///
+    /// The rule provides several options that are detailed in the following subsections.
+    ///
+    /// ```json
+    /// {
+    ///     "//": "...",
+    ///     "options": {
+    ///         "allowExpressions": false,
+    ///         "allowTypedFunctionExpressions": true,
+    ///         "allowHigherOrderFunctions": true
+    ///         "allowDirectConstAssertionInArrowFunctions": true
+    ///         "allowConciseArrowFunctionExpressionsStartingWithVoid": false
+    ///         "allowFunctionsWithoutTypeParameters": false
+    ///         "allowedNames": [],
+    ///         "allowIIFEs": false
+    ///     }
+    /// }
     /// ```
     ///
     pub UseExplicitFunctionReturnType {
         version: "next",
         name: "useExplicitFunctionReturnType",
-        language: "js",
+        language: "ts",
         recommended: false,
+        sources: &[RuleSource::EslintTypeScript("explicit-function-return-type")],
     }
 }
 
