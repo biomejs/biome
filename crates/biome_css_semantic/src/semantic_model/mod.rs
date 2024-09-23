a3483e4298213290eedd1df4225e5e15cfcf3972
togami <62130798+togami2864@users.noreply.github.com>
2024-09-24 00:44:15 +0900

    perf(linter): performance improvement for css semantic model (#4044)

--- crates/biome_css_semantic/src/semantic_model/mod.rs
+++ crates/biome_css_semantic/src/semantic_model/mod.rs
@@ -16,7 +16,6 @@ pub fn semantic_model(root: &CssRoot) -> SemanticModel {
     for node in root.preorder() {
         match node {
             biome_css_syntax::WalkEvent::Enter(node) => {
-                builder.push_node(&node);
                 extractor.enter(&node);
             }
             biome_css_syntax::WalkEvent::Leave(node) => extractor.leave(&node),
