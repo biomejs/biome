df43317fae00d7e218109e4cee56a63237d33725
kaioduarte <duartedossantos@gmail.com>
2024-09-24 11:39:50 +0100

    add code fix and cover more scenarios on `noPrototypeBuiltins`

---
+++ crates/biome_js_analyze/tests/specs/suspicious/noPrototypeBuiltins/validPrototype.js
@@ -0,0 +1,15 @@
+foo.call(Object.prototype.hasOwnProperty, Object.prototype.hasOwnProperty.call);
+Object.prototype;
+Object.prototype(obj, prop);
+Object.prototype.hasOwnProperty.call;
+foo.Object.prototype.hasOwnProperty.call(obj, prop);
+foo.prototype.hasOwnProperty.call(obj, prop);
+Object.prototype.foo.call(obj, prop);
+Object.prototype.hasOwnProperty.foo(obj, prop);
+Object.prototype.hasOwnProperty.call.foo(obj, prop);
+Object.prototype.prototype.hasOwnProperty.call(a, b);
+Object.hasOwnProperty.prototype.hasOwnProperty.call(a, b);
+Object.prototype[hasOwnProperty].call(obj, prop);
+Object.prototype.hasOwnProperty[call](obj, prop);
+Object[prototype].hasOwnProperty.call(obj, prop);
+({}).prototype.hasOwnProperty.call(a, b);
