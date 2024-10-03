fe7667d24249fc07dff9f84fe872a469539e3d41
Brandon Ly <brandonly@lostcoding.com>
2024-10-03 18:00:46 -0500

    Reorder tokens

--- crates/biome_grit_formatter/src/grit/patterns/pattern_where.rs
+++ crates/biome_grit_formatter/src/grit/patterns/pattern_where.rs
@@ -14,6 +14,6 @@ impl FormatNodeRule<GritPatternWhere> for FormatGritPatternWhere {
         let pattern_format = pattern.format();
         let side_condition = side_condition.format();
         let where_token = where_token.format();
-        write!(f, [pattern_format, side_condition, where_token,])
+        write!(f, [pattern_format, where_token, side_condition,])
     }
 }
