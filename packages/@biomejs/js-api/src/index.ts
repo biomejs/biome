497a7e1dfa541d892424e311a7f0dc8404b61abf
Anthony Shew <anthonyshew@gmail.com>
2024-10-05 13:45:26 -0600

    WIP

--- packages/@biomejs/js-api/src/index.ts
+++ packages/@biomejs/js-api/src/index.ts
@@ -159,7 +159,7 @@ export class Biome {
 			const biomePath: BiomePath = {
 				path,
 				was_written: false,
-				kind: "Handleable",
+				kind: ["Handleable"],
 			};
 
 			this.workspace.openFile({
@@ -260,6 +260,9 @@ export class Biome {
 						path,
 						fix_file_mode: fixFileMode,
 						should_format: false,
+						only: [],
+						skip: [],
+						rule_categories: ["Syntax", "Lint"],
 					});
 
 					code = result.code;
