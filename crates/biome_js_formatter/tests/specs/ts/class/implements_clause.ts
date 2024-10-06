6d5184c0e9a523be7c00e8abe1f2191dfbe9feb8
mdm317 <dbswh1008@gmail.com>
2024-10-06 14:50:26 +0900

    test: add test case

--- crates/biome_js_formatter/tests/specs/ts/class/implements_clause.ts
+++ crates/biome_js_formatter/tests/specs/ts/class/implements_clause.ts
@@ -2,6 +2,10 @@ class ClassName implements Interface { }
 
 class LongClassName implements Interface1, Interface2, Interface3, Interface4, Interface5 { }
 
+class LongClassName implements InterfaceNameLengthIsSixtySixCharacterssssssssssssssssssssssssssss {
+    constructer(){}
+  }
+
 class LongClassName implements InterfaceNameLengthIsSixtySevenCharactersssssssssssssssssssssssssss {
     constructer(){}
   }
