d7cc01fa8df4d495c8a127e3e4233620814ce399
Brandon Ly <brandonly@lostcoding.com>
2024-10-08 03:23:12 -0500

    feat(format/grit): add basic formatting for where pattern (#4095)
    Co-authored-by: Emanuele Stoppa <my.burning@gmail.com>
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    Co-authored-by: Antony David <antonydavid945@gmail.com>
    Co-authored-by: Victorien Elvinger <victorien@elvinger.fr>
    Co-authored-by: matsuura <79092292+tunamaguro@users.noreply.github.com>

--- crates/biome_grit_formatter/src/grit/predicates/predicate_and.rs
+++ crates/biome_grit_formatter/src/grit/predicates/predicate_and.rs
@@ -1,6 +1,5 @@
 use crate::prelude::*;
 use biome_grit_syntax::GritPredicateAnd;
-use biome_rowan::AstNode;
 #[derive(Debug, Clone, Default)]
 pub(crate) struct FormatGritPredicateAnd;
 impl FormatNodeRule<GritPredicateAnd> for FormatGritPredicateAnd {
