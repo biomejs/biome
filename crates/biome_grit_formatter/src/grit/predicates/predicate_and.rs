935e3351e216b095f01ef6a5b8970f006b264cf0
Brandon Ly <brandonly@lostcoding.com>
2024-10-10 08:38:32 -0500

    Add formatting for predicate assignment and predicate and

--- crates/biome_grit_formatter/src/grit/predicates/predicate_and.rs
+++ crates/biome_grit_formatter/src/grit/predicates/predicate_and.rs
@@ -1,9 +1,28 @@
 use crate::prelude::*;
-use biome_grit_syntax::GritPredicateAnd;
+use biome_formatter::write;
+use biome_grit_syntax::{GritPredicateAnd, GritPredicateAndFields};
+
 #[derive(Debug, Clone, Default)]
 pub(crate) struct FormatGritPredicateAnd;
 impl FormatNodeRule<GritPredicateAnd> for FormatGritPredicateAnd {
     fn fmt_fields(&self, node: &GritPredicateAnd, f: &mut GritFormatter) -> FormatResult<()> {
-        format_verbatim_node(node.syntax()).fmt(f)
+        let GritPredicateAndFields {
+            l_curly_token,
+            and_token,
+            predicates,
+            r_curly_token,
+        } = node.as_fields();
+        write!(
+            f,
+            [
+                l_curly_token.format(),
+                hard_line_break(),
+                and_token.format(),
+                hard_line_break(),
+                soft_block_indent(&predicates.format()),
+                hard_line_break(),
+                r_curly_token.format()
+            ]
+        )
     }
 }
