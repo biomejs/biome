15310fd75b62e32a61c59cf40d9004bed9d95b21
unvalley <kirohi.code@gmail.com>
2024-10-17 04:12:33 +0900

    refactor: use box and simplify variable statement fix

--- crates/biome_js_analyze/src/lint/nursery/no_useless_undefined.rs
+++ crates/biome_js_analyze/src/lint/nursery/no_useless_undefined.rs
@@ -3,16 +3,13 @@ use biome_analyze::{
     RuleSource,
 };
 use biome_console::markup;
-use biome_js_factory::make::{self, js_function_body, js_variable_declarator_list};
+use biome_js_factory::make::{self, js_function_body};
 use biome_js_syntax::{
     AnyJsExpression, AnyJsFunctionBody, JsArrayBindingPatternElement, JsArrowFunctionExpression,
-    JsFormalParameter, JsLanguage, JsObjectBindingPatternShorthandProperty, JsReturnStatement,
-    JsSyntaxToken, JsVariableDeclarator, JsVariableStatement, JsYieldArgument, T,
+    JsFormalParameter, JsObjectBindingPatternShorthandProperty, JsReturnStatement,
+    JsVariableStatement, JsYieldArgument, T,
 };
-use biome_rowan::{
-    chain_trivia_pieces, declare_node_union, AstNode, BatchMutationExt, SyntaxTriviaPiece,
-    TextRange,
-};
+use biome_rowan::{declare_node_union, AstNode, BatchMutationExt, TextRange};
 
 use crate::JsRuleAction;
 
@@ -106,10 +103,15 @@ fn find_undefined_range(expr: Option<&AnyJsExpression>) -> Option<TextRange> {
     }
 }
 
+pub struct RuleState {
+    binding_text: Option<Box<str>>,
+    diagnostic_range: TextRange,
+}
+
 impl Rule for NoUselessUndefined {
     type Query = Ast<AnyUndefinedNode>;
-    type State = (Option<String>, TextRange);
-    type Signals = Vec<Self::State>;
+    type State = RuleState;
+    type Signals = Box<[Self::State]>;
     type Options = ();
 
     fn run(ctx: &RuleContext<Self>) -> Self::Signals {
@@ -120,11 +122,11 @@ impl Rule for NoUselessUndefined {
             // let foo = undefined;
             AnyUndefinedNode::JsVariableStatement(statement) => {
                 let Ok(node) = statement.declaration() else {
-                    return signals;
+                    return signals.into_boxed_slice();
                 };
                 let let_or_var = node.is_let() || node.is_var();
                 if !let_or_var {
-                    return signals;
+                    return signals.into_boxed_slice();
                 }
 
                 for declarator in node.declarators() {
@@ -133,11 +135,14 @@ impl Rule for NoUselessUndefined {
                         continue;
                     };
                     let expr = initializer.expression().ok();
-                    if let Some(range) = find_undefined_range(expr.as_ref()) {
-                        let Some(binding_name) = decl.id().ok().map(|id| id.text()) else {
+                    if let Some(undefined_range) = find_undefined_range(expr.as_ref()) {
+                        let Ok(binding_text) = decl.id() else {
                             continue;
                         };
-                        signals.push((Some(binding_name), range));
+                        signals.push(RuleState {
+                            binding_text: Some(binding_text.text().into()),
+                            diagnostic_range: undefined_range,
+                        })
                     }
                 }
             }
@@ -148,7 +153,10 @@ impl Rule for NoUselessUndefined {
                 if let Some(init) = js_object_binding_pattern_shorthand_property.init() {
                     let expr = init.expression().ok();
                     if let Some(range) = find_undefined_range(expr.as_ref()) {
-                        signals.push((None, range));
+                        signals.push(RuleState {
+                            binding_text: None,
+                            diagnostic_range: range,
+                        });
                     }
                 }
             }
@@ -157,25 +165,34 @@ impl Rule for NoUselessUndefined {
                 if let Some(init) = js_array_binding_pattern_element.init() {
                     let expr = init.expression().ok();
                     if let Some(range) = find_undefined_range(expr.as_ref()) {
-                        signals.push((None, range));
+                        signals.push(RuleState {
+                            binding_text: None,
+                            diagnostic_range: range,
+                        });
                     }
                 }
             }
             // yield undefined
             AnyUndefinedNode::JsYieldArgument(yield_argument) => {
                 if yield_argument.star_token().is_some() {
-                    return signals;
+                    return signals.into_boxed_slice();
                 }
                 let expr = yield_argument.expression().ok();
                 if let Some(range) = find_undefined_range(expr.as_ref()) {
-                    signals.push((None, range));
+                    signals.push(RuleState {
+                        binding_text: None,
+                        diagnostic_range: range,
+                    });
                 }
             }
             // return undefined
             AnyUndefinedNode::JsReturnStatement(js_return_statement) => {
                 let expr = js_return_statement.argument();
                 if let Some(range) = find_undefined_range(expr.as_ref()) {
-                    signals.push((None, range));
+                    signals.push(RuleState {
+                        binding_text: None,
+                        diagnostic_range: range,
+                    });
                 }
             }
             // const noop = () => undefined
@@ -183,7 +200,10 @@ impl Rule for NoUselessUndefined {
                 if let Ok(body) = js_arrow_function_expression.body() {
                     let expr = body.as_any_js_expression();
                     if let Some(range) = find_undefined_range(expr) {
-                        signals.push((None, range));
+                        signals.push(RuleState {
+                            binding_text: None,
+                            diagnostic_range: range,
+                        });
                     }
                 }
             }
@@ -192,20 +212,23 @@ impl Rule for NoUselessUndefined {
                 if let Some(init) = js_formal_parameter.initializer() {
                     let expr = init.expression().ok();
                     if let Some(range) = find_undefined_range(expr.as_ref()) {
-                        signals.push((None, range));
+                        signals.push(RuleState {
+                            binding_text: None,
+                            diagnostic_range: range,
+                        });
                     }
                 }
             }
         };
 
-        signals
+        signals.into_boxed_slice()
     }
 
     fn diagnostic(_ctx: &RuleContext<Self>, state: &Self::State) -> Option<RuleDiagnostic> {
         Some(
             RuleDiagnostic::new(
                 rule_category!(),
-                state.1,
+                state.diagnostic_range,
                 markup! {
                     "Don't use unnecessary "<Emphasis>"undefined"</Emphasis>"."
                 },
@@ -222,70 +245,21 @@ impl Rule for NoUselessUndefined {
 
         match node {
             AnyUndefinedNode::JsVariableStatement(js_variable_statement) => {
-                let assignment_statement = js_variable_statement.clone();
                 let current_declaration_statement = js_variable_statement.declaration().ok()?;
                 let declarators = current_declaration_statement.declarators();
 
-                let current_declaration = declarators
-                    .clone()
-                    .into_iter()
-                    .filter_map(|declarator| declarator.ok())
-                    .find(|decl| {
-                        decl.id()
-                            .is_ok_and(|id| id.text() == state.0.clone().unwrap_or_default())
-                    })?;
+                let current_declaration =
+                    declarators
+                        .into_iter()
+                        .filter_map(Result::ok)
+                        .find(|decl| {
+                            decl.id().is_ok_and(|id| {
+                                id.text() == state.binding_text.as_deref().unwrap_or("")
+                            })
+                        })?;
 
                 let current_initializer = current_declaration.initializer()?;
-
-                let eq_token_trivia = current_initializer
-                    .eq_token()
-                    .map(|token| token.trailing_trivia())
-                    .ok()?
-                    .pieces();
-
-                let expression_trivia = current_initializer
-                    .expression()
-                    .ok()?
-                    .as_js_reference_identifier()
-                    .map(|reference| reference.value_token())?
-                    .ok()?
-                    .trailing_trivia()
-                    .pieces();
-
-                // Save the separators too
-                let separators_syntax = declarators.clone().into_syntax();
-                let separators: Vec<JsSyntaxToken> = separators_syntax.tokens().collect();
-
-                let new_declaration = current_declaration.clone().with_initializer(None);
-                let new_declarators: Vec<JsVariableDeclarator> = declarators
-                    .clone()
-                    .into_iter()
-                    .filter_map(|decl| decl.ok())
-                    .map(|decl| {
-                        if decl == current_declaration {
-                            new_declaration.clone()
-                        } else {
-                            decl
-                        }
-                    })
-                    .collect();
-
-                // Recreate the declaration statement with updated declarators
-                let new_declaration_statement = current_declaration_statement
-                    .with_declarators(js_variable_declarator_list(new_declarators, separators));
-
-                let chained_comments: Vec<SyntaxTriviaPiece<JsLanguage>> =
-                    chain_trivia_pieces(eq_token_trivia, expression_trivia)
-                        .filter(|trivia| trivia.is_comments())
-                        .collect();
-
-                // Create the whole statement using updated subtree and append comments to the statement
-                let new_node = assignment_statement
-                    .clone()
-                    .with_declaration(new_declaration_statement)
-                    .append_trivia_pieces(chained_comments)?;
-
-                mutation.replace_node_discard_trivia(assignment_statement, new_node);
+                mutation.remove_node(current_initializer);
             }
             AnyUndefinedNode::JsObjectBindingPatternShorthandProperty(property) => {
                 mutation.remove_node(property.init()?);
