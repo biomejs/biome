28f45f236213adc101b690052b624de4e0a6fa8b
tunamaguro <tsu7magu6@gmail.com>
2024-10-21 13:33:59 +0000

    doc: add document

--- crates/biome_js_analyze/src/lint/nursery/no_noninteractive_element_interactions.rs
+++ crates/biome_js_analyze/src/lint/nursery/no_noninteractive_element_interactions.rs
@@ -10,9 +10,9 @@ declare_lint_rule! {
     ///
     /// Non-interactive HTML elements indicate _content_ and _containers_ in the user interface.
     /// Non-interactive elements include `<main>`, `<area>`, `<h1>` (,`<h2>`, etc), `<img>`, `<li>`, `<ul>` and `<ol>`.
-    /// 
+    ///
     /// A Non-interactive element does not support event handlers(mouse and key handlers).
-    /// 
+    ///
     ///
     /// ## Examples
     ///
@@ -26,24 +26,38 @@ declare_lint_rule! {
     ///
     /// ```jsx
     /// <button onClick={() => {}}>button</button>
+    /// // Adding a role to element does not add behavior.
+    /// // If not used semantic HTML elements like `button`, developers need to implement the expected behavior for role(like focusability and key press support)
+    /// // See https://www.w3.org/WAI/ARIA/apg/
     /// <div role="button" onClick={() => {}}>button</div>
     /// <div role="presentation" onClick={() => {}}>button</div>
     /// ```
-    /// 
+    ///
     /// ```jsx
     /// // Hidden from screen reader.
     /// <div onClick={() => void 0} role="button" aria-hidden />
     /// ```
-    /// 
+    ///
     /// ```jsx
     /// // Custom component is not checked.
     /// <SomeComponent onClick={()=>{}}>button</SomeComponent>
     /// ```
-    /// 
+    ///
     /// ```jsx
     /// // Spread attributes is not supported.
     /// <div {...{"onClick":()=>{}}}>button</div>
     /// ```
+    /// 
+    /// ## Accessibility guidelines
+    ///
+    /// - [WCAG 4.1.2](https://www.w3.org/WAI/WCAG21/Understanding/name-role-value)
+    ///
+    /// ### Resources
+    ///
+    /// - [WAI-ARIA roles](https://www.w3.org/TR/wai-aria-1.1/#usage_intro)
+    /// - [WAI-ARIA Authoring Practices Guide - Design Patterns and Widgets](https://www.w3.org/TR/wai-aria-practices-1.1/#aria_ex)
+    /// - [Fundamental Keyboard Navigation Conventions](https://www.w3.org/TR/wai-aria-practices-1.1/#kbd_generalnav)
+    /// - [Mozilla Developer Network - ARIA Techniques](https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/ARIA_Techniques/Using_the_button_role#Keyboard_and_focus)
     ///
     pub NoNoninteractiveElementInteractions {
         version: "next",
@@ -54,7 +68,7 @@ declare_lint_rule! {
     }
 }
 
-/// Copy from https://github.com/jsx-eslint/jsx-ast-utils/blob/v3.3.5/src/eventHandlers.js
+/// Ref: https://github.com/jsx-eslint/jsx-ast-utils/blob/v3.3.5/src/eventHandlers.js
 const INTERACTIVE_HANDLERS: &[&str] = &[
     "onClick",
     "onContextMenu",
@@ -91,7 +105,7 @@ fn has_handler_props(attributes: AttributesRef) -> bool {
 /// Check if the element's implicit ARIA semantics have been removed.
 /// See https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Roles/presentation_role
 ///
-/// Copy from https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/v6.10.0/src/util/isPresentationRole.js
+/// Ref: https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/v6.10.0/src/util/isPresentationRole.js
 fn has_presentation_role(attributes: AttributesRef) -> bool {
     const PRESENTATION_ROLE: &[&str] = &["presentation", "none"];
 
@@ -111,7 +125,7 @@ fn has_presentation_role(attributes: AttributesRef) -> bool {
 /// - https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Attributes/aria-hidden
 /// - https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/hidden
 ///
-/// Copy from https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/v6.10.0/src/util/isHiddenFromScreenReader.js
+/// Ref: https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/v6.10.0/src/util/isHiddenFromScreenReader.js
 fn is_hidden_from_screen_reader(element_name: &str, attributes: AttributesRef) -> bool {
     if let Some(attributes) = attributes {
         let aria_hidden = attributes
@@ -132,7 +146,7 @@ fn is_hidden_from_screen_reader(element_name: &str, attributes: AttributesRef) -
 /// Check if the element is `contentEditable`
 /// See https://developer.mozilla.org/en-US/docs/Web/HTML/Global_attributes/contenteditable
 ///
-/// Copy from https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/v6.10.0/src/util/isContentEditable.js
+/// Ref: https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/v6.10.0/src/util/isContentEditable.js
 fn is_content_editable(attributes: AttributesRef) -> bool {
     if let Some(attributes) = attributes {
         if let Some(values) = attributes.get("contentEditable") {
@@ -254,22 +268,18 @@ impl Rule for NoNoninteractiveElementInteractions {
     }
 
     fn diagnostic(ctx: &RuleContext<Self>, _state: &Self::State) -> Option<RuleDiagnostic> {
-        //
-        // Read our guidelines to write great diagnostics:
-        // https://docs.rs/biome_analyze/latest/biome_analyze/#what-a-rule-should-say-to-the-user
-        //
         let node = ctx.query();
         Some(
             RuleDiagnostic::new(
                 rule_category!(),
                 node.range(),
                 markup! {
-                    "Variable is read here."
+                    "Non-interactive element should not have event handler."
                 },
             )
             .note(markup! {
-                "This note will give you more information."
-            }),
+                "Consider replace semantically interactive element("<Emphasis>"<button/>"</Emphasis>","<Emphasis>"<a href/>"</Emphasis>")."
+            })
         )
     }
 }
