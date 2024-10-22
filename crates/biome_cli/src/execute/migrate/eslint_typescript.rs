sepruko/biomejs-biome

refactor: add deserilization context

crates/biome_cli/src/execute/migrate/eslint_typescript.rs
@@ -499,9 +499,9 @@ impl NamingConventionSelection {
 pub(crate) struct Anything;
 impl Deserializable for Anything {
     fn deserialize(
+        _ctx: &mut impl biome_deserialize::DeserializableContext,
         _value: &impl biome_deserialize::DeserializableValue,
         _name: &str,
-        _diagnostics: &mut Vec<biome_deserialize::DeserializationDiagnostic>,
     ) -> Option<Self> {
         Some(Anything)
     }

crates/biome_deserialize/src/validator.rs
@@ -1,4 +1,4 @@
-use crate::DeserializationDiagnostic;
+use crate::{DeserializableContext, DeserializationDiagnostic};
 use biome_console::markup;
 use biome_rowan::TextRange;
 
@@ -14,21 +14,21 @@ pub trait DeserializableValidator {
     /// should be rejected.
     fn validate(
         &mut self,
+        ctx: &mut impl DeserializableContext,
         name: &str,
         range: TextRange,
-        diagnostics: &mut Vec<DeserializationDiagnostic>,
     ) -> bool;
 }
 
 /// Validates whether the given value is non-empty.
 pub fn non_empty<T: IsEmpty>(
+    ctx: &mut impl DeserializableContext,
     value: &T,
     name: &str,
     range: TextRange,
-    diagnostics: &mut Vec<DeserializationDiagnostic>,
 ) -> bool {
     if value.is_empty() {
-        diagnostics.push(
+        ctx.report(
             DeserializationDiagnostic::new(markup! {
                 <Emphasis>{name}</Emphasis>" may not be empty"
             })

crates/biome_js_analyze/src/lint/correctness/use_exhaustive_dependencies.rs
@@ -3,7 +3,9 @@ use crate::services::semantic::Semantic;
 use biome_analyze::RuleSource;
 use biome_analyze::{context::RuleContext, declare_lint_rule, Rule, RuleDiagnostic};
 use biome_console::markup;
-use biome_deserialize::{non_empty, DeserializableValidator, DeserializationDiagnostic};
+use biome_deserialize::{
+    non_empty, DeserializableContext, DeserializableValidator, DeserializationDiagnostic,
+};
 use biome_deserialize_macros::Deserializable;
 use biome_js_semantic::{Capture, SemanticModel};
 use biome_js_syntax::{
@@ -354,15 +356,15 @@ pub struct Hook {
 impl DeserializableValidator for Hook {
     fn validate(
         &mut self,
+        ctx: &mut impl DeserializableContext,
         _name: &str,
         range: biome_rowan::TextRange,
-        diagnostics: &mut Vec<DeserializationDiagnostic>,
     ) -> bool {
         match (self.closure_index, self.dependencies_index) {
             (Some(closure_index), Some(dependencies_index))
                 if closure_index == dependencies_index =>
             {
-                diagnostics.push(
+                ctx.report(
                     DeserializationDiagnostic::new(markup! {
                         <Emphasis>"closureIndex"</Emphasis>" and "<Emphasis>"dependenciesIndex"</Emphasis>" may not be the same"
                     })

biomejs/biome

crates/biome_cli/src/execute/migrate/eslint_typescript.rs
@@ -499,9 +499,9 @@ impl NamingConventionSelection {
 pub(crate) struct Anything;
 impl Deserializable for Anything {
     fn deserialize(
+        _ctx: &mut impl biome_deserialize::DeserializationContext,
         _value: &impl biome_deserialize::DeserializableValue,
         _name: &str,
-        _diagnostics: &mut Vec<biome_deserialize::DeserializationDiagnostic>,
     ) -> Option<Self> {
         Some(Anything)
     }

crates/biome_deserialize/src/validator.rs
@@ -1,4 +1,4 @@
-use crate::DeserializationDiagnostic;
+use crate::{DeserializationContext, DeserializationDiagnostic};
 use biome_console::markup;
 use biome_rowan::TextRange;
 
@@ -14,21 +14,21 @@ pub trait DeserializableValidator {
     /// should be rejected.
     fn validate(
         &mut self,
+        ctx: &mut impl DeserializationContext,
         name: &str,
         range: TextRange,
-        diagnostics: &mut Vec<DeserializationDiagnostic>,
     ) -> bool;
 }
 
 /// Validates whether the given value is non-empty.
 pub fn non_empty<T: IsEmpty>(
+    ctx: &mut impl DeserializationContext,
     value: &T,
     name: &str,
     range: TextRange,
-    diagnostics: &mut Vec<DeserializationDiagnostic>,
 ) -> bool {
     if value.is_empty() {
-        diagnostics.push(
+        ctx.report(
             DeserializationDiagnostic::new(markup! {
                 <Emphasis>{name}</Emphasis>" may not be empty"
             })

crates/biome_js_analyze/src/lint/correctness/use_exhaustive_dependencies.rs
@@ -3,7 +3,9 @@ use crate::services::semantic::Semantic;
 use biome_analyze::RuleSource;
 use biome_analyze::{context::RuleContext, declare_lint_rule, Rule, RuleDiagnostic};
 use biome_console::markup;
-use biome_deserialize::{non_empty, DeserializableValidator, DeserializationDiagnostic};
+use biome_deserialize::{
+    non_empty, DeserializableValidator, DeserializationContext, DeserializationDiagnostic,
+};
 use biome_deserialize_macros::Deserializable;
 use biome_js_semantic::{Capture, SemanticModel};
 use biome_js_syntax::{
@@ -354,15 +356,15 @@ pub struct Hook {
 impl DeserializableValidator for Hook {
     fn validate(
         &mut self,
+        ctx: &mut impl DeserializationContext,
         _name: &str,
         range: biome_rowan::TextRange,
-        diagnostics: &mut Vec<DeserializationDiagnostic>,
     ) -> bool {
         match (self.closure_index, self.dependencies_index) {
             (Some(closure_index), Some(dependencies_index))
                 if closure_index == dependencies_index =>
             {
-                diagnostics.push(
+                ctx.report(
                     DeserializationDiagnostic::new(markup! {
                         <Emphasis>"closureIndex"</Emphasis>" and "<Emphasis>"dependenciesIndex"</Emphasis>" may not be the same"
                     })
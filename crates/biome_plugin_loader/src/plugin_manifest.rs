118dd26c3ed93a80c9d69e4517fb48e1dcfabf2a
Victorien Elvinger <victorien@elvinger.fr>
2024-10-02 16:47:21 +0200

    refactor: add deserilization context

--- crates/biome_plugin_loader/src/plugin_manifest.rs
+++ crates/biome_plugin_loader/src/plugin_manifest.rs
@@ -1,5 +1,5 @@
 use biome_console::markup;
-use biome_deserialize::DeserializationDiagnostic;
+use biome_deserialize::{DeserializationContext, DeserializationDiagnostic};
 use biome_deserialize_macros::Deserializable;
 use biome_rowan::TextRange;
 
@@ -15,15 +15,15 @@ pub struct PluginManifest {
 
 // There's only one manifest version now.
 pub fn supported_version(
+    ctx: &mut impl DeserializationContext,
     value: &u8,
     name: &str,
     range: TextRange,
-    diagnostics: &mut Vec<DeserializationDiagnostic>,
 ) -> bool {
     if *value == 1 {
         true
     } else {
-        diagnostics.push(
+        ctx.report(
             DeserializationDiagnostic::new(markup! {
                 <Emphasis>{name}</Emphasis>" must be 1"
             })
