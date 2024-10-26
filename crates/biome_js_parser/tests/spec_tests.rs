6c209ef7f4c60a1d04acbf3b7ae5730730e00e64
Emanuele Stoppa <my.burning@gmail.com>
2024-10-26 07:58:18 +0100

    test: use `insta` for JS parser (#4386)

---
+++ crates/biome_js_parser/tests/spec_tests.rs
@@ -0,0 +1,10 @@
+#![allow(non_snake_case)]
+
+mod spec_test;
+
+mod ok {
+    tests_macros::gen_tests! {"tests/js_test_suite/ok/**/*.{js,cjs,mjs,jsx,ts,tsx,d.ts}", crate::spec_test::run, "ok"}
+}
+mod err {
+    tests_macros::gen_tests! {"tests/js_test_suite/error/**/*.{js,cjs,mjs,jsx,ts,tsx,d.ts}", crate::spec_test::run, "error"}
+}
