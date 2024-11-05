043cd037bf7a5614b9b4367af1ae735e7ddb9c23
Emanuele Stoppa <my.burning@gmail.com>
2024-11-05 16:13:52 +0000

    feat(grit): add formatting of more nodes (#4472)

--- crates/biome_grit_formatter/src/grit/auxiliary/sequential.rs
+++ crates/biome_grit_formatter/src/grit/auxiliary/sequential.rs
@@ -1,10 +1,29 @@
 use crate::prelude::*;
 use biome_grit_syntax::GritSequential;
-use biome_rowan::AstNode;
+
 #[derive(Debug, Clone, Default)]
 pub(crate) struct FormatGritSequential;
 impl FormatNodeRule<GritSequential> for FormatGritSequential {
     fn fmt_fields(&self, node: &GritSequential, f: &mut GritFormatter) -> FormatResult<()> {
         format_verbatim_node(node.syntax()).fmt(f)
+
+        // TODO: investigate the verbatim panic when this code runs
+        // let GritSequentialFields {
+        //     l_curly_token,
+        //     sequential_token,
+        //     sequential,
+        //     r_curly_token,
+        // } = node.as_fields();
+        //
+        // write!(
+        //     f,
+        //     [
+        //         sequential_token.format(),
+        //         space(),
+        //         l_curly_token.format(),
+        //         sequential.format(),
+        //         r_curly_token.format()
+        //     ]
+        // )
     }
 }
--- crates/biome_grit_formatter/src/grit/predicates/curly_predicate_list.rs
+++ crates/biome_grit_formatter/src/grit/predicates/curly_predicate_list.rs
@@ -1,10 +1,24 @@
 use crate::prelude::*;
-use biome_grit_syntax::GritCurlyPredicateList;
-use biome_rowan::AstNode;
+use biome_formatter::write;
+use biome_grit_syntax::{GritCurlyPredicateList, GritCurlyPredicateListFields};
+
 #[derive(Debug, Clone, Default)]
 pub(crate) struct FormatGritCurlyPredicateList;
 impl FormatNodeRule<GritCurlyPredicateList> for FormatGritCurlyPredicateList {
     fn fmt_fields(&self, node: &GritCurlyPredicateList, f: &mut GritFormatter) -> FormatResult<()> {
-        format_verbatim_node(node.syntax()).fmt(f)
+        let GritCurlyPredicateListFields {
+            predicates,
+            r_curly_token,
+            l_curly_token,
+        } = node.as_fields();
+
+        write!(
+            f,
+            [
+                l_curly_token.format(),
+                predicates.format(),
+                r_curly_token.format()
+            ]
+        )
     }
 }
