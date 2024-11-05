42da1ebb5a63e8fca83dcc2acbfb059565d16ea7
Brandon Ly <brandonly@lostcoding.com>
2024-11-04 19:31:02 -0600

    Fill lines and add new line if needed

--- crates/biome_grit_formatter/src/grit/auxiliary/list.rs
+++ crates/biome_grit_formatter/src/grit/auxiliary/list.rs
@@ -1,5 +1,5 @@
 use crate::prelude::*;
-use biome_formatter::write;
+use biome_formatter::{format_args, write};
 use biome_grit_syntax::{GritList, GritListFields};
 
 #[derive(Debug, Clone, Default)]
@@ -12,13 +12,21 @@ impl FormatNodeRule<GritList> for FormatGritList {
             patterns,
             r_brack_token,
         } = node.as_fields();
+
+        let should_expand = f.comments().has_dangling_comments(node.syntax());
+
         write!(
             f,
             [
                 l_brack_token.format(),
                 name.format(),
-                patterns.format(),
-                r_brack_token.format(),
+                group(&soft_block_indent(&format_args![
+                    patterns.format(),
+                    format_dangling_comments(node.syntax())
+                ]))
+                .should_expand(should_expand),
+                line_suffix_boundary(),
+                r_brack_token.format()
             ]
         )
     }
--- crates/biome_grit_formatter/src/grit/lists/list_pattern_list.rs
+++ crates/biome_grit_formatter/src/grit/lists/list_pattern_list.rs
@@ -1,4 +1,5 @@
 use crate::prelude::*;
+use biome_formatter::write;
 use biome_grit_syntax::GritListPatternList;
 
 #[derive(Debug, Clone, Default)]
@@ -6,12 +7,21 @@ pub(crate) struct FormatGritListPatternList;
 impl FormatRule<GritListPatternList> for FormatGritListPatternList {
     type Context = GritFormatContext;
     fn fmt(&self, node: &GritListPatternList, f: &mut GritFormatter) -> FormatResult<()> {
-        let mut joiner = f.fill();
+        let mut filler = f.fill();
 
-        for (_, formatted) in node.elements().zip(node.format_separated(",")) {
-            joiner.entry(&soft_line_break_or_space(), &formatted);
+        for (element, formatted) in node.iter().zip(node.format_separated(",")) {
+            filler.entry(
+                &format_once(|f| {
+                    if get_lines_before(element?.syntax()) > 0 {
+                        write!(f, [empty_line()])
+                    } else {
+                        write!(f, [soft_line_break_or_space()])
+                    }
+                }),
+                &formatted,
+            );
         }
 
-        joiner.finish()
+        filler.finish()
     }
 }
