c0cd5f20eedfa0c8b6ba6db903e1866bbad549e0
eryue0220 <eryue0220@gmail.com>
2024-11-06 16:02:01 +0800

    fix: update fix logic

--- crates/biome_css_formatter/src/css/properties/generic_property.rs
+++ crates/biome_css_formatter/src/css/properties/generic_property.rs
@@ -12,6 +12,47 @@ impl FormatNodeRule<CssGenericProperty> for FormatCssGenericProperty {
             value,
         } = node.as_fields();
 
+        let has_comma = value
+            .iter()
+            .any(|v| v.text().eq(","));
+
+        if has_comma {
+            write!(
+                f,
+                [
+                    name.format(),
+                    colon_token.format(),
+                    indent(&format_with(|f| {
+                        let last = value.last().unwrap();
+
+                        for (idx, v) in value.iter().enumerate() {
+                            if idx == 0 {
+                                write!(f, [soft_line_break_or_space()])?;
+                            }
+
+                            if v.text().eq(",") {
+                                write!(f, [v.format(), hard_line_break()])?;
+                            } else {
+                                if let Some(next) = value.iter().nth(idx + 1) {
+                                    if next.text().eq(",") || v == last {
+                                        write!(f, [v.format()])?;
+                                    } else {
+                                        write!(f, [v.format(), space()])?;
+                                    }
+                                } else {
+                                    write!(f, [v.format()])?;
+                                }
+                            }
+                        }
+
+                        Ok(())
+                    })),
+                ]
+            )?;
+
+            return Ok(());
+        }
+
         write!(
             f,
             [name.format(), colon_token.format(), space(), value.format()]
