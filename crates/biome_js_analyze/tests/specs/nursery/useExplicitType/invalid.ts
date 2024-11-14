c840c35db636cdc03b4c4b271ac5eea015f4c61f
kaykdm <kay.kodama43@gmail.com>
2024-11-14 19:52:30 +0900

    feat(useExplicitType): implement support for methods and functions without return types in interfaces, abstract classes, and modules

--- crates/biome_js_analyze/tests/specs/nursery/useExplicitType/invalid.ts
+++ crates/biome_js_analyze/tests/specs/nursery/useExplicitType/invalid.ts
@@ -29,67 +29,95 @@ class Test {
 
 const obj = {
 	method() {
-		return "test"
-	}
-}
-
-const obj = {
-  get method() {
-    return "test"
-  },
+		return "test";
+	},
 };
 
-const func = (value: number) => ({ type: 'X', value }) as any;
-const func = (value: number) => ({ type: 'X', value }) as Action;
+const obj = {
+	get method() {
+		return "test";
+	},
+};
+
+const func = (value: number) => ({ type: "X", value }) as any;
+const func = (value: number) => ({ type: "X", value }) as Action;
 
 export default () => {};
 export default function () {}
 
 // check higher order functions
 const arrowFn = () => () => {};
-const arrowFn = () => function() {}
+const arrowFn = () => function () {};
 const arrowFn = () => {
-  return () => { };
-}
+	return () => {};
+};
 
 // does not support detecting a return of a function inside other statements like if, switch, etc.
-// we check only the first statment 
+// we check only the first statment
 const arrowFn = (a: number) => {
-  if (a === 1) {
-    return (): void => { };
-  } else {
-    return (): number => {
-      return a + 2
-    }
-  }
-}
+	if (a === 1) {
+		return (): void => {};
+	} else {
+		return (): number => {
+			return a + 2;
+		};
+	}
+};
 const arrowFn = (a: number) => {
-  switch (a) {
-    case 1: {
-      return (): void => { };
-    }
-    case 2: {
-      return (): void => { };
-    }
-    default: {
-      return (): void => { };
-    }
-  }
-}
+	switch (a) {
+		case 1: {
+			return (): void => {};
+		}
+		case 2: {
+			return (): void => {};
+		}
+		default: {
+			return (): void => {};
+		}
+	}
+};
 
 function f() {
-  if (x) {
-    return 0;
-  }
-  return (): void => {}
+	if (x) {
+		return 0;
+	}
+	return (): void => {};
 }
 
 function fn() {
-  let str = "hey";
-  return function (): string {
-      return str;
-  };
+	let str = "hey";
+	return function (): string {
+		return str;
+	};
 }
 
-const x = { prop: () => {} }
-const x = { bar: { prop: () => {} } }
+const x = { prop: () => {} };
+const x = { bar: { prop: () => {} } };
+
+interface Array<Type> {
+	pop(): Type | undefined;
+	push(...items: Type[]): number;
+	method();
+}
+
+type MyObject = {
+	(input: string);
+	propertyName: string;
+};
+
+abstract class MyClass {
+	public abstract method();
+}
+
+abstract class P<T> {
+	abstract method(): T;
+	abstract get poke();
+}
+
+declare namespace myLib {
+	function makeGreeting(s: string);
+}
+
+declare module "foo" {
+	export default function bar();
+}
