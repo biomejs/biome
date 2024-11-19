15768df19a5c9db88bfb0804cc3d3b8e7e69ea8f
Lukas Waslowski <cr7pt0gr4ph7@gmail.com>
2024-11-19 11:45:03 +0000

    docs(noRestrictedImport): extend rule documentation

--- crates/biome_js_analyze/src/lint/nursery/no_restricted_imports.rs
+++ crates/biome_js_analyze/src/lint/nursery/no_restricted_imports.rs
@@ -23,30 +23,20 @@ use serde::{Deserialize, Serialize};
 declare_lint_rule! {
     /// Disallow specified modules when loaded by import or require.
     ///
-    /// ## Options
+    /// ## Examples
     ///
     /// ```json,options
     /// {
-    ///     "noRestrictedImports": {
-    ///         "options": {
-    ///             "paths": {
-    ///                 "lodash": "Using lodash is not encouraged",
-    ///                 "underscore": "Using underscore is not encouraged",
-    ///                 "import-foo": {
-    ///                     "importNames": ["Bar"],
-    ///                     "message": "Please use Bar from /import-bar/baz/ instead."
-    ///                 },
-    ///                 "import-bar": {
-    ///                   "allowImportNames": ["Bar"],
-    ///                   "message": "Please use only Bar from import-bar."
-    ///                 }
-    ///             }
+    ///     "options": {
+    ///         "paths": {
+    ///             "lodash": "Using lodash is not encouraged.",
+    ///             "underscore": "",
+    ///             "import-foo": { "importNames": ["Bar"] },
+    ///             "import-bar": { "allowImportNames": ["Bar"] }
     ///         }
     ///     }
     /// }
     /// ```
-    ///
-    /// ## Examples
     ///
     /// ### Invalid
     ///
@@ -69,6 +59,221 @@ declare_lint_rule! {
     /// import "allowed-import";
     /// const myImport = await import("allowed-import");
     /// const myImport = require("allowed-import");
+    /// ```
+    ///
+    /// ## Supported Import Syntaxes
+    ///
+    /// The rule tries to parse the context of the import to see if only one or more
+    /// of the allowed import names have been imported from a given module.
+    ///
+    /// All of the following import syntaxes are supported:
+    ///
+    /// ### Static `import` declaration
+    ///
+    /// Normal static [ESM `import` declarations](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/import) are supported:
+    ///
+    /// ```js
+    /// // Static `import` declaration:
+    /// // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/import
+    ///
+    /// import "sideeffect-import";
+    /// import * as alias1 from "namespace-import";
+    /// import { export1, export2 as alias2, "string-name" as alias3, default as defaultExport /* … */ } from "named-import";
+    /// import defaultExport from "default-import";
+    /// import defaultExport, * as alias5 from "default+namespace-import";
+    /// import defaultExport, { export1 /* … */ } from "default+named-import";
+    /// ```
+    ///
+    /// The TypeScript-specific [type-only imports](https://www.typescriptlang.org/docs/handbook/modules/reference.html#type-only-imports-and-exports) are also supported:
+    ///
+    /// ```ts
+    /// // TypeScript-specific type-only `import` declaration:
+    /// // https://www.typescriptlang.org/docs/handbook/modules/reference.html#type-only-imports-and-exports
+    ///
+    /// import { type export1, type export2 as alias2, type "string-name" as alias3, type default as defaultExport /* … */ } from "named-import";
+    /// import type { export1, export2 as alias2, "string-name" as alias3, default as defaultExport /* … */ } from "named-import";
+    /// import type defaultExport from "default-import";
+    /// ```
+    ///
+    /// ### Dynamic `import()` calls
+    ///
+    /// Dynamic [ESM `import()` calls](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/import) are also supported.
+    /// Because the import is performed at runtime, it is not always possible to determine which import names are being used.
+    /// Nevertheless, the rule tries to detect the following common usage patterns where the set of imported names is determined statically:
+    ///
+    /// ```js
+    /// // Dynamic `import()` calls:
+    /// // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/import
+    ///
+    /// import('sideeffect-import');
+    /// await import('sideeffect-import');
+    ///
+    /// // ...using await + destructuring-assignment:
+    /// const alias1 = await import('namespace-import');
+    /// const { default: defaultExport } = await import('default-import')
+    /// const { export1, export2: alias2, "string-name": alias3, default: defaultExport /* … */ } = await import("named-import");
+    ///
+    /// // ...using then() with arrow-function + destructuring parameters:
+    /// import('namespace-import').then(alias1 => { /* … */ });
+    /// import('namespace-import').then((alias1) => { /* … */ });
+    /// import('default-import').then(({ default: defaultExport }) => { /* … */ });
+    /// import('named-import').then(({ export1, export2: alias2, "string-name": alias3, default: defaultExport /* … */ }) => { /* … */ });
+    ///
+    /// // ...using then() with function + destructuring parameters:
+    /// import('namespace-import').then(function(alias1) { /* … */ });
+    /// import('default-import').then(function({ default: defaultExport }) { /* … */ });
+    /// import('named-import').then(function({ export1, export2: alias2, "string-name": alias3, default: defaultExport /* … */ }) { /* … */ });
+    ///
+    /// // Standalone `import('...')` calls that appear in some other
+    /// // unrecognized context will be treated as a namespace import,
+    /// // because the return value of `import('...')` is a namespace object:
+    ///
+    /// myFunction(...args, import("namespace-import"), ...args)
+    /// ```
+    ///
+    /// ### Dynamic `require()` calls
+    ///
+    /// NodeJS-style `require()` calls are also supported.
+    /// Due to the way `require()` works, these are always treated as default imports.
+    ///
+    /// ```js
+    /// // Dynamic `require()` call
+    /// const defaultExport = require('default-import');
+    /// ```
+    ///
+    /// ## Options
+    ///
+    /// ```json,options
+    /// {
+    ///     "options": {
+    ///         "paths": {
+    ///             "lodash": "Using lodash is not encouraged",
+    ///             "underscore": "Using underscore is not encouraged",
+    ///             "import-foo": {
+    ///                 "importNames": ["Bar"],
+    ///                 "message": "Please use Bar from /import-bar/baz/ instead."
+    ///             },
+    ///             "import-bar": {
+    ///               "allowImportNames": ["Bar"],
+    ///               "message": "Please use only Bar from import-bar."
+    ///             }
+    ///         }
+    ///     }
+    /// }
+    /// ```
+    ///
+    /// ### `paths`
+    ///
+    /// Specifies the import paths that are either wholly or partially restricted.
+    ///
+    /// ### `paths.<import>`
+    ///
+    /// The message be shown when this import is used and/or additional options that configure which import names are restricted.
+    ///
+    /// Can be either a `"message string"` or an object with one or more of the following properties:
+    ///
+    /// ### `paths.<import>.message`
+    ///
+    /// Specifies the message to be shown when the restricted import is used.
+    ///
+    /// ```json,options
+    /// {
+    ///     "options": {
+    ///         "paths": {
+    ///             "import-foo": {
+    ///                 "message": "Please use import-bar instead."
+    ///             }
+    ///         }
+    ///     }
+    /// }
+    /// ```
+    ///
+    /// ```js,expect_diagnostic,use_options
+    /// import { export1 } from 'import-foo';
+    /// ```
+    ///
+    /// A default message will be generated if `message` is empty or not specified.
+    ///
+    /// ```json,options
+    /// {
+    ///     "options": {
+    ///         "paths": {
+    ///             "import-foo": { }
+    ///         }
+    ///     }
+    /// }
+    /// ```
+    ///
+    /// ```js,expect_diagnostic,use_options
+    /// import { export1 } 'import-foo';
+    /// ```
+    ///
+    /// ### `paths.<import>.importNames`
+    ///
+    /// Specifies the array of import names that should be explicitly forbidden.
+    /// The following import name specifiers are supported:
+    ///
+    /// - **Named import:** `"someIdentifier"` (`import { someIdentifier } from 'named-import'`)
+    /// - **Default import:** `"default"` (`import defaultExport from 'default-import'`)
+    /// - **Namespace import:** `"*"` (`import * as alias1 from 'namespace-import'`)
+    /// - **Sideeffect/Bare import:** `""` (`import "sideeffect-import"`)
+    ///
+    /// **Only one of `importNames` and `allowImportNames` must be specified.**
+    ///
+    /// ```json,options
+    /// {
+    ///     "options": {
+    ///         "paths": {
+    ///             "import-foo": {
+    ///                 "importNames": ["Bar"],
+    ///                 "message": "Please use Bar from /import-bar/baz/ instead."
+    ///             }
+    ///         }
+    ///     }
+    /// }
+    /// ```
+    ///
+    /// #### Invalid
+    ///
+    /// ```js,expect_diagnostic,use_options
+    /// import { Bar } from 'import-foo';
+    /// ```
+    ///
+    /// #### Valid
+    ///
+    /// ```js,use_options
+    /// import { Foo } from 'import-foo';
+    /// ```
+    ///
+    /// ### `paths.<import>.allowImportNames`
+    ///
+    /// Specifies the set of import names that should be explicitly allowed.
+    /// See `importNames` for the set of supported import name specifiers.
+    ///
+    /// **Only one of `importNames` and `allowImportNames` must be specified.**
+    ///
+    /// ```json,options
+    /// {
+    ///     "options": {
+    ///         "paths": {
+    ///             "import-bar": {
+    ///               "allowImportNames": ["Bar"]
+    ///             }
+    ///         }
+    ///     }
+    /// }
+    /// ```
+    ///
+    /// #### Invalid
+    ///
+    /// ```js,expect_diagnostic,use_options
+    /// import { Baz } from 'import-bar';
+    /// ```
+    ///
+    /// #### Valid
+    ///
+    /// ```js,use_options
+    /// import { Bar } from 'import-bar';
     /// ```
     pub NoRestrictedImports {
         version: "1.6.0",
@@ -818,15 +1023,13 @@ impl Rule for NoRestrictedImports {
         if !state.allowed_import_names.is_empty() {
             let mut sorted = state.allowed_import_names.to_vec();
             sorted.sort();
-            let allowed_import_names = sorted
-                .into_iter()
-                .map(|name| {
-                    if &**&name == RestrictedImportVisitor::BARE_IMPORT_ALIAS {
-                        "Side-effect only import".into()
-                    } else {
-                        name
-                    }
-                });;
+            let allowed_import_names = sorted.into_iter().map(|name| {
+                if &**&name == RestrictedImportVisitor::BARE_IMPORT_ALIAS {
+                    "Side-effect only import".into()
+                } else {
+                    name
+                }
+            });
 
             rule_diagnostic = rule_diagnostic.footer_list(
                 markup! { "Only the following imports from "<Emphasis>"'"{state.import_source}"'"</Emphasis>" are allowed:" },
