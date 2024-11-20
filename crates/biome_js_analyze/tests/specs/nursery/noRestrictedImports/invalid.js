3cc83c391075f45c337399a481458747b4ac437f
Lukas Waslowski <cr7pt0gr4ph7@gmail.com>
2024-11-20 13:07:20 +0000

    feat(noRestrictedImports): add tests for ESM re-exports

--- crates/biome_js_analyze/tests/specs/nursery/noRestrictedImports/invalid.js
+++ crates/biome_js_analyze/tests/specs/nursery/noRestrictedImports/invalid.js
@@ -6,6 +6,7 @@ import 'namespace-allowed';
 import 'default-allowed';
 import 'default-and-member-allowed';
 
+export * from 'namespace-forbidden';
 import * as n1 from 'namespace-forbidden';
 const n2 = await import('namespace-forbidden');
 const n2b = ((await (import('namespace-forbidden'))));
@@ -19,6 +20,7 @@ import('namespace-forbidden').then(function (n6) { });
 ((import('namespace-forbidden'))).then(((function (n8) { })));
 
 import d0 from 'default-forbidden';
+export { default as d1 } from 'default-forbidden';
 import { default as d1 } from 'default-forbidden';
 const { "default": d2 } = await import('default-forbidden');
 const { default: d3 } = await import('default-forbidden');
@@ -27,6 +29,7 @@ const { allowed2, default: d5 } = await import('default-forbidden');
 const { allowed3, default: d6 } = import('default-forbidden');
 const { "allowed4": a1, "default": d7 } = import('default-forbidden');
 
+export { allowed5, forbidden1 } from 'member-forbidden';
 import { allowed5, forbidden1 } from 'member-forbidden';
 const { allowed6, forbidden2, forbidden3: f1, "forbidden4": f2 } = await import('member-forbidden');
 import('member-forbidden').then(({ allowed6, forbidden2, forbidden3: f1, "forbidden4": f2 }) => { })
@@ -39,6 +42,7 @@ const { allowed1 } = require('default-forbidden');
 const { allowed2 } = require('namespace-allowed');
 const { allowed3 } = require('member-allowed');
 
+export * from 'default-allowed';
 import * as n1 from 'default-allowed';
 const n2 = await import('default-allowed');
 const n2b = ((await (import('default-allowed'))));
@@ -51,6 +55,7 @@ import('default-allowed').then(function (n6) { });
 ((import('default-allowed'))).then((((n7) => { })));
 ((import('default-allowed'))).then(((function (n8) { })));
 
+export * from 'member-allowed';
 import * as n1 from 'member-allowed';
 const n2 = await import('member-allowed');
 const n2b = ((await (import('member-allowed'))));
@@ -63,6 +68,7 @@ import('member-allowed').then(function (n6) { });
 ((import('member-allowed'))).then((((n7) => { })));
 ((import('member-allowed'))).then(((function (n8) { })));
 
+export * from 'bare-allowed';
 import * as n1 from 'bare-allowed';
 const n2 = await import('bare-allowed');
 const n2b = ((await (import('bare-allowed'))));
@@ -76,6 +82,7 @@ import('bare-allowed').then(function (n6) { });
 ((import('bare-allowed'))).then(((function (n8) { })));
 
 import d0 from 'namespace-allowed';
+export { default as d1 } from 'namespace-allowed';
 import { default as d1 } from 'namespace-allowed';
 const { "default": d2 } = await import('namespace-allowed');
 const { default: d3 } = await import('namespace-allowed');
@@ -85,6 +92,7 @@ const { forbidden3, default: d6 } = import('namespace-allowed');
 const { "forbidden4": f4, "default": d7 } = import('namespace-allowed');
 
 import d0 from 'member-allowed';
+export { default as d1 } from 'member-allowed';
 import { default as d1 } from 'member-allowed';
 const { "default": d2 } = await import('member-allowed');
 const { default: d3 } = await import('member-allowed');
@@ -94,24 +102,29 @@ const { forbidden3, default: d6 } = import('member-allowed');
 const { "forbidden4": f4, "default": d7 } = import('member-allowed');
 
 import d0 from 'bare-allowed';
+export { default as d1 } from 'bare-allowed';
 import { default as d1 } from 'bare-allowed';
 const { "default": d2 } = await import('bare-allowed');
 const { default: d3 } = await import('bare-allowed');
+export { default as d4, forbidden1 } from 'bare-allowed';
 import { default as d4, forbidden1 } from 'bare-allowed';
 const { forbidden2, default: d5 } = await import('bare-allowed');
 const { forbidden3, default: d6 } = import('bare-allowed');
 const { "forbidden4": f4, "default": d7 } = import('bare-allowed');
 
+export { forbidden1 } from 'default-allowed';
 import { forbidden1 } from 'default-allowed';
 const { forbidden2, forbidden3: f1, "forbidden4": f2 } = await import('default-allowed');
 import('default-allowed').then(({ forbidden2, forbidden3: f1, "forbidden4": f2 }) => { })
 import('default-allowed').then(function ({ forbidden2, forbidden3: f1, "forbidden4": f2 }) { })
 
+export { forbidden1 } from 'namespace-allowed';
 import { forbidden1 } from 'namespace-allowed';
 const { forbidden2, forbidden3: f1, "forbidden4": f2 } = await import('namespace-allowed');
 import('namespace-allowed').then(({ forbidden2, forbidden3: f1, "forbidden4": f2 }) => { })
 import('namespace-allowed').then(function ({ forbidden2, forbidden3: f1, "forbidden4": f2 }) { })
 
+export { forbidden1 } from 'bare-allowed';
 import { forbidden1 } from 'bare-allowed';
 const { forbidden2, forbidden3: f1, "forbidden4": f2 } = await import('bare-allowed');
 import('bare-allowed').then(({ forbidden2, forbidden3: f1, "forbidden4": f2 }) => { })
--- crates/biome_js_analyze/tests/specs/nursery/noRestrictedImports/valid.js
+++ crates/biome_js_analyze/tests/specs/nursery/noRestrictedImports/valid.js
@@ -5,6 +5,7 @@ const l = require('lodash');
 
 import 'bare-allowed';
 
+export * from 'namespace-allowed';
 import * as n1 from 'namespace-allowed';
 const n2 = await import('namespace-allowed');
 const n2b = ((await (import('namespace-allowed'))));
@@ -17,15 +18,19 @@ import('namespace-allowed').then(function (n6) { });
 ((import('namespace-allowed'))).then((((n7) => { })));
 ((import('namespace-allowed'))).then(((function (n8) { })));
 
+export { default } from 'default-and-member-allowed';
 import d0 from 'default-and-member-allowed';
+export { default as d1 } from 'default-and-member-allowed';
 import { default as d1 } from 'default-and-member-allowed';
 const { "default": d2 } = await import('default-and-member-allowed');
 const { default: d3 } = await import('default-and-member-allowed');
+export { default as d4, allowed1 } from 'default-and-member-allowed';
 import { default as d4, allowed1 } from 'default-and-member-allowed';
 const { allowed2, default: d5 } = await import('default-and-member-allowed');
 const { allowed3, default: d6 } = import('default-and-member-allowed');
 const { "allowed4": a1, "default": d7 } = import('default-and-member-allowed');
 
+export { allowed2, allowed3 as a3 } from 'member-allowed';
 import { allowed2, allowed3 as a3 } from 'member-allowed';
 const { allowed2, allowed3: a3, "allowed4": a4 } = await import('member-allowed');
 import('member-allowed').then(({ allowed2, allowed3: a3, "allowed4": a4 }) => { })
@@ -40,6 +45,7 @@ const { some, random, identifiers } = require('default-allowed');
 const { some, random, identifiers } = require('namespace-forbidden');
 const { some, random, identifiers } = require('member-forbidden');
 
+export * from 'bare-forbidden';
 import * as n1 from 'bare-forbidden';
 const n2 = await import('bare-forbidden');
 const n2b = ((await (import('bare-forbidden'))));
@@ -52,6 +58,7 @@ import('bare-forbidden').then(function (n6) { });
 ((import('bare-forbidden'))).then((((n7) => { })));
 ((import('bare-forbidden'))).then(((function (n8) { })));
 
+export * from 'default-forbidden';
 import * as n1 from 'default-forbidden';
 const n2 = await import('default-forbidden');
 const n2b = ((await (import('default-forbidden'))));
@@ -64,6 +71,7 @@ import('default-forbidden').then(function (n6) { });
 ((import('default-forbidden'))).then((((n7) => { })));
 ((import('default-forbidden'))).then(((function (n8) { })));
 
+export * from 'member-forbidden';
 import * as n1 from 'member-forbidden';
 const n2 = await import('member-forbidden');
 const n2b = ((await (import('member-forbidden'))));
@@ -76,16 +84,19 @@ import('member-forbidden').then(function (n6) { });
 ((import('member-forbidden'))).then((((n7) => { })));
 ((import('member-forbidden'))).then(((function (n8) { })));
 
+export { allowed2, allowed3 as a3 } from 'bare-forbidden';
 import { allowed2, allowed3 as a3 } from 'bare-forbidden';
 const { allowed2, allowed3: a3, "allowed4": a4 } = await import('bare-forbidden');
 import('bare-forbidden').then(({ allowed2, allowed3: a3, "allowed4": a4 }) => { })
 import('bare-forbidden').then(function ({ allowed2, allowed3: a3, "allowed4": a4 }) { })
 
+export { allowed2, allowed3 as a3 } from 'default-forbidden';
 import { allowed2, allowed3 as a3 } from 'default-forbidden';
 const { allowed2, allowed3: a3, "allowed4": a4 } = await import('default-forbidden');
 import('default-forbidden').then(({ allowed2, allowed3: a3, "allowed4": a4 }) => { })
 import('default-forbidden').then(function ({ allowed2, allowed3: a3, "allowed4": a4 }) { })
 
+export { allowed2, allowed3 as a3 } from 'namespace-forbidden';
 import { allowed2, allowed3 as a3 } from 'namespace-forbidden';
 const { allowed2, allowed3: a3, "allowed4": a4 } = await import('namespace-forbidden');
 import('namespace-forbidden').then(({ allowed2, allowed3: a3, "allowed4": a4 }) => { })
