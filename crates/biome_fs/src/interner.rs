d2016ae93b19f456ac3281283deac95805af701e
Arend van Beelen jr. <arendjr@gmail.com>
2024-12-01 15:17:41 +0100

    chore: use `papaya` in `PathInterner` (#4669)

--- crates/biome_fs/src/interner.rs
+++ crates/biome_fs/src/interner.rs
@@ -1,13 +1,13 @@
 use crossbeam::channel::{unbounded, Receiver, Sender};
-use rustc_hash::FxHashSet;
+use papaya::HashSet;
+use rustc_hash::FxBuildHasher;
 use std::path::PathBuf;
-use std::sync::RwLock;
 
 /// File paths interner cache
 ///
 /// The path interner stores an instance of [PathBuf]
 pub struct PathInterner {
-    storage: RwLock<FxHashSet<PathBuf>>,
+    storage: HashSet<PathBuf, FxBuildHasher>,
     handler: Sender<PathBuf>,
 }
 
@@ -15,7 +15,7 @@ impl PathInterner {
     pub fn new() -> (Self, Receiver<PathBuf>) {
         let (send, recv) = unbounded();
         let interner = Self {
-            storage: RwLock::new(FxHashSet::default()),
+            storage: HashSet::default(),
             handler: send,
         };
 
@@ -25,7 +25,7 @@ impl PathInterner {
     /// Insert the path.
     /// Returns `true` if the path was not previously inserted.
     pub fn intern_path(&self, path: PathBuf) -> bool {
-        let result = self.storage.write().unwrap().insert(path.clone());
+        let result = self.storage.pin().insert(path.clone());
         if result {
             self.handler.send(path).ok();
         }
