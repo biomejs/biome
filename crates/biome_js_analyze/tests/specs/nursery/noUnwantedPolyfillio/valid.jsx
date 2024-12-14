86b38147d2c68da0173f1a16147a2948a8e8a8f8
unvalley <kirohi.code@gmail.com>
2024-12-14 17:33:27 +0900

    test: fix waste indent

--- crates/biome_js_analyze/tests/specs/nursery/noUnwantedPolyfillio/valid.jsx
+++ crates/biome_js_analyze/tests/specs/nursery/noUnwantedPolyfillio/valid.jsx
@@ -3,7 +3,7 @@ import Script from 'next/script';
 export function MyApp({ Component, pageProps }) {
   return (
     <div>
-      <Component {...pageProps} />
+        <Component {...pageProps} />
         <script src='https://polyfill.io/v3/polyfill.min.js?features=AbortController'></script>
         <script src='https://polyfill.io/v3/polyfill.min.js?features=IntersectionObserver'></script>
         <Script src='https://polyfill.io/v3/polyfill.min.js?features=IntersectionObserver' />
