bca4d30d6396888f9d0198ee70fb179c6be15433
wanghao <wanghao.engineer@icloud.com>
2024-12-17 23:20:22 +0800

    feat(lint): add `noProcessGlobal` rule (#4694)
    Co-authored-by: Wang Hao <hao.wang@shopee.com>

---
+++ crates/biome_js_analyze/tests/specs/nursery/noProcessGlobal/invalid.js
@@ -0,0 +1,16 @@
+// head
+/**
+ * head comment
+ */
+const c = process.env;
+
+const d = process;
+
+const e = process.env.e;
+
+// A copyright notice
+const f = process.env;
+
+function main() {
+    const local = process.env;
+}
