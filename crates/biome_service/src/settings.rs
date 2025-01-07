6e376a9b9ac312d42c160a7cade7693765294792
Emanuele Stoppa <my.burning@gmail.com>
2025-01-07 08:48:13 +0000

    lint

--- crates/biome_service/src/settings.rs
+++ crates/biome_service/src/settings.rs
@@ -1501,14 +1501,14 @@ impl TryFrom<OverrideAssistConfiguration> for AssistSettings {
 /// The case `Some(true)` (top-level) / `None` (language) varies based on `is_override`.
 ///
 /// - If `is_override` is `false` we don't want a top level
-/// feature to override the language-specific feature whose default
-/// value is false but in an "unset" state (`None`). So that we can
-/// still use `.unwrap_or_default()` to retrieve the correct
-/// fallback value. This happens when we want to mark the features
-/// of some languages as opt-in.
+///   feature to override the language-specific feature whose default
+///   value is false but in an "unset" state (`None`). So that we can
+///   still use `.unwrap_or_default()` to retrieve the correct
+///   fallback value. This happens when we want to mark the features
+///   of some languages as opt-in.
 ///
 /// - If `is_override` is `true`, we don't care if a feature is meant to be opt-in, so we fall back
-/// to the top-level configuration.
+///   to the top-level configuration.
 pub(crate) fn check_feature_activity<const LANG: bool, const TOP: bool>(
     language_specific_feature_activity: Option<Bool<LANG>>,
     top_level_feature_activity: Option<Bool<TOP>>,
