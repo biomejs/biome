b33f6ed1bf91e00d206ffda9ca8d1c287622fd16
Arend van Beelen jr. <arend@arendjr.nl>
2025-01-24 11:40:44 +0100

    Don't use recursion in cycle detection + other feedback

--- crates/biome_js_analyze/src/lint/nursery/no_import_cycles.rs
+++ crates/biome_js_analyze/src/lint/nursery/no_import_cycles.rs
@@ -1,9 +1,10 @@
-use std::collections::BTreeSet;
+use std::collections::HashSet;
 
 use biome_analyze::{context::RuleContext, declare_lint_rule, Rule, RuleDiagnostic, RuleSource};
 use biome_console::markup;
 use biome_dependency_graph::ModuleImports;
-use biome_js_syntax::inner_string_text;
+use biome_diagnostics::Severity;
+use biome_js_syntax::{inner_string_text, AnyJsImportLike};
 use biome_rowan::AstNode;
 use camino::{Utf8Path, Utf8PathBuf};
 
@@ -82,6 +83,7 @@ declare_lint_rule! {
         sources: &[
             RuleSource::EslintImport("no-cycle"),
         ],
+        severity: Severity::Warning,
         recommended: false,
     }
 }
@@ -95,22 +97,19 @@ impl Rule for NoImportCycles {
     fn run(ctx: &RuleContext<Self>) -> Self::Signals {
         let file_imports = ctx.imports_for_path(ctx.file_path())?;
 
-        let name_token = ctx.query().module_name_token()?;
+        let node = ctx.query();
+        let name_token = node.module_name_token()?;
         let specifier_text = inner_string_text(&name_token);
         let specifier = specifier_text.text();
-        let import = file_imports
-            .static_imports
-            .get(specifier)
-            .or_else(|| file_imports.dynamic_imports.get(specifier))?;
+        let import = if is_static_import(node) {
+            file_imports.static_imports.get(specifier)
+        } else {
+            file_imports.dynamic_imports.get(specifier)
+        }?;
         let resolved_path = import.resolved_path.as_ref().ok()?;
 
         let imports = ctx.imports_for_path(resolved_path)?;
-        find_cycle(
-            ctx,
-            imports,
-            &mut BTreeSet::new(),
-            vec![resolved_path.to_string()],
-        )
+        find_cycle(ctx, resolved_path, imports)
     }
 
     fn diagnostic(ctx: &RuleContext<Self>, state: &Self::State) -> Option<RuleDiagnostic> {
@@ -121,6 +120,7 @@ impl Rule for NoImportCycles {
                 .map(|cwd| cwd.to_string_lossy().to_string())
                 .unwrap_or_default(),
         );
+
         let mut note = markup!("This import resolves to ").to_owned();
         for (i, path) in state.iter().enumerate() {
             if i > 0 {
@@ -147,39 +147,66 @@ impl Rule for NoImportCycles {
     }
 }
 
+/// Attempts to find a cycle by traversing all imports from `start_path` and
+/// finding those that lead back to `ctx.file_path()`.
+///
+/// Cycles that don't lead back to `ctx.file_path()` are not reported, since
+/// they should be reported for the respective files instead.
+///
+/// `imports` are the imports found in `start_path`.
+///
+/// If a cycle is found, this returns a vector with all the paths involved in
+/// the cycle, starting with `start_path` and ending with `ctx.file_path()`.
 fn find_cycle(
     ctx: &RuleContext<NoImportCycles>,
-    imports: ModuleImports,
-    seen: &mut BTreeSet<String>,
-    mut stack: Vec<String>,
+    start_path: &Utf8Path,
+    mut imports: ModuleImports,
 ) -> Option<Vec<String>> {
-    for (_specifier, import) in imports
-        .static_imports
-        .into_iter()
-        .chain(imports.dynamic_imports)
-    {
-        let Ok(resolved_path) = import.resolved_path else {
-            continue;
-        };
+    let mut seen = HashSet::new();
+    let mut stack = Vec::new();
 
-        if resolved_path == ctx.file_path() {
-            stack.push(resolved_path.into_string());
-            return Some(stack);
+    'outer: loop {
+        while let Some((_specifier, import)) = imports.drain_one() {
+            let Ok(resolved_path) = import.resolved_path else {
+                continue;
+            };
+
+            if resolved_path == ctx.file_path() {
+                // Return all the paths from `start_path` to `resolved_path`:
+                let paths = Some(start_path.to_string())
+                    .into_iter()
+                    .chain(stack.into_iter().map(|(path, _)| path))
+                    .chain(Some(resolved_path.into()))
+                    .collect();
+                return Some(paths);
+            }
+
+            // FIXME: Use `get_or_insert_with()` once it's stabilized.
+            //        See: https://github.com/rust-lang/rust/issues/60896
+            if seen.contains(resolved_path.as_str()) {
+                continue;
+            }
+
+            seen.insert(resolved_path.to_string());
+
+            if let Some(resolved_imports) = ctx.imports_for_path(&resolved_path) {
+                stack.push((resolved_path.into(), imports));
+                imports = resolved_imports;
+                continue 'outer;
+            }
         }
 
-        if !seen.insert(resolved_path.to_string()) {
-            continue;
-        }
-
-        let result = ctx.imports_for_path(&resolved_path).and_then(|imports| {
-            let mut stack = stack.clone();
-            stack.push(resolved_path.to_string());
-            find_cycle(ctx, imports, seen, stack)
-        });
-        if let Some(cycle) = result {
-            return Some(cycle);
+        match stack.pop() {
+            Some((_previous_path, previous_imports)) => {
+                imports = previous_imports;
+            }
+            None => break,
         }
     }
 
     None
+}
+
+fn is_static_import(node: &AnyJsImportLike) -> bool {
+    matches!(node, AnyJsImportLike::JsModuleSource(_))
 }
