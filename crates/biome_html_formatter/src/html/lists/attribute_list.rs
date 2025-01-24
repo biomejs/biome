b27188e1aec1441b7e863d6e0bb2abfe4989000f
Carson McManus <carson.mcmanus1@gmail.com>
2025-01-24 12:27:44 -0500

    feat(format/html): implement bracketSameLine

--- crates/biome_html_formatter/src/html/lists/attribute_list.rs
+++ crates/biome_html_formatter/src/html/lists/attribute_list.rs
@@ -1,24 +1,14 @@
 use crate::prelude::*;
-use biome_formatter::{write, AttributePosition};
 use biome_html_syntax::HtmlAttributeList;
 #[derive(Debug, Clone, Default)]
 pub(crate) struct FormatHtmlAttributeList;
 impl FormatRule<HtmlAttributeList> for FormatHtmlAttributeList {
     type Context = HtmlFormatContext;
     fn fmt(&self, node: &HtmlAttributeList, f: &mut HtmlFormatter) -> FormatResult<()> {
-        let line_break = if f.options().attribute_position() == AttributePosition::Multiline {
-            hard_line_break()
-        } else {
-            soft_line_break_or_space()
-        };
-
-        write!(
-            f,
-            [&group(&soft_block_indent(&format_with(|f| {
-                f.join_with(&line_break)
-                    .entries(node.iter().formatted())
-                    .finish()
-            })))]
-        )
+        // The formatting of this node is handled by `HtmlOpeningElement` and `HtmlSelfClosingElement` instead.
+        //
+        // This is because whether or not the element children breaks is partially dependent whether or not the
+        // attributes break, and to be able to handle the `bracketSameLine` option correctly.
+        format_verbatim_node(node.syntax()).fmt(f)
     }
 }
