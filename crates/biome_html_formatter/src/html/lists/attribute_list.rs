ff7729fac32deb4140cc7038bedf6e3f8783592e
Carson McManus <carson.mcmanus1@gmail.com>
2025-01-28 10:28:26 -0500

    chore: format

--- crates/biome_html_formatter/src/html/lists/attribute_list.rs
+++ crates/biome_html_formatter/src/html/lists/attribute_list.rs
@@ -7,7 +7,9 @@ impl FormatRule<HtmlAttributeList> for FormatHtmlAttributeList {
     type Context = HtmlFormatContext;
     fn fmt(&self, node: &HtmlAttributeList, f: &mut HtmlFormatter) -> FormatResult<()> {
         let attribute_len = node.iter().len();
-        let line_break = if f.options().attribute_position() == AttributePosition::Multiline && attribute_len > 1 {
+        let line_break = if f.options().attribute_position() == AttributePosition::Multiline
+            && attribute_len > 1
+        {
             hard_line_break()
         } else {
             soft_line_break_or_space()
