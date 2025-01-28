d76e8d3968eac6440efc3a253adfd60dc54f7805
Hyeonggeun Oh <kandy1002@naver.com>
2025-01-29 06:01:20 +0900

    feat(format/html): apply line break with multiple attributes in multiline configuration (#4984)
    Co-authored-by: Carson McManus <carson.mcmanus1@gmail.com>

--- crates/biome_html_formatter/src/html/lists/attribute_list.rs
+++ crates/biome_html_formatter/src/html/lists/attribute_list.rs
@@ -6,7 +6,10 @@ pub(crate) struct FormatHtmlAttributeList;
 impl FormatRule<HtmlAttributeList> for FormatHtmlAttributeList {
     type Context = HtmlFormatContext;
     fn fmt(&self, node: &HtmlAttributeList, f: &mut HtmlFormatter) -> FormatResult<()> {
-        let line_break = if f.options().attribute_position() == AttributePosition::Multiline {
+        let attribute_len = node.iter().len();
+        let line_break = if f.options().attribute_position() == AttributePosition::Multiline
+            && attribute_len > 1
+        {
             hard_line_break()
         } else {
             soft_line_break_or_space()
