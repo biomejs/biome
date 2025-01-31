e5892613205910c46ebc0c1d1cf519fa56f9921b
kaykdm <34934746+kaykdm@users.noreply.github.com>
2025-01-31 16:19:51 +0900

    feat(lint): support promise expression in noFloatingPromises (#4977)

--- crates/biome_js_analyze/tests/specs/nursery/noFloatingPromises/valid.ts
+++ crates/biome_js_analyze/tests/specs/nursery/noFloatingPromises/valid.ts
@@ -13,3 +13,16 @@ returnsPromise().then(
 );
 
 returnsPromise().catch(() => { });
+
+
+const promise = new Promise((resolve) => resolve('value'));
+promise.then(() => { }, () => { })
+
+Promise.resolve('value').then(() => { }, () => { })
+Promise.all([p1, p2, p3]).catch(() => { })
+
+const Promise = { resolve(): {} };
+Promise.resolve()
+async function bindingPromiseInsideFunction() {
+  Promise.resolve()
+}
