fb8b83f8226b72991421a7701b91f4522dbf2102
Carson McManus <carson.mcmanus1@gmail.com>
2025-02-03 12:30:09 -0500

    fix(format/html): enforce whitespace sensitivity for siblings in HtmlElementList

--- crates/biome_html_formatter/src/utils/metadata.rs
+++ crates/biome_html_formatter/src/utils/metadata.rs
@@ -1,3 +1,8 @@
+use biome_html_syntax::AnyHtmlElement;
+use biome_rowan::AstNode;
+
+use crate::HtmlFormatter;
+
 /// HTML tags that have an "inline" layout by default.
 ///
 /// In HTML, The inline layout treats the element as if it were a single line of text. This means that the element does not start on a new line, and only takes up as much width as necessary.
@@ -12,3 +17,29 @@ pub const HTML_INLINE_TAGS: &[&str] = &[
     // TODO: this is incomplete. derive this from the HTML spec.
     "b", "i", "u", "span", "a", "strong", "em", "small", "big",
 ];
+
+/// Whether or not an element should be considered whitespace sensitive, considering the element's tag name and the
+/// formatter's whitespace sensitivity options.
+pub(crate) fn is_element_whitespace_sensitive(f: &HtmlFormatter, element: &AnyHtmlElement) -> bool {
+    let name = match element {
+        AnyHtmlElement::HtmlElement(element) => {
+            element.opening_element().and_then(|element| element.name())
+        }
+        AnyHtmlElement::HtmlSelfClosingElement(element) => element.name(),
+        _ => return false,
+    };
+
+    let is_inline_element = if let Ok(name) = name {
+        let Ok(Some(tag_name)) = name.trim_trivia().map(|t| t.value_token()).transpose() else {
+            return false;
+        };
+        HTML_INLINE_TAGS
+            .iter()
+            .any(|tag| tag_name.text().eq_ignore_ascii_case(tag))
+    } else {
+        false
+    };
+
+    let sensitivity = f.options().whitespace_sensitivity();
+    sensitivity.is_css() && is_inline_element || sensitivity.is_strict()
+}
