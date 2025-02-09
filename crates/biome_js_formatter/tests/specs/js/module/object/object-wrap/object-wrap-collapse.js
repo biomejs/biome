4d1fd0bbfce5e174dffdf6c8f6291b13f8a4097c
Naoki Ikeguchi <me@s6n.jp>
2025-02-09 22:18:05 +0900

    test(biome_js_formatter): Add test cases for objectWrap option
    Signed-off-by: Naoki Ikeguchi <me@s6n.jp>

---
+++ crates/biome_js_formatter/tests/specs/js/module/object/object-wrap/object-wrap-collapse.js
@@ -0,0 +1,17 @@
+const a = { already: "collapsed" };
+
+const b = {
+	needs: "to",
+	be: "collapsed",
+};
+
+const c = { very: "long", object: "literal", with: "many", properties: "it", needs: "to", be: "expanded"  };
+
+const d = {
+	very: "long",
+	object: "literal",
+	but: "it",
+	is: "already",
+	expanded: "so",
+	do: "nothing",
+};
