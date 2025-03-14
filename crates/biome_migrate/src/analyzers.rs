47910140c539e3996cc060e2ba1d6bd2321ba29f
Emanuele Stoppa <my.burning@gmail.com>
2025-03-14 10:35:40 +0000

    refactor(linter): rename rule `noMultipleSpacesInRegularExpressionLiterals`

--- crates/biome_migrate/src/analyzers.rs
+++ crates/biome_migrate/src/analyzers.rs
@@ -1,20 +1,22 @@
 use crate::analyzers::all::RulesAll;
 use crate::analyzers::deleted_rules::DeletedRules;
 use crate::analyzers::includes::Includes;
+use crate::analyzers::no_multiple_spaces_in_regex::UseMultipleSpacesInRegex;
 use crate::analyzers::no_var::NoVar;
 use crate::analyzers::nursery_rules::NurseryRules;
 use crate::analyzers::organize_imports::OrganizeImports;
 use crate::analyzers::schema::Schema;
 use crate::analyzers::style_rules::StyleRules;
 use crate::analyzers::trailing_comma::TrailingComma;
+use crate::analyzers::use_naming_convention_enum_member_case::UseNamingConventionEnumMemberCase;
 use crate::analyzers::use_while::UseWhile;
 use biome_analyze::{GroupCategory, RegistryVisitor, RuleCategory, RuleGroup};
 use biome_json_syntax::JsonLanguage;
-use use_naming_convention_enum_member_case::UseNamingConventionEnumMemberCase;
 
 mod all;
 mod deleted_rules;
 mod includes;
+mod no_multiple_spaces_in_regex;
 mod no_var;
 mod nursery_rules;
 mod organize_imports;
@@ -48,6 +50,7 @@ impl RuleGroup for MigrationGroup {
         registry.record_rule::<Includes>();
         registry.record_rule::<TrailingComma>();
         registry.record_rule::<UseNamingConventionEnumMemberCase>();
+        registry.record_rule::<UseMultipleSpacesInRegex>()
     }
 }
 
