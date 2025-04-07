f6db30dd58ecfb040e875f967441cc2fc0ededa5
Victorien Elvinger <victorien@elvinger.fr>
2025-04-07 21:39:13 +0200

    feat(assist/organizeImports): merge imports and exports (#5579)

---
+++ crates/biome_js_analyze/tests/specs/source/organizeImports/mergeable_export.ts
@@ -0,0 +1,8 @@
+// Comment 1
+export type { T4, T2 } from "mod";
+// Comment 2
+export type { T3, T1 } from "mod";
+// Comment 3
+export { A, D } from "mod";
+// Comment 4
+export { B, C } from "mod";
---
+++ crates/biome_js_analyze/tests/specs/source/organizeImports/mergeable_unsorted_imports.ts
@@ -0,0 +1,23 @@
+// File header comment
+import type { T1, T2 } from "mod";
+// Comment 2
+import type { U1 } from "package";
+// Comment 3
+import type { T3, T4 } from "mod";
+// Comment 4
+import { A, B } from "mod";
+// Comment 5
+import { X } from "package";
+// Comment 6
+import { C, D } from "mod";
+// Comment 7
+import { Y } from "package";
+// Comment 8
+import type { U2 } from "package";
+
+// Chunk 2
+
+// Comment 9
+import type { U3 } from "package";
+// Comment 10
+import { E } from "mod";
