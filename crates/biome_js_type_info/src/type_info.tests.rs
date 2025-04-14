6edbc023cbca063f4f2c4744d42b6f76e4110523
Arend van Beelen jr. <arendjr@gmail.com>
2025-04-14 14:02:14 +0200

    chore: use test for type size verification (#5666)

--- crates/biome_js_type_info/src/type_info.tests.rs
+++ crates/biome_js_type_info/src/type_info.tests.rs
@@ -1,1 +1,16 @@
+use super::*;
 
+#[test]
+#[cfg(target_pointer_width = "64")]
+fn verify_type_sizes() {
+    assert_eq!(
+        std::mem::size_of::<Type>(),
+        8,
+        "`Type` should not be bigger than 8 bytes"
+    );
+    assert_eq!(
+        std::mem::size_of::<TypeInner>(),
+        16,
+        "`TypeInner` should not be bigger than 16 bytes"
+    );
+}
