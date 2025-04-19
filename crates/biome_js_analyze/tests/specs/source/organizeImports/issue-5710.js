4feb0bc671ba454b1ac011146efeb233cc011e2d
Naoki Ikeguchi <me@s6n.jp>
2025-04-19 20:56:26 +0900

    fix(assist): skip sorting specifiers if it contains any bogus nodes (#5711)

---
+++ crates/biome_js_analyze/tests/specs/source/organizeImports/issue-5710.js
@@ -0,0 +1,5 @@
+// The last `inject` specifier is a bogus node, so this won't emit any code actions.
+import { inject, Injectable, inject } from '@angular/core';
+
+// The last `key` attribute is a bogus node, so this won't emit any code actions.
+import { names } from "module-name" with { key: "data", key2: "data2", key: "data3" };
