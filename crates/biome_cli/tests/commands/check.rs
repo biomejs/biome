62c5e23175277e0a8fbf6bd8272338168c246471
unvalley <kirohi.code@gmail.com>
2025-04-27 03:38:50 +0900

    test: add snapshot test

--- crates/biome_cli/tests/commands/check.rs
+++ crates/biome_cli/tests/commands/check.rs
@@ -2892,7 +2892,15 @@ fn check_skip_errors() {
     let (fs, result) = run_cli(
         fs,
         &mut console,
-        Args::from(["check", "--skip-parse-errors", valid.as_str(), invalid.as_str()].as_slice()),
+        Args::from(
+            [
+                "check",
+                "--skip-parse-errors",
+                valid.as_str(),
+                invalid.as_str(),
+            ]
+            .as_slice(),
+        ),
     );
 
     assert!(result.is_err(), "run_cli returned {result:?}");
