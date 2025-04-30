113313ebe815c6033be0aca7947ec3f625b30988
Emanuele Stoppa <my.burning@gmail.com>
2025-04-30 13:03:58 +0100

    fix(core): ignore node_modules during the traversal (#5825)

--- .changeset/fluffy-hornets-follow.md
+++
@@ -1,7 +0,0 @@
----
-"@biomejs/biome": major
----
-
-Biome doesn't ignore `node_modules` anymore *by default*. If the folder wasn't ignored already, run the command `biome migrate` and Biome will add it to the configuration.
-
-The migration doesn't check if `node_modules` is already ignored in the `.gitignore` file. If the entry is already available, delete `**/node_modules/**` entry from the `includes` configuration.
--- crates/biome_service/src/workspace/scanner.rs
+++ crates/biome_service/src/workspace/scanner.rs
@@ -8,6 +8,13 @@
 //! In other words, the scanner is both the scanning logic in this module as
 //! well as the watcher to allow continuous scanning.
 
+use super::ServiceDataNotification;
+use super::server::WorkspaceServer;
+use crate::diagnostics::Panic;
+use crate::projects::ProjectKey;
+use crate::workspace::{DocumentFileSource, FileContent, OpenFileParams};
+use crate::workspace_watcher::WatcherSignalKind;
+use crate::{Workspace, WorkspaceError};
 use biome_diagnostics::serde::Diagnostic;
 use biome_diagnostics::{Diagnostic as _, Error, Severity};
 use biome_fs::{BiomePath, PathInterner, TraversalContext, TraversalScope};
@@ -19,15 +26,6 @@ use std::sync::RwLock;
 use std::thread;
 use std::time::{Duration, Instant};
 use tracing::instrument;
-
-use crate::diagnostics::Panic;
-use crate::projects::ProjectKey;
-use crate::workspace::{DocumentFileSource, FileContent, OpenFileParams};
-use crate::workspace_watcher::WatcherSignalKind;
-use crate::{IGNORE_ENTRIES, Workspace, WorkspaceError};
-
-use super::ServiceDataNotification;
-use super::server::WorkspaceServer;
 
 pub(crate) struct ScanResult {
     /// Diagnostics reported while scanning the project.
@@ -244,6 +242,17 @@ impl ScanContext<'_> {
     }
 }
 
+/// Path entries that we want to ignore during the scanning phase.
+pub const SCANNER_IGNORE_ENTRIES: &[&[u8]] = &[
+    b".cache",
+    b".git",
+    b".hg",
+    b".svn",
+    b".yarn",
+    b".timestamp",
+    b".DS_Store",
+];
+
 impl TraversalContext for ScanContext<'_> {
     fn interner(&self) -> &PathInterner {
         &self.interner
@@ -260,7 +269,7 @@ impl TraversalContext for ScanContext<'_> {
     fn can_handle(&self, path: &BiomePath) -> bool {
         if path
             .file_name()
-            .is_some_and(|file_name| IGNORE_ENTRIES.contains(&file_name.as_bytes()))
+            .is_some_and(|file_name| SCANNER_IGNORE_ENTRIES.contains(&file_name.as_bytes()))
         {
             return false;
         }
