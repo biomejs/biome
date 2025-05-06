228f11ed28089dc86a11537ca0610e9fa1fd9b44
Vo Hoang Long <vo.hoang.long@outlook.com>
2025-05-06 15:57:07 +0900

    feat(yaml): parse plain node

--- crates/biome_yaml_parser/src/parser/block.rs
+++ crates/biome_yaml_parser/src/parser/block.rs
@@ -3,6 +3,7 @@ use biome_yaml_syntax::YamlSyntaxKind::*;
 
 use super::YamlParser;
 
+#[expect(dead_code)]
 pub(crate) fn parse_any_block(p: &mut YamlParser) -> CompletedMarker {
     parse_literal_scalar(p)
 }
--- crates/biome_yaml_parser/src/parser/document.rs
+++ crates/biome_yaml_parser/src/parser/document.rs
@@ -1,11 +1,18 @@
 use biome_parser::{
     CompletedMarker, Parser,
     parse_lists::ParseNodeList,
+    parse_recovery::{ParseRecoveryTokenSet, RecoveryResult},
     prelude::ParsedSyntax::{self, *},
+    token_set,
 };
-use biome_yaml_syntax::YamlSyntaxKind::{self, *};
+use biome_yaml_syntax::{
+    T,
+    YamlSyntaxKind::{self, *},
+};
 
-use super::{YamlParser, block::parse_any_block};
+use crate::lexer::YamlLexContext;
+
+use super::{YamlParser, flow::parse_any_flow_node, parse_error::expected_directive};
 
 #[derive(Default)]
 pub(crate) struct DocumentList;
@@ -28,16 +35,56 @@ impl ParseNodeList for DocumentList {
         &mut self,
         _p: &mut Self::Parser<'_>,
         parsed_element: ParsedSyntax,
-    ) -> biome_parser::parse_recovery::RecoveryResult {
+    ) -> RecoveryResult {
         Ok(parsed_element.expect("A document can not be absent"))
     }
 }
 
 fn parse_document(p: &mut YamlParser) -> CompletedMarker {
+    let m = p.start();
     p.eat(UNICODE_BOM);
-    parse_any_node(p)
+    DirectiveList.parse_list(p);
+    parse_any_flow_node(p, YamlLexContext::FlowOut);
+    m.complete(p, YAML_DOCUMENT)
 }
 
-fn parse_any_node(p: &mut YamlParser) -> CompletedMarker {
-    parse_any_block(p)
+#[derive(Default)]
+pub(crate) struct DirectiveList;
+
+impl ParseNodeList for DirectiveList {
+    type Kind = YamlSyntaxKind;
+    type Parser<'source> = YamlParser<'source>;
+
+    const LIST_KIND: Self::Kind = YAML_DIRECTIVE_LIST;
+
+    fn parse_element(&mut self, p: &mut Self::Parser<'_>) -> ParsedSyntax {
+        parse_directive(p)
+    }
+
+    fn is_at_list_end(&self, p: &mut Self::Parser<'_>) -> bool {
+        p.at(T![...])
+            // Consecutive directives are rare, so it's better to just fail fast here
+            || !p.at(DIRECTIVE_LITERAL)
+    }
+
+    fn recover(
+        &mut self,
+        p: &mut Self::Parser<'_>,
+        parsed_element: ParsedSyntax,
+    ) -> RecoveryResult {
+        parsed_element.or_recover_with_token_set(
+            p,
+            &ParseRecoveryTokenSet::new(YAML_BOGUS, token_set![DIRECTIVE_LITERAL]),
+            expected_directive,
+        )
+    }
+}
+
+fn parse_directive(p: &mut YamlParser) -> ParsedSyntax {
+    if !p.at(DIRECTIVE_LITERAL) {
+        return Absent;
+    }
+    let m = p.start();
+    p.bump(DIRECTIVE_LITERAL);
+    Present(m.complete(p, YAML_DIRECTIVE))
 }
