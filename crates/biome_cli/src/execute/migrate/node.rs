c79f8b25a6e788b77819ce50f797dd2ac7015935
Naoki Ikeguchi <me@s6n.jp>
2025-05-06 17:29:25 +0900

    feat(cli): prettify ESLint config on loading for migration (#5902)

--- crates/biome_cli/src/execute/migrate/node.rs
+++ crates/biome_cli/src/execute/migrate/node.rs
@@ -9,7 +9,7 @@ pub(crate) fn load_config(specifier: &str) -> Result<Resolution, CliDiagnostic> 
         .env("NODE_NO_WARNINGS", "1")
         .arg("--eval")
         .arg(format!(
-            "{UNCYCLE_FUNCTION} import('{specifier}').then((c) => console.log(JSON.stringify(uncycle(c.default))))"
+            "{UNCYCLE_FUNCTION} import('{specifier}').then((c) => console.log(JSON.stringify(uncycle(c.default), null, 2)))"
         ))
         .output();
     match content_output {
