229825b697ed5f64d2050a8376d79e6e6a8b4318
Victorien Elvinger <victorien@elvinger.fr>
2025-05-11 23:41:11 +0200

    refactor(migrate): handle already migrated rules

--- crates/biome_migrate/src/analyzers/rule_mover.rs
+++ crates/biome_migrate/src/analyzers/rule_mover.rs
@@ -133,32 +133,21 @@ impl Rule for RuleMover {
         // If the rule is moved or renamed
         let rule_node = state.rule_node.clone();
         // Rename the rule
-        let rule_node = if let Some(old_rule_name) = state.old_rule_name {
+        let new_rule_node = if let Some(old_rule_name) = state.old_rule_name {
             let new_name =
                 make::json_member_name(make::json_string_literal(state.new_rule.as_str()));
-            let rule_node = rule_node.with_name(new_name);
-            if let Some(value) = rule_node
+            let new_rule_node = rule_node.with_name(new_name);
+            if let Some(value) = new_rule_node
                 .value()
                 .ok()
                 .and_then(|value| transform_value(value, old_rule_name))
             {
-                rule_node.with_value(value)
+                new_rule_node.with_value(value)
             } else {
-                rule_node
+                new_rule_node
             }
         } else {
             rule_node
-        };
-
-        // If the rule is not moved and just renamed, we return early.
-        if state.old_group.is_none() {
-            mutation.replace_node(state.rule_node.clone(), rule_node);
-            return Some(MigrationAction::new(
-                ctx.metadata().action_category(ctx.category(), ctx.group()),
-                ctx.metadata().applicability(),
-                markup! { "Rename the rule." }.to_owned(),
-                mutation,
-            ));
         };
 
         // Update or create the group where the rule is moved to
@@ -173,12 +162,34 @@ impl Rule for RuleMover {
             };
             let old_list = group_obj.json_member_list();
             let mut new_elements = Vec::with_capacity(old_list.len() + 1);
+            let new_rule_name = state.new_rule.as_str();
+            let mut last_has_separator = false;
+            let mut is_rule_migrated = false;
             for elt in old_list.elements() {
-                new_elements.push((elt.node.ok()?, elt.trailing_separator.ok()?));
+                let node = elt.node.ok()?;
+                if let Ok(name) = node.name().and_then(|name| name.inner_string_text()) {
+                    if name.text() == new_rule_name {
+                        // This happens when:
+                        // - the rule has already been manually migrated, but the old one was not removed
+                        // - several rules are renamed and merged into a same rule.
+                        is_rule_migrated = true;
+                    } else if state.old_rule_name == Some(name.text()) {
+                        // Remove the old rule.
+                        // This happens when the new and old rule are located in the same group.
+                        continue;
+                    }
+                }
+                let trailing_separator = elt.trailing_separator.ok()?;
+                last_has_separator = trailing_separator.is_some();
+                new_elements.push((node, trailing_separator));
             }
-            let last_has_separator = new_elements.last().is_some_and(|(_, sep)| sep.is_some());
-            // Add the new rule
-            new_elements.push((rule_node, None));
+            if !is_rule_migrated {
+                let new_rule_node = new_rule_node.with_leading_trivia_pieces(
+                    state.rule_node.syntax().first_leading_trivia()?.pieces(),
+                )?;
+                // Add the new rule
+                new_elements.push((new_rule_node, None));
+            }
             handle_trvia(
                 new_elements.iter_mut().map(|(a, b)| (a, b)),
                 last_has_separator,
@@ -225,12 +236,12 @@ impl Rule for RuleMover {
                     make::token(T!['{']).with_trailing_trivia([(TriviaPieceKind::Whitespace, " ")]),
                     if last_has_separator {
                         make::json_member_list(
-                            [rule_node],
+                            [new_rule_node],
                             [make::token(T![,])
                                 .with_trailing_trivia([(TriviaPieceKind::Whitespace, " ")])],
                         )
                     } else {
-                        make::json_member_list([rule_node], [])
+                        make::json_member_list([new_rule_node], [])
                     },
                     make::token(T!['}']).with_leading_trivia([(TriviaPieceKind::Whitespace, " ")]),
                 )
@@ -240,12 +251,15 @@ impl Rule for RuleMover {
         };
 
         // Update `rules` by updating or adding `new_group_node`.
-        // It is also in charge of removing the old rule.
+        // It is also in charge of removing the old rule if it is not in the same group as the new one.
         let mut new_rules_elements = Vec::with_capacity(old_rules_list.len() + 1);
         let rule_group_node = state.rule_node.syntax().grand_parent()?.parent()?;
         for elt in old_rules_list.elements() {
             let node = elt.node.ok()?;
-            let node = if node.range() == rule_group_node.text_range() {
+            let node = if new_group_range.is_some_and(|range| range == node.range()) {
+                // Update the group
+                new_group_node.clone()
+            } else if node.range() == rule_group_node.text_range() {
                 // Remove the old rule
                 let old_list = state.rule_node.parent::<JsonMemberList>()?;
                 let mut new_elements = Vec::with_capacity(old_list.len() - 1);
@@ -273,9 +287,6 @@ impl Rule for RuleMover {
                 let obj = old_list.parent::<JsonObjectValue>()?;
                 let obj = obj.with_json_member_list(new_list);
                 node.with_value(obj.into())
-            } else if new_group_range.is_some_and(|range| range == node.range()) {
-                // Update the group
-                new_group_node.clone()
             } else {
                 node
             };
@@ -305,7 +316,13 @@ impl Rule for RuleMover {
         Some(MigrationAction::new(
             ctx.metadata().action_category(ctx.category(), ctx.group()),
             ctx.metadata().applicability(),
-            markup! { "Move the rule." }.to_owned(),
+            if state.old_group.is_none() {
+                "Rename the rule."
+            } else if state.old_rule_name.is_none() {
+                "Move the rule."
+            } else {
+                "Move and rename the rule."
+            },
             mutation,
         ))
     }
