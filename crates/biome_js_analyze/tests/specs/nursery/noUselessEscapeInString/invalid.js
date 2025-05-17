01c962c655f2cbc5bb635876cb028d3f17d3815a
Lucas Weng <lucasheweng@gmail.com>
2025-05-17 09:09:43 +0800

    fix(lint/noUselessEscapeInString): flag useless escapes after skipping \${ in template literals

--- crates/biome_js_analyze/tests/specs/nursery/noUselessEscapeInString/invalid.js
+++ crates/biome_js_analyze/tests/specs/nursery/noUselessEscapeInString/invalid.js
@@ -5,5 +5,7 @@ var s = {
     // A test with unicode characters that take more than one byte
     key: "😀\😀",
     // https://github.com/biomejs/biome/issues/6039
-    templateLiterals: `\$x`
+    templateLiterals1: `\$x`,
+    templateLiterals2: `\${\a`,
+    templateLiterals3: `\${} \a`
 };
