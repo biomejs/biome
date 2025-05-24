745bf9236b3e905b3367f6e3bc93e45b60dc02d4
vladimir ivanov <vladoivan78@googlemail.com>
2025-05-24 08:45:08 +0100

    Update crates/biome_js_analyze/src/lint/nursery/use_index_of.rs
    Co-authored-by: Victorien Elvinger <victorien@elvinger.fr>

--- crates/biome_js_analyze/src/lint/nursery/use_index_of.rs
+++ crates/biome_js_analyze/src/lint/nursery/use_index_of.rs
@@ -22,7 +22,7 @@ use biome_rowan::{AstNode, AstSeparatedList, BatchMutationExt, SyntaxToken};
 declare_lint_rule! {
     /// Prefer `Array#{indexOf,lastIndexOf}()` over `Array#{findIndex,findLastIndex}()` when looking for the index of an item.
     ///
-    /// Array#findIndex() and Array#findLastIndex() are intended for more complex needs.
+    /// `Array#findIndex()` and `Array#findLastIndex()` are intended for more complex needs.
     /// If you are just looking for the index where the given item is present, then the code can be simplified to use Array#indexOf() or Array#lastIndexOf().
     /// This applies to any search with a literal, a variable, or any expression that doesn't have any explicit side effects.
     /// However, if the expression you are looking for relies on an item related to the function (its arguments, the function self, etc.), the case is still valid.
