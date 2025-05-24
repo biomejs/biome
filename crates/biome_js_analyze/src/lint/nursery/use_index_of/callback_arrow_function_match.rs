100e7b3d7e9080f6368b4814952a2513b3455305
vladimir ivanov <vladoivan78@googlemail.com>
2025-05-24 10:12:35 +0100

    Update crates/biome_js_analyze/src/lint/nursery/use_index_of/callback_arrow_function_match.rs
    Co-authored-by: Victorien Elvinger <victorien@elvinger.fr>

--- crates/biome_js_analyze/src/lint/nursery/use_index_of/callback_arrow_function_match.rs
+++ crates/biome_js_analyze/src/lint/nursery/use_index_of/callback_arrow_function_match.rs
@@ -5,8 +5,8 @@ use crate::lint::nursery::use_index_of::{
 use biome_js_syntax::{JsArrowFunctionExpression, JsSyntaxToken};
 
 pub fn callback_arrow_function_match(
-    function: &JsArrowFunctionExpression,
-    member_name_token: &JsSyntaxToken,
+    function: JsArrowFunctionExpression,
+    member_name_token: JsSyntaxToken,
 ) -> Option<JsSyntaxMatchPair> {
     if function.async_token().is_some() {
         return None;
@@ -19,7 +19,7 @@ pub fn callback_arrow_function_match(
     let matched = find_index_comparable_expression(&body, &parameter_name, false);
 
     matched.as_ref().map(|token_match| JsSyntaxMatchPair {
-        matching_array_element: token_match.clone(),
-        member_name: member_name_token.clone(),
+        matching_array_element: token_match,
+        member_name: member_name_token,
     })
 }
