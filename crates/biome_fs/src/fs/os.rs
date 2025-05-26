4afc3da3e8a02d922c86fbe4a47149c8a0e31f7f
Emanuele Stoppa <my.burning@gmail.com>
2025-05-26 16:09:13 +0100

    feedback

--- crates/biome_fs/src/fs/os.rs
+++ crates/biome_fs/src/fs/os.rs
@@ -406,7 +406,7 @@ pub struct TemporaryFs {
     /// name passed in the [TemporaryFs::new] function
     pub working_directory: Utf8PathBuf,
     files: Vec<(Utf8PathBuf, String)>,
-    former_working_directory: Option<Utf8PathBuf>,
+    project_directory: Utf8PathBuf,
 }
 
 impl TemporaryFs {
@@ -424,9 +424,9 @@ impl TemporaryFs {
         let path = fs::canonicalize(path).unwrap();
 
         Self {
-            working_directory: Utf8PathBuf::from_path_buf(path).unwrap(),
+            working_directory: Utf8PathBuf::from_path_buf(path.clone()).unwrap(),
             files: Vec::new(),
-            former_working_directory: None,
+            project_directory: Utf8PathBuf::from_path_buf(path).unwrap(),
         }
     }
 
@@ -447,8 +447,7 @@ impl TemporaryFs {
     }
 
     pub fn append_to_working_directory(&mut self, path: &str) {
-        self.former_working_directory = Some(self.working_directory.clone());
-        self.working_directory = self.working_directory.join(path);
+        self.working_directory = self.project_directory.join(path);
     }
 
     /// Returns the path to use when running the CLI
@@ -465,15 +464,10 @@ impl TemporaryFs {
     /// will be stripped of the working directory path, making snapshots predictable.
     pub fn create_mem(&self) -> MemoryFileSystem {
         let mut fs = MemoryFileSystem::default();
-        let working_directory = if let Some(working_directory) = &self.former_working_directory {
-            working_directory.clone()
-        } else {
-            self.working_directory.clone()
-        };
         for (path, content) in self.files.iter() {
             fs.insert(
                 path.clone()
-                    .strip_prefix(working_directory.as_str())
+                    .strip_prefix(self.project_directory.as_str())
                     .expect("Working directory")
                     .to_path_buf(),
                 content.as_bytes(),
