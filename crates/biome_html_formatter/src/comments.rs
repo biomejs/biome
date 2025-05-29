690007eb18c6198e1dd3c2569eb5ec3064a5f546
Carson McManus <carson.mcmanus1@gmail.com>
2025-05-29 13:56:35 -0400

    chore(format/html): remove dead code around comment formatting

--- crates/biome_html_formatter/src/comments.rs
+++ crates/biome_html_formatter/src/comments.rs
@@ -3,13 +3,12 @@ use biome_formatter::{
     FormatRule,
     comments::{
         CommentKind, CommentPlacement, CommentStyle, Comments, DecoratedComment, SourceComment,
-        is_alignable_comment,
     },
     prelude::*,
     write,
 };
 use biome_html_syntax::HtmlLanguage;
-use biome_rowan::{SyntaxTriviaPieceComments, TextLen};
+use biome_rowan::SyntaxTriviaPieceComments;
 use biome_suppression::parse_suppression_comment;
 
 use crate::context::HtmlFormatContext;
@@ -17,57 +16,9 @@ use crate::context::HtmlFormatContext;
 pub type HtmlComments = Comments<HtmlLanguage>;
 
 #[derive(Default)]
-pub struct FormatHtmlLeadingComment;
+pub struct FormatHtmlComment;
 
-impl FormatRule<SourceComment<HtmlLanguage>> for FormatHtmlLeadingComment {
-    type Context = HtmlFormatContext;
-
-    fn fmt(
-        &self,
-        comment: &SourceComment<HtmlLanguage>,
-        f: &mut Formatter<Self::Context>,
-    ) -> FormatResult<()> {
-        if is_alignable_comment(comment.piece()) {
-            let mut source_offset = comment.piece().text_range().start();
-
-            let mut lines = comment.piece().text().lines();
-
-            // SAFETY: Safe, `is_alignable_comment` only returns `true` for multiline comments
-            let first_line = lines.next().unwrap();
-            write!(f, [dynamic_text(first_line.trim_end(), source_offset)])?;
-
-            source_offset += first_line.text_len();
-
-            // Indent the remaining lines by one space so that all `*` are aligned.
-            write!(
-                f,
-                [&format_once(|f| {
-                    for line in lines {
-                        write!(
-                            f,
-                            [
-                                hard_line_break(),
-                                text(" "),
-                                dynamic_text(line.trim(), source_offset)
-                            ]
-                        )?;
-
-                        source_offset += line.text_len();
-                    }
-
-                    Ok(())
-                })]
-            )
-        } else {
-            write!(f, [comment.piece().as_piece()])
-        }
-    }
-}
-
-#[derive(Default)]
-pub struct FormatHtmlTrailingComment;
-
-impl FormatRule<SourceComment<HtmlLanguage>> for FormatHtmlTrailingComment {
+impl FormatRule<SourceComment<HtmlLanguage>> for FormatHtmlComment {
     type Context = HtmlFormatContext;
 
     fn fmt(
