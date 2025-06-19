c0f6e12b4a019ba91ff631629176e785582077d9
vladimirivanov <vladoivan78@gmail.com>
2025-06-19 12:08:50 +0100

    feat(biome-js-analyse): added new option  to  rule to ignore unused function parameters that are siblings of the rest parameter.

--- crates/biome_rule_options/src/no_unused_function_parameters.rs
+++ crates/biome_rule_options/src/no_unused_function_parameters.rs
@@ -3,4 +3,8 @@ use serde::{Deserialize, Serialize};
 #[derive(Default, Clone, Debug, Deserialize, Deserializable, Eq, PartialEq, Serialize)]
 #[cfg_attr(feature = "schema", derive(schemars::JsonSchema))]
 #[serde(rename_all = "camelCase", deny_unknown_fields, default)]
-pub struct NoUnusedFunctionParametersOptions {}
+pub struct NoUnusedFunctionParametersOptions {
+    /// Whether to ignore unused variables from an object destructuring with a spread.
+    #[serde(default)]
+    pub ignore_rest_siblings: bool,
+}
