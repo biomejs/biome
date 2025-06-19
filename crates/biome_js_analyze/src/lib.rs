e546fe9ea98e92a62cef0134c48d14b63664fd2c
Arend van Beelen jr. <arend@arendjr.nl>
2025-06-19 18:28:05 +0200

    perf: preallocate type store (#6421)

--- crates/biome_js_analyze/src/lib.rs
+++ crates/biome_js_analyze/src/lib.rs
@@ -154,11 +154,7 @@ where
 
     let type_resolver = module_graph
         .module_info_for_path(file_path.as_ref())
-        .map(|module_info| {
-            let mut resolver = ModuleResolver::for_module(module_info, module_graph.clone());
-            resolver.run_inference();
-            resolver
-        })
+        .map(|module_info| ModuleResolver::for_module(module_info, module_graph.clone()))
         .map(Arc::new);
 
     services.insert_service(Arc::new(AriaRoles));
