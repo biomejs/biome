6b1753ea37bb2be3db03c686d8826f06da2b4e5b
Vojtech Miksu <vojtech@miksu.cz>
2025-06-26 16:19:22 -0700

    Grit: Failing test case for analyzing named imports

---
+++ crates/biome_grit_patterns/tests/specs/ts/detectButtonLinkImport.ts
@@ -0,0 +1,8 @@
+// Should match - ButtonLink is imported
+import { ButtonLink } from "geist/components";
+
+// Should match - ButtonLink is imported with other components
+import { Button, ButtonLink, Card } from "geist/components";
+
+// This actually matches, I guess not surprising?
+import ButtonLink from "geist/components";
