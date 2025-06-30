93f02e1b1027cd37e367665a29803f11aa0e14ef
Sébastien Règne <regseb@gmail.com>
2025-06-30 20:50:56 +0200

    remove bundler test

--- packages/@biomejs/js-api/tests/bundler.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, expect, it } from "vitest";
-import { Biome } from "../dist/bundler";
-
-describe("Biome for bundler", () => {
-	it("should export Biome", () => {
-		expect(Biome).not.toBeUndefined();
-	});
-});
