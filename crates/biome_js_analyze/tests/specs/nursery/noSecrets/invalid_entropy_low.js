81d0756112b9b454d8bedeb184c4044f86a337df
unvalley <kirohi.code@gmail.com>
2025-06-30 21:26:06 +0900

    test: update for entropy

---
+++ crates/biome_js_analyze/tests/specs/nursery/noSecrets/invalid_entropy_low.js
@@ -0,0 +1,5 @@
+// These should trigger with low entropy threshold
+const mediumEntropy1 = "abc123def456ghi";
+const mediumEntropy2 = "user2024Password";
+const mediumEntropy3 = "SecretKey123ABC";
+const mediumEntropy4 = "TokenXyZ98765";
