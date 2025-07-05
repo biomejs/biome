13d031b5d0946f112d310a4eb19a1d0fea79f50b
STerliakov <terlya.stas@gmail.com>
2025-07-05 02:20:05 +0200

    Prefer inline test to separate snapshot in semi-unrelated directory

--- crates/biome_js_analyze/tests/specs/correctness/noUnusedVariables/suppressionIgnoresComments.js
+++
@@ -1,6 +0,0 @@
-// This is a leading comment
-/* And this is too */
-
-  /** biome-ignore-all lint/correctness/noUnusedVariables: explanation */
-
-const test = "hello";
--- crates/biome_js_analyze/tests/specs/correctness/noUnusedVariables/suppressionIgnoresShebang.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bun
-
-/** biome-ignore-all lint/correctness/noUnusedVariables: explanation */
-
-const test = "hello";
