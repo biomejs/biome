c767c5e183aa7293b9ee6ec7392d2b0445412d5b
vladimir ivanov <vladoivan78@googlemail.com>
2025-07-07 08:58:40 +0100

    Update crates/biome_package/src/node_js_package/package_json.rs
    Co-authored-by: Emanuele Stoppa <my.burning@gmail.com>

--- crates/biome_package/src/node_js_package/package_json.rs
+++ crates/biome_package/src/node_js_package/package_json.rs
@@ -39,7 +39,7 @@ pub struct PackageJson {
     pub author: Option<JsonValue>,
     pub exports: Option<JsonValue>,
     pub imports: Option<JsonValue>,
-    pub main: Option<JsonValue>,
+    pub main: Option<String>,
     pub types: Option<JsonValue>,
 }
 
