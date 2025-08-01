2645a4265d421e304a1b58955476d73a0606ff69
Jake Leventhal <jakeleventhal@me.com>
2025-08-01 17:25:30 -0400

    Adding more tests for comments

--- crates/biome_js_analyze/tests/specs/nursery/useConsistentTypeDefinitions/invalid.ts
+++ crates/biome_js_analyze/tests/specs/nursery/useConsistentTypeDefinitions/invalid.ts
@@ -22,6 +22,10 @@ type Config = {
     retries: number;
 };
 
+type Comments /* comment 1 */ = /* comment 2 */ { /* comment 3 */
+    /* comment 4 */ lorem: /* comment 5 */ boolean; /* comment 6 */
+/* comment 7 */ };
+
 // These should be valid and not trigger errors
 type Union = { a: string } | { b: number };
 type Intersection = { a: string } & { b: number };
--- crates/biome_js_analyze/tests/specs/nursery/useConsistentTypeDefinitions/invalidType.ts
+++ crates/biome_js_analyze/tests/specs/nursery/useConsistentTypeDefinitions/invalidType.ts
@@ -23,6 +23,10 @@ interface Config {
     retries: number;
 }
 
+interface /* comment 1 */ Comments /* comment 2 */ { /* comment 3 */
+    /* comment 4 */ lorem:/* comment 5 */ boolean/* comment 6 */
+/* comment 7 */ }
+
 // These should not trigger errors as interfaces cannot represent these
 type Union = { a: string } | { b: number };
 type Intersection = { a: string } & { b: number };
