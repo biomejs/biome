34f749c7878a72120860369ddf0ccbe86799e9d3
Emanuele Stoppa <my.burning@gmail.com>
2025-08-19 13:24:32 +0100

    chore: update function

--- crates/biome_module_graph/src/js_module_info/utils.rs
+++ crates/biome_module_graph/src/js_module_info/utils.rs
@@ -2,10 +2,8 @@ use crate::js_module_info::JsModuleInfoDiagnostic;
 
 pub(crate) const MAX_NUM_TYPES: usize = 100_000;
 
-/// Checks the given `num_types` against a threshold and prints a warning to
-/// `stderr` if the threshold is reached.
-///
-/// Returns `true` when the threshold is reached.
+/// Checks the given `num_types` against a threshold and returns a diagnostic
+/// if the threshold is exceeded.
 pub(crate) fn reached_too_many_types(num_types: usize) -> Result<(), JsModuleInfoDiagnostic> {
     if num_types < MAX_NUM_TYPES {
         return Ok(());
