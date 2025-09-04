26ef6937f55690591ce8cdcf5620cbaa212a25e4
Santiago Garcia <santiago.garcia@posteo.de>
2025-09-04 16:03:55 +0200

    Very basic implementation

--- crates/biome_js_analyze/src/lint/nursery/no_jsx_props_bind.rs
+++ crates/biome_js_analyze/src/lint/nursery/no_jsx_props_bind.rs
@@ -1,49 +1,69 @@
-use biome_analyze::{Ast, Rule, RuleDiagnostic, context::RuleContext, declare_lint_rule};
+use biome_analyze::{
+    Ast, Rule, RuleDiagnostic, RuleDomain, RuleSource, context::RuleContext, declare_lint_rule,
+};
 use biome_console::markup;
-use biome_js_syntax::JsIdentifierBinding;
+use biome_js_syntax::{AnyJsExpression, JsxAttribute};
 use biome_rowan::AstNode;
 use biome_rule_options::no_jsx_props_bind::NoJsxPropsBindOptions;
 
 declare_lint_rule! {
-    /// Succinct description of the rule.
+    /// Disallow .bind() or function declaration in JSX props
     ///
-    /// Put context and details about the rule.
-    /// As a starting point, you can take the description of the corresponding _ESLint_ rule (if any).
+    /// Using `.bind()` on a function or declaring a function directly in props
+    /// creates a new function on every render, which is treated as a completely different function.
     ///
-    /// Try to stay consistent with the descriptions of implemented rules.
-    ///
-    /// ## Examples
     ///
     /// ### Invalid
     ///
     /// ```js,expect_diagnostic
-    /// var a = 1;
-    /// a = 2;
+    /// <Foo onClick={this._handleClick.bind(this)}></Foo>
+    /// ```
+    ///
+    /// ```js,expect_diagnostic
+    /// <Foo onClick={() => console.log('Hello!')}></Foo>
+    /// ```
+    ///
+    /// ```js,expect_diagnostic
+    /// <Foo onClick={function () { console.log('Hello!'); }}></Foo>
     /// ```
     ///
     /// ### Valid
     ///
     /// ```js
-    /// // var a = 1;
+    /// <Foo onClick={this._handleClick}></Foo>
     /// ```
     ///
     pub NoJsxPropsBind {
         version: "next",
         name: "noJsxPropsBind",
-        language: "js",
+        language: "jsx",
         recommended: false,
+        sources: &[RuleSource::EslintReact("jsx-no-bind").inspired()],
+        domains: &[RuleDomain::React],
     }
 }
 
 impl Rule for NoJsxPropsBind {
-    type Query = Ast<JsIdentifierBinding>;
+    type Query = Ast<JsxAttribute>;
     type State = ();
     type Signals = Option<Self::State>;
     type Options = NoJsxPropsBindOptions;
 
     fn run(ctx: &RuleContext<Self>) -> Self::Signals {
-        let _binding = ctx.query();
-        Some(())
+        let expression = ctx
+            .query()
+            .initializer()?
+            .value()
+            .ok()?
+            .as_jsx_expression_attribute_value()?
+            .expression()
+            .ok()?;
+        match expression {
+            AnyJsExpression::JsArrowFunctionExpression(_) => Some(()),
+            AnyJsExpression::JsFunctionExpression(_) => Some(()),
+            AnyJsExpression::JsCallExpression(_) => Some(()),
+            _ => None,
+        }
     }
 
     fn diagnostic(ctx: &RuleContext<Self>, _state: &Self::State) -> Option<RuleDiagnostic> {
