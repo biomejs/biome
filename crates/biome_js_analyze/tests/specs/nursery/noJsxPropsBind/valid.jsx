7451157d4bfe3ae2013c3d59493de57c1bc7e804
Santiago Garcia <santiago.garcia@posteo.de>
2025-09-07 13:23:34 +0200

    Add a testcase for useCallback

--- crates/biome_js_analyze/tests/specs/nursery/noJsxPropsBind/valid.jsx
+++ crates/biome_js_analyze/tests/specs/nursery/noJsxPropsBind/valid.jsx
@@ -1,5 +1,7 @@
 /* should not generate diagnostics */
 
+import { useCallback } from "react";
+
 function onClick() {}
 <Foo onClick={onClick} />
 
@@ -11,3 +13,8 @@ const onClick = () => {}
 
 <Foo onClick={this.handleClick}></Foo>;
 <Foo onClick={bind()}></Foo>;
+
+function Foo() {
+    const onClick = useCallback(() => {}, []);
+    return <Bar onClick={onClick} />
+}
