fab10f5dee0d91e2523f1423089b185d39fcb091
Marat Dulin <github-commit@maratd.com>
2025-09-20 14:17:34 +0200

    feat(lint): implement `noVueDuplicateKeys` rule

--- crates/biome_rule_options/src/lib.rs
+++ crates/biome_rule_options/src/lib.rs
@@ -237,6 +237,7 @@ pub mod no_void;
 pub mod no_void_elements_with_children;
 pub mod no_void_type_return;
 pub mod no_vue_data_object_declaration;
+pub mod no_vue_duplicate_keys;
 pub mod no_vue_reserved_keys;
 pub mod no_vue_reserved_props;
 pub mod no_with;
