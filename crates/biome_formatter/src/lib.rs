a464e7f4dcf4b51c04198821e38bf47f757aa31a
Arend van Beelen jr. <arend@arendjr.nl>
2025-09-22 17:00:27 +0200

    remove unused method

--- crates/biome_formatter/src/lib.rs
+++ crates/biome_formatter/src/lib.rs
@@ -916,10 +916,6 @@ impl<Context> Formatted<Context> {
     pub fn into_document(self) -> Document {
         self.document
     }
-
-    pub fn swap_document(&mut self, document: Document) {
-        self.document = document;
-    }
 }
 
 impl<Context> Formatted<Context>
