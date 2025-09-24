cadad2cadbd3852873cbd3f721c26ae7ceb3f39a
Marat Dulin <github-commit@maratd.com>
2025-09-24 21:59:11 +0200

    feat(lint): implement `noVueDuplicateKeys` rule (#7542)

--- packages/@biomejs/backend-jsonrpc/src/workspace.ts
+++ packages/@biomejs/backend-jsonrpc/src/workspace.ts
@@ -1698,6 +1698,10 @@ export interface Nursery {
 	 */
 	noVueDataObjectDeclaration?: RuleFixConfiguration_for_NoVueDataObjectDeclarationOptions;
 	/**
+	 * Disallow duplicate keys in Vue component data, methods, computed properties, and other options.
+	 */
+	noVueDuplicateKeys?: RuleConfiguration_for_NoVueDuplicateKeysOptions;
+	/**
 	 * Disallow reserved keys in Vue component data and computed properties.
 	 */
 	noVueReservedKeys?: RuleConfiguration_for_NoVueReservedKeysOptions;
@@ -3044,6 +3048,9 @@ export type RuleFixConfiguration_for_NoUselessUndefinedOptions =
 export type RuleFixConfiguration_for_NoVueDataObjectDeclarationOptions =
 	| RulePlainConfiguration
 	| RuleWithFixOptions_for_NoVueDataObjectDeclarationOptions;
+export type RuleConfiguration_for_NoVueDuplicateKeysOptions =
+	| RulePlainConfiguration
+	| RuleWithOptions_for_NoVueDuplicateKeysOptions;
 export type RuleConfiguration_for_NoVueReservedKeysOptions =
 	| RulePlainConfiguration
 	| RuleWithOptions_for_NoVueReservedKeysOptions;
@@ -5571,6 +5578,16 @@ export interface RuleWithFixOptions_for_NoVueDataObjectDeclarationOptions {
 	 * Rule's options
 	 */
 	options: NoVueDataObjectDeclarationOptions;
+}
+export interface RuleWithOptions_for_NoVueDuplicateKeysOptions {
+	/**
+	 * The severity of the emitted diagnostics by the rule
+	 */
+	level: RulePlainConfiguration;
+	/**
+	 * Rule's options
+	 */
+	options: NoVueDuplicateKeysOptions;
 }
 export interface RuleWithOptions_for_NoVueReservedKeysOptions {
 	/**
@@ -8157,6 +8174,7 @@ export interface NoUnusedExpressionsOptions {}
 export interface NoUselessCatchBindingOptions {}
 export interface NoUselessUndefinedOptions {}
 export interface NoVueDataObjectDeclarationOptions {}
+export interface NoVueDuplicateKeysOptions {}
 export interface NoVueReservedKeysOptions {}
 export interface NoVueReservedPropsOptions {}
 export type UseAnchorHrefOptions = null;
@@ -8908,6 +8926,7 @@ export type Category =
 	| "lint/nursery/noUselessCatchBinding"
 	| "lint/nursery/noUselessUndefined"
 	| "lint/nursery/noVueDataObjectDeclaration"
+	| "lint/nursery/noVueDuplicateKeys"
 	| "lint/nursery/noVueReservedKeys"
 	| "lint/nursery/noVueReservedProps"
 	| "lint/nursery/useAnchorHref"
