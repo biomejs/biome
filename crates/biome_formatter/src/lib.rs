202cdb5b726e1bcc69b7ac9246dc484afa8cd5ed
Anaïs Betts <anais@anaisbetts.org>
2025-09-26 17:59:12 +0200

    test(formatter): add test for auto line ending
    - Add test file with LF line endings
    - Add test configuration using auto line ending option
    - Add snapshot test for auto line ending behavior

--- crates/biome_formatter/src/lib.rs
+++ crates/biome_formatter/src/lib.rs
@@ -162,9 +162,13 @@ impl LineEnding {
             Self::Cr => "\r",
             Self::Auto => {
                 #[cfg(windows)]
-                { "\r\n" }
+                {
+                    "\r\n"
+                }
                 #[cfg(not(windows))]
-                { "\n" }
+                {
+                    "\n"
+                }
             }
         }
     }
