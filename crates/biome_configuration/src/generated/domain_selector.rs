4d6edfe7dcbd1d40e358b696e7c6db73b8d96c70
Emanuele Stoppa <my.burning@gmail.com>
2025-10-03 17:35:00 +0100

    chore: optimise code

--- crates/biome_configuration/src/generated/domain_selector.rs
+++ crates/biome_configuration/src/generated/domain_selector.rs
@@ -38,8 +38,6 @@ static QWIK_FILTERS: LazyLock<Vec<RuleFilter<'static>>> = LazyLock::new(|| {
         RuleFilter::Rule("nursery", "useAnchorHref"),
         RuleFilter::Rule("nursery", "useImageSize"),
         RuleFilter::Rule("nursery", "useQwikClasslist"),
-        RuleFilter::Rule("nursery", "useQwikMethodUsage"),
-        RuleFilter::Rule("nursery", "useQwikValidLexicalScope"),
         RuleFilter::Rule("suspicious", "noReactSpecificProps"),
     ]
 });
