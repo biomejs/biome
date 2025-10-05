4a90fd596fcfd6b6eeb5bd805bc5e6ac251eff7b
Emanuele Stoppa <my.burning@gmail.com>
2025-10-05 09:09:14 +0100

    chore: add tests

--- crates/biome_cli/tests/cases/html.rs
+++ crates/biome_cli/tests/cases/html.rs
@@ -331,3 +331,45 @@ fn should_apply_fixes_to_embedded_languages() {
         result,
     ));
 }
+
+#[test]
+fn should_lint_a_html_file() {
+    let fs = MemoryFileSystem::default();
+    let mut console = BufferConsole::default();
+
+    let html_file = Utf8Path::new("file.html");
+    fs.insert(
+        html_file.into(),
+        r#"<div scope="col"></div>
+"#
+        .as_bytes(),
+    );
+
+    fs.insert(
+        Utf8Path::new("biome.json").into(),
+        r#"{
+    "html": {
+        "linter": {
+            "enabled": true
+        }
+    }
+}"#
+        .as_bytes(),
+    );
+
+    let (fs, result) = run_cli(
+        fs,
+        &mut console,
+        Args::from(["lint", html_file.as_str()].as_slice()),
+    );
+
+    assert!(result.is_err(), "run_cli returned {result:?}");
+
+    assert_cli_snapshot(SnapshotPayload::new(
+        module_path!(),
+        "should_lint_a_html_file",
+        fs,
+        console,
+        result,
+    ));
+}
