5f9105dd1d1fb7e8a6222776a37d64d7b46a3fe0
Tom Anderson <tom@ando.sh>
2025-10-13 16:41:17 +1100

    fix(use-hook-at-top-level): check for hook invocation at module level
    Before performing hook call recursion checks, walk the call's ancestors
    searching for `AnyJsFunctionOrMethod` nodes. If one is not encountered,
    it is assumed the call is at the top of the file (or in a top-level
    expression statement), and the new `SuggestionKind::TopLevel` lint is
    generated.

---
+++ crates/biome_js_analyze/tests/specs/correctness/useHookAtTopLevel/eslintCompat.js
@@ -0,0 +1,34 @@
+// Invalid as hooks cannot be used at module level.
+useHook();
+
+/*
+// Invalid as hooks cannot be called in non-component functions.
+function notAComponent() {
+	useHook();
+}
+
+// Valid as hook is called in a component (by naming convention).
+function AComponent() {
+	useHook();
+}
+
+// Invalid as hooks cannot be called in non-hook functions.
+function notUseMyHook() {
+	useHook();
+}
+
+// Valid as hook is called in a hook (by naming convention).
+function useMyHook() {
+	useHook();
+}
+
+// Valid as hooks can be called within anonymous functions.
+test("the hook", () => {
+	useHook();
+});
+
+// Valid as hooks can be called within nested anonymous functions.
+test("more hook", () => {
+	renderHook(() => useHook());
+});
+*/
