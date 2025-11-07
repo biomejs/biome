b900d959334c411b7993582ae1e31c912375d8de
skearya <77034153+skearya@users.noreply.github.com>
2025-11-07 13:58:03 -0500

    feat: add the checkForEach option to useIterableCallbackReturn

--- packages/@biomejs/backend-jsonrpc/src/workspace.ts
+++ packages/@biomejs/backend-jsonrpc/src/workspace.ts
@@ -7124,26 +7124,31 @@ export interface NoUnknownAtRulesOptions {
 	 */
 	ignore?: string[];
 }
-export type NoUnsafeDeclarationMergingOptions = {};
-export type NoUnsafeNegationOptions = {};
-export type NoUselessEscapeInStringOptions = {};
-export type NoUselessRegexBackrefsOptions = {};
-export type NoVarOptions = {};
-export type NoWithOptions = {};
-export type UseAdjacentOverloadSignaturesOptions = {};
-export type UseAwaitOptions = {};
-export type UseBiomeIgnoreFolderOptions = {};
-export type UseDefaultSwitchClauseLastOptions = {};
-export type UseErrorMessageOptions = {};
-export type UseGetterReturnOptions = {};
-export type UseGoogleFontDisplayOptions = {};
-export type UseGuardForInOptions = {};
-export type UseIsArrayOptions = {};
-export type UseIterableCallbackReturnOptions = {};
-export type UseNamespaceKeywordOptions = {};
-export type UseNumberToFixedDigitsArgumentOptions = {};
-export type UseStaticResponseMethodsOptions = {};
-export type UseStrictModeOptions = {};
+export interface NoUnsafeDeclarationMergingOptions {}
+export interface NoUnsafeNegationOptions {}
+export interface NoUselessEscapeInStringOptions {}
+export interface NoUselessRegexBackrefsOptions {}
+export interface NoVarOptions {}
+export interface NoWithOptions {}
+export interface UseAdjacentOverloadSignaturesOptions {}
+export interface UseAwaitOptions {}
+export interface UseBiomeIgnoreFolderOptions {}
+export interface UseDefaultSwitchClauseLastOptions {}
+export interface UseErrorMessageOptions {}
+export interface UseGetterReturnOptions {}
+export interface UseGoogleFontDisplayOptions {}
+export interface UseGuardForInOptions {}
+export interface UseIsArrayOptions {}
+export interface UseIterableCallbackReturnOptions {
+	/**
+	 * When set to `false`, rule will skip reporting `forEach` callbacks that return a value.
+	 */
+	checkForEach?: boolean;
+}
+export interface UseNamespaceKeywordOptions {}
+export interface UseNumberToFixedDigitsArgumentOptions {}
+export interface UseStaticResponseMethodsOptions {}
+export interface UseStrictModeOptions {}
 export type ImportGroup = null | GroupMatcher | GroupMatcher[];
 export type Visibility = "public" | "package" | "private";
 /**
