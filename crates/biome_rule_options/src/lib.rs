ba7b0765894522a3436f00df9355255f8678f9d6
Carson McManus <dyc3@users.noreply.github.com>
2025-11-11 13:20:05 -0500

    feat(html/analyze): add useVueValidVBind (#8060)

--- crates/biome_rule_options/src/lib.rs
+++ crates/biome_rule_options/src/lib.rs
@@ -372,5 +372,6 @@ pub mod use_valid_lang;
 pub mod use_valid_typeof;
 pub mod use_vue_define_macros_order;
 pub mod use_vue_multi_word_component_names;
+pub mod use_vue_valid_v_bind;
 pub mod use_while;
 pub mod use_yield;
