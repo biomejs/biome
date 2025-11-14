69c9f83733fb948178382855edbbb33403e89ea1
Peter Wagenet <peter@wagenet.us>
2025-11-14 14:37:57 -0800

    Add docstrings

--- crates/biome_js_formatter/src/js/auxiliary/glimmer_template.rs
+++ crates/biome_js_formatter/src/js/auxiliary/glimmer_template.rs
@@ -6,6 +6,22 @@ use biome_js_syntax::{JsGlimmerTemplate, JsGlimmerTemplateFields};
 pub(crate) struct FormatJsGlimmerTemplate;
 
 impl FormatNodeRule<JsGlimmerTemplate> for FormatJsGlimmerTemplate {
+    /// Formats the fields of a `JsGlimmerTemplate` into the provided formatter.
+    ///
+    /// This writes the template token field of the node to `f`.
+    ///
+    /// # Examples
+    ///
+    /// ```no_run
+    /// use biome_js_formatter::js::auxiliary::FormatJsGlimmerTemplate;
+    /// use biome_js_syntax::JsGlimmerTemplate;
+    /// use biome_js_formatter::JsFormatter;
+    ///
+    /// let formatter = &mut JsFormatter::default();
+    /// let node: JsGlimmerTemplate = /* obtain or parse a Glimmer template node */;
+    /// let rule = FormatJsGlimmerTemplate::default();
+    /// rule.fmt_fields(&node, formatter).unwrap();
+    /// ```
     fn fmt_fields(&self, node: &JsGlimmerTemplate, f: &mut JsFormatter) -> FormatResult<()> {
         let JsGlimmerTemplateFields {
             template_token_token,
