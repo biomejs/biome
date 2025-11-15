741aad404c9752509f02024db19b8733edc3fad9
Maikel van Dort <maikel.van.dort@gmail.com>
2025-11-15 02:01:30 +0100

    feat(analyze): implement `noSyncScripts` (#8108)

--- crates/biome_cli/src/execute/migrate/eslint_any_rule_to_biome.rs
+++ crates/biome_cli/src/execute/migrate/eslint_any_rule_to_biome.rs
@@ -113,6 +113,18 @@ pub(crate) fn migrate_eslint_any_rule(
                 .get_or_insert(Default::default());
             rule.set_level(rule.level().max(rule_severity.into()));
         }
+        "@next/no-sync-scripts" => {
+            if !options.include_nursery {
+                results.add(eslint_name, eslint_to_biome::RuleMigrationResult::Nursery);
+                return false;
+            }
+            let group = rules.nursery.get_or_insert_with(Default::default);
+            let rule = group
+                .unwrap_group_as_mut()
+                .no_sync_scripts
+                .get_or_insert(Default::default());
+            rule.set_level(rule.level().max(rule_severity.into()));
+        }
         "@next/no-unwanted-polyfillio" => {
             let group = rules.performance.get_or_insert_with(Default::default);
             let rule = group
--- justfile
+++ justfile
@@ -144,10 +144,12 @@ test-lintrule name:
   just _touch crates/biome_json_analyze/tests/spec_tests.rs
   just _touch crates/biome_css_analyze/tests/spec_tests.rs
   just _touch crates/biome_graphql_analyze/tests/spec_tests.rs
+  just _touch crates/biome_html_analyze/tests/spec_tests.rs
   cargo test -p biome_js_analyze -- {{snakecase(name)}} --show-output
   cargo test -p biome_json_analyze -- {{snakecase(name)}} --show-output
   cargo test -p biome_css_analyze -- {{snakecase(name)}} --show-output
   cargo test -p biome_graphql_analyze -- {{snakecase(name)}} --show-output
+  cargo test -p biome_html_analyze -- {{snakecase(name)}} --show-output
 
 # Tests a lint rule. The name of the rule needs to be camel case
 test-transformation name:
