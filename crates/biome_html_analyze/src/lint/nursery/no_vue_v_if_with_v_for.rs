748e948166f9ec5685458dd633fe0060781d886a
Carson McManus <dyc3@users.noreply.github.com>
2025-11-18 12:00:12 -0500

    docs(lint/html): fix some html rule docs that mess up markdown rendering on the website (#8154)

--- crates/biome_html_analyze/src/lint/nursery/no_vue_v_if_with_v_for.rs
+++ crates/biome_html_analyze/src/lint/nursery/no_vue_v_if_with_v_for.rs
@@ -10,8 +10,8 @@ declare_lint_rule! {
     /// Disallow using `v-if` and `v-for` directives on the same element.
     ///
     /// There are two common cases where this can be tempting:
-    /// - To filter items in a list (e.g. v-for="user in users" v-if="user.isActive"). In these cases, replace users with a new computed property that returns your filtered list (e.g. activeUsers).
-    /// - To avoid rendering a list if it should be hidden (e.g. v-for="user in users" v-if="shouldShowUsers"). In these cases, move the v-if to a container element (e.g. ul, ol).
+    /// - To filter items in a list (e.g. `v-for="user in users" v-if="user.isActive"`). In these cases, replace users with a new computed property that returns your filtered list (e.g. activeUsers).
+    /// - To avoid rendering a list if it should be hidden (e.g. `v-for="user in users" v-if="shouldShowUsers"`). In these cases, move the v-if to a container element.
     ///
     /// ## Examples
     ///
