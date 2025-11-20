b3fde282c10e9a8b4bc93fea9068c3acb5b9d36a
Dibash Thapa <dibashthapa55@gmail.com>
2025-11-20 22:58:41 +0545

    fix(noLeakedRender): replaced same with inspired

--- crates/biome_cli/src/execute/migrate/eslint_any_rule_to_biome.rs
+++ crates/biome_cli/src/execute/migrate/eslint_any_rule_to_biome.rs
@@ -2514,6 +2514,10 @@ pub(crate) fn migrate_eslint_any_rule(
             rule.set_level(rule.level().max(rule_severity.into()));
         }
         "react/no-leaked-render" => {
+            if !options.include_inspired {
+                results.add(eslint_name, eslint_to_biome::RuleMigrationResult::Inspired);
+                return false;
+            }
             if !options.include_nursery {
                 results.add(eslint_name, eslint_to_biome::RuleMigrationResult::Nursery);
                 return false;
--- crates/biome_js_analyze/src/lint/nursery/no_leaked_render.rs
+++ crates/biome_js_analyze/src/lint/nursery/no_leaked_render.rs
@@ -91,7 +91,7 @@ declare_lint_rule! {
         language: "jsx",
         domains: &[RuleDomain::React],
         sources: &[
-            RuleSource::EslintReact("no-leaked-render").same(),
+            RuleSource::EslintReact("no-leaked-render").inspired(),
         ],
         recommended: false,
     }
