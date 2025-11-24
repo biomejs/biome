4cc8563ce134cac2bf50c327a7585e53f434abe8
Cormac Relf <web@cormacrelf.net>
2025-11-24 18:04:59 +1100

    Improve path handling in e2e tests, now can run from workspace root

--- e2e-tests/test-all.sh
+++ e2e-tests/test-all.sh
@@ -1,12 +1,19 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 set -eu
 
+cd "$(dirname "$0")"
+if [[ -z "${1:-}" ]]; then FILTER="*"; else FILTER="*$1*"; fi
+
 for x in *; do
     if test -d "$x"; then
+			if [[ "$x" != $FILTER ]]; then
+				echo "Skipping $x"
+				continue
+			fi
         echo "Testing $x..."
-        cd "$x"
-        sh test.sh
-        cd ..
+        pushd "$x"
+        bash test.sh
+        popd
     fi
 done
