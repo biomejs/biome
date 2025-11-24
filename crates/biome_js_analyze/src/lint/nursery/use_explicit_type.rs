37410a9bec37fe438b95a0a28f8b0786a8e72314
arnold095 <arnold9525@gmail.com>
2025-11-24 21:57:25 +0100

    refactor(lint): simplify conditional

--- crates/biome_js_analyze/src/lint/nursery/use_explicit_type.rs
+++ crates/biome_js_analyze/src/lint/nursery/use_explicit_type.rs
@@ -1254,16 +1254,15 @@ fn parameter_has_not_type(parameter: &JsFormalParameter) -> Option<State> {
         }
     } else {
         // If parameter has an initializer with a trivially inferrable type, allow it
-        if let Some(initializer) = parameter.initializer() {
-            if let Ok(expr) = initializer.expression() {
-                let expr = expr.omit_parentheses();
-                // For parameters, we're strict about placeholders (null/undefined)
-                if is_allowed_in_untyped_expression(&expr, false) {
-                    return None;
-                }
+        if let Some(initializer) = parameter.initializer()
+            && let Ok(expr) = initializer.expression()
+        {
+            let expr = expr.omit_parentheses();
+            if is_allowed_in_untyped_expression(&expr, false) {
+                return None;
             }
         }
-        
+
         Some((parameter.range(), ViolationKind::UntypedParameter))
     }
 }
