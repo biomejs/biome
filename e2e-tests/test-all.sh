f6a33bef071f74da41ea4c9b543bf4f3486b5b2a
Cormac Relf <web@cormacrelf.net>
2025-11-26 09:54:23 +1100

    Improve e2e test runner, can now run from workspace root and filter tests
    This also runs the tests with bash, not sh. So we can do more advanced shell scripts
    in the various test.sh.

--- e2e-tests/test-all.sh
+++ e2e-tests/test-all.sh
@@ -1,12 +1,35 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
+# Usage:
+#  ./test-all.sh
+#  ./test-all.sh stdin   # to filter tests by name
 
 set -eu
 
+# Change to the script's directory
+cd "$(dirname "$0")"
+
+# Glob matcher
+if [[ -z "${1:-}" ]]; then FILTER="*"; else FILTER="*$1*"; fi
+
+redecho() {
+    echo -e "\033[1;31m$1\033[0m"
+}
+
+bail() {
+    redecho "Error: $1"
+    exit 1
+}
+
 for x in *; do
     if test -d "$x"; then
+        if [[ "$x" != $FILTER ]]; then
+            echo "Skipping $x"
+            continue
+        fi
         echo "Testing $x..."
-        cd "$x"
-        sh test.sh
-        cd ..
+        pushd "$x" > /dev/null
+        bash test.sh || bail "Test failed: $x. To re-run: $0 $x"
+        popd > /dev/null
     fi
 done
