92ba92a489df81f59a166be29065705573603a7e
Justin Poehnelt <jpoehnelt@google.com>
2025-11-25 13:07:50 -0700

    feat: Add support for Google Apps Script (`.gs`) files by recognizing the extension

--- crates/biome_js_syntax/src/file_source.rs
+++ crates/biome_js_syntax/src/file_source.rs
@@ -352,6 +352,7 @@ impl JsFileSource {
             "vue" => Ok(Self::vue()),
             // TODO: Remove once we have full support of svelte files
             "svelte" => Ok(Self::svelte()),
+            "gs" => Ok(Self::js_module()),
 
             _ => Err(FileSourceError::UnknownExtension),
         }
@@ -385,6 +386,7 @@ impl JsFileSource {
             "vue" | "vuejs" => Ok(Self::vue()),
             // TODO: Remove once we have full support of svelte files
             "svelte" => Ok(Self::svelte()),
+            "appsscript" => Ok(Self::js_module()),
             _ => Err(FileSourceError::UnknownLanguageId),
         }
     }
