2df62eddac545006cc2b17140520084b39bd024a
Maikel <maikel.van.dort@gmail.com>
2025-12-27 17:58:41 +0100

    feat(graphql_analyze): implement useUniqueVariableNames

---
+++ .changeset/public-walls-buy.md
@@ -0,0 +1,5 @@
+---
+"@biomejs/biome": patch
+---
+
+Added the nursery rule [`useUniqueVariableNames`](https://biomejs.dev/linter/rules/use-unique-variable-names/). Enforce unique variable names for GraphQL operations.
--- crates/biome_graphql_analyze/src/lint/nursery.rs
+++ crates/biome_graphql_analyze/src/lint/nursery.rs
@@ -7,4 +7,5 @@ pub mod no_empty_source;
 pub mod use_consistent_graphql_descriptions;
 pub mod use_deprecated_date;
 pub mod use_unique_graphql_operation_name;
-declare_lint_group! { pub Nursery { name : "nursery" , rules : [self :: no_empty_source :: NoEmptySource , self :: use_consistent_graphql_descriptions :: UseConsistentGraphqlDescriptions , self :: use_deprecated_date :: UseDeprecatedDate , self :: use_unique_graphql_operation_name :: UseUniqueGraphqlOperationName ,] } }
+pub mod use_unique_variable_names;
+declare_lint_group! { pub Nursery { name : "nursery" , rules : [self :: no_empty_source :: NoEmptySource , self :: use_consistent_graphql_descriptions :: UseConsistentGraphqlDescriptions , self :: use_deprecated_date :: UseDeprecatedDate , self :: use_unique_graphql_operation_name :: UseUniqueGraphqlOperationName , self :: use_unique_variable_names :: UseUniqueVariableNames ,] } }
