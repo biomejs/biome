640979b2b75675607004f604c86bbb3d5339f47e
Bertie690 <taylormw163@gmail.com>
2025-12-30 16:02:03 -0500

    docs(lint): update documentation, rule source for `lint/complexity/noBannedTypes`

--- crates/biome_js_analyze/src/lint/complexity/no_banned_types.rs
+++ crates/biome_js_analyze/src/lint/complexity/no_banned_types.rs
@@ -36,29 +36,32 @@ declare_lint_rule! {
     /// ### Disallow the unsafe `Function` type
     ///
     /// TypeScript's built-in `Function` type is capable of accepting callbacks of any shape or form,
-    /// behaving equivalent to `(...rest: any[]) => any` (which uses the unsafe `any` type) when called directly.
+    /// behaving equivalent to `(...rest: any[]) => any` (which uses the unsafe `any` type) when called directly. \
     /// It also accepts classes or plain objects that happen to possess all properties of the `Function` class,
     /// which is likewise a potential source of confusion.
     ///
     /// As such, it is almost always preferable to explicitly specify function parameters and return types where possible. \
     /// When a generic "catch-all" callback type is required, one of the following can be used instead:
     /// - `() => void`: A function that accepts no parameters and whose return value is ignored
-    /// - `(...args: never) => unknown`: A "top type" for functions that can be assigned any function type,
+    /// - `(...args: never) => unknown`: A "top type" for functions that can be _assigned_ any function type,
     ///    but can't be called directly
     ///
     /// ### Disallow the misleading empty object type `{}`
-    /// In TypeScript, the type `{}` _doesn't_ represent an empty object (as many new to the language may assume).
-    /// It actually accepts any non-nullish value, _including non-object primitives_.
-    /// The following TypeScript example is thus perfectly valid:
+    /// `{}`, also known as the "empty object" type, _doesn't_ actually represent an empty object (despite what many new to TypeScript may assume).
+    /// It actually accepts _any non-nullish value_, including non-object primitives like numbers and strings[^1]. \
+    /// The following example is thus perfectly valid TypeScript:
     ///
-    /// ```ts,expect_diagnostic
+    /// ```ts,ignore
     /// const n: {} = 0;
     /// ```
     ///
     /// Often, developers writing `{}` actually mean one of the following:
     /// - `object`: Represents any object value
     /// - `unknown`: Represents any value at all, including `null` and `undefined`
-    /// - `{ [k: string]: never }` or `Record<string, never>`: Represent object types that disallow property access
+    /// - `{ [k: keyof any]: never }` or `Record<keyof any, never>`: Represent object types whose properties are all of type `never` (and cannot be used)
+    /// - `{ [myUniqueInternalSymbol]?: never }`: Represents an object type whose only "property" is an unexported `unique symbol`, thereby forcing external consumers to omit it[^2]. \
+    ///   This can be used as a type guard for use in `extends` clauses or a type annotation for use in [excess property checks](https://www.typescriptlang.org/docs/handbook/2/objects.html#excess-property-checks),
+    ///   both with their own respective use cases and pitfalls.
     ///
     /// To avoid confusion, this rule forbids the use of the type `{}`, except in two situations:
     ///
@@ -149,11 +152,20 @@ declare_lint_rule! {
     /// type notNull<T> = T & {};
     /// ```
     ///
+    /// [^1]: For those curious, this occurs as a natural consequence of TypeScript's type system being _structural_ instead of nominal.
+    /// `{}`, instead of requiring compatible types have _exactly_ 0 properties, simply requires they have _0 or more_ properties.
+    /// This is the exact same mechanism that allows passing `{ foo: number, bar: string }`
+    /// to a function expecting `{ bar: number }` (albeit much more surprising).
+    /// [^2]: In this case, you'd write `declare const myUniqueInternalSymbol: unique symbol` somewhere in the same file.
     pub NoBannedTypes {
         version: "1.0.0",
         name: "noBannedTypes",
         language: "ts",
-        sources: &[RuleSource::EslintTypeScript("ban-types").same()],
+        sources: &[
+            RuleSource::EslintTypeScript("no-empty-object-type").same()
+            RuleSource::EslintTypeScript("no-wrapper-object-types").same()
+            RuleSource::EslintTypeScript("no-unsafe-function-type").same()
+        ],
         recommended: true,
         severity: Severity::Warning,
         fix_kind: FixKind::Safe,
