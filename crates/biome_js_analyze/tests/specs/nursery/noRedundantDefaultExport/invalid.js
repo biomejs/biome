79110d1b42aff542743d483f00e2a145b05f6c19
koshin <ekoshin2001@icloud.com>
2026-01-04 03:03:53 +0900

    Add valid case

--- crates/biome_js_analyze/tests/specs/nursery/noRedundantDefaultExport/invalid.js
+++ crates/biome_js_analyze/tests/specs/nursery/noRedundantDefaultExport/invalid.js
@@ -1,4 +1,26 @@
-/* should generate diagnostics */
-var a = 1;
-a = 2;
-a = 3;
+export const foo = 42;
+export default foo;
+
+export function bar() {
+  return true;
+}
+export default bar;
+
+export class Baz {
+  constructor() {}
+}
+export default Baz;
+
+export const x = 1;
+export const y = 2;
+export default x;
+
+export const handler = () => {};
+export default handler;
+
+export const config = { a: 1 };
+export default config;
+
+export const value = 100;
+const anotherValue = 200;
+export default value;
