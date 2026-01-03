1df92a0beb6ba1f7bb17addc017ff41ca8e4ba47
Emanuele Stoppa <my.burning@gmail.com>
2026-01-03 14:12:18 +0000

    add one more test

--- crates/biome_cli/tests/cases/configuration.rs
+++ crates/biome_cli/tests/cases/configuration.rs
@@ -108,3 +108,51 @@ fn can_read_configuration_from_user_home() {
         result,
     ));
 }
+
+#[test]
+fn uses_project_config_before_user_config() {
+    let fs = MemoryFileSystem::default();
+    let mut console = BufferConsole::default();
+
+    let file_path = Utf8Path::new("src/index.js");
+    fs.insert(file_path.into(), "a['b']  =  42;".as_bytes());
+
+    let config_path = Utf8Path::new(".biome.json");
+    fs.insert(
+        config_path.into(),
+        r#"{
+  "javascript": {
+    "formatter": {
+      "quoteStyle": "single"
+    }
+  }
+}"#
+        .as_bytes(),
+    );
+
+    let config_dir = fs.user_config_dir().expect("config dir to exist");
+    let config_file = config_dir.join("biome.json");
+    fs.insert(
+        config_file,
+        r#"{
+  "javascript": {
+    "formatter": {
+      "enabled": false
+    }
+  }
+}"#
+        .as_bytes(),
+    );
+
+    let (fs, result) = run_cli(fs, &mut console, Args::from(["check"].as_slice()));
+
+    assert!(result.is_err(), "run_cli returned {result:?}");
+
+    assert_cli_snapshot(SnapshotPayload::new(
+        module_path!(),
+        "uses_project_config_before_user_config",
+        fs,
+        console,
+        result,
+    ));
+}
