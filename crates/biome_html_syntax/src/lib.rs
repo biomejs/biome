06ffb3f86efcb36f10b83fd350e2ef38bf182e17
Emanuele Stoppa <my.burning@gmail.com>
2026-01-05 09:39:59 +0000

    feat(html/analyze) add useValidLang

--- crates/biome_html_syntax/src/lib.rs
+++ crates/biome_html_syntax/src/lib.rs
@@ -2,10 +2,12 @@
 
 #[macro_use]
 mod attr_ext;
+pub mod attribute_ext;
 pub mod element_ext;
 mod file_source;
 mod generated;
 mod script_type;
+pub mod static_value;
 mod string_ext;
 mod syntax_node;
 
