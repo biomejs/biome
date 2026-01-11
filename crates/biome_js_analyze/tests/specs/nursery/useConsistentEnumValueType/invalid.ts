d082c0d85b1a38aeea131cd9b59a0b86a958c84b
Netail <maikel.van.dort@gmail.com>
2026-01-11 17:55:29 +0100

    feat(js_analyze): implement useConsistentEnumValueType

---
+++ crates/biome_js_analyze/tests/specs/nursery/useConsistentEnumValueType/invalid.ts
@@ -0,0 +1,6 @@
+/* should generate diagnostics */
+enum Invalid1 {
+	Unknown,
+	Closed = 1,
+	Open = 'open',
+}
---
+++ crates/biome_js_analyze/tests/specs/nursery/useConsistentEnumValueType/valid.ts
@@ -0,0 +1,18 @@
+/* should not generate diagnostics */
+enum Valid1 {
+	Unknown = 0,
+	Closed = 1,
+	Open = 2,
+}
+
+enum Valid2 {
+	Unknown,
+	Closed,
+	Open,
+}
+
+enum Valid3 {
+	Unknown = 'unknown',
+	Closed = 'closed',
+	Open = 'open',
+}
