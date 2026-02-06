68bb9dc7602931f559562de78c971f2024a03ac6
Vinh Tran <daivinhtran.vt@gmail.com>
2026-02-06 22:04:13 +0700

    reproduce deprecation in next branch

--- crates/biome_html_formatter/src/html/any/attribute_initializer.rs
+++ crates/biome_html_formatter/src/html/any/attribute_initializer.rs
@@ -1,32 +1,15 @@
 //! This is a generated file. Don't modify it by hand! Run 'cargo codegen formatter' to re-generate the file.
 
 use crate::prelude::*;
-use biome_formatter::FormatRuleWithOptions;
 use biome_html_syntax::AnyHtmlAttributeInitializer;
 #[derive(Debug, Clone, Default)]
-pub(crate) struct FormatAnyHtmlAttributeInitializer {
-    compact: bool,
-}
-
+pub(crate) struct FormatAnyHtmlAttributeInitializer;
 impl FormatRule<AnyHtmlAttributeInitializer> for FormatAnyHtmlAttributeInitializer {
     type Context = HtmlFormatContext;
     fn fmt(&self, node: &AnyHtmlAttributeInitializer, f: &mut HtmlFormatter) -> FormatResult<()> {
         match node {
-            AnyHtmlAttributeInitializer::HtmlSingleTextExpression(node) => {
-                node.format().with_options(self.compact).fmt(f)
-            }
-            AnyHtmlAttributeInitializer::HtmlString(node) => {
-                node.format().with_options(self.compact).fmt(f)
-            }
+            AnyHtmlAttributeInitializer::HtmlSingleTextExpression(node) => node.format().fmt(f),
+            AnyHtmlAttributeInitializer::HtmlString(node) => node.format().fmt(f),
         }
-    }
-}
-
-impl FormatRuleWithOptions<AnyHtmlAttributeInitializer> for FormatAnyHtmlAttributeInitializer {
-    type Options = bool;
-
-    fn with_options(mut self, options: Self::Options) -> Self {
-        self.compact = options;
-        self
     }
 }
--- crates/biome_html_formatter/src/svelte/any/binding_property.rs
+++ crates/biome_html_formatter/src/svelte/any/binding_property.rs
@@ -1,33 +1,15 @@
 //! This is a generated file. Don't modify it by hand! Run 'cargo codegen formatter' to re-generate the file.
 
 use crate::prelude::*;
-use biome_formatter::FormatRuleWithOptions;
 use biome_html_syntax::AnySvelteBindingProperty;
 #[derive(Debug, Clone, Default)]
-pub(crate) struct FormatAnySvelteBindingProperty {
-    /// Whether it should be formatted in compact mode. In compact mode, all tokens and children
-    /// are removed
-    pub compact: bool,
-}
+pub(crate) struct FormatAnySvelteBindingProperty;
 impl FormatRule<AnySvelteBindingProperty> for FormatAnySvelteBindingProperty {
     type Context = HtmlFormatContext;
     fn fmt(&self, node: &AnySvelteBindingProperty, f: &mut HtmlFormatter) -> FormatResult<()> {
         match node {
-            AnySvelteBindingProperty::SvelteLiteral(node) => {
-                node.format().with_options(self.compact).fmt(f)
-            }
-            AnySvelteBindingProperty::SvelteName(node) => {
-                node.format().with_options(self.compact).fmt(f)
-            }
+            AnySvelteBindingProperty::SvelteLiteral(node) => node.format().fmt(f),
+            AnySvelteBindingProperty::SvelteName(node) => node.format().fmt(f),
         }
-    }
-}
-
-impl FormatRuleWithOptions<AnySvelteBindingProperty> for FormatAnySvelteBindingProperty {
-    type Options = bool;
-
-    fn with_options(mut self, options: Self::Options) -> Self {
-        self.compact = options;
-        self
     }
 }
