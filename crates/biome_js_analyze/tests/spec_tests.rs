e1d71e5eacda5f3dabfb87faaa1295a95a1ccf58
Emanuele Stoppa <my.burning@gmail.com>
2026-02-13 18:37:20 +0000

    fix linting

--- crates/biome_js_analyze/tests/spec_tests.rs
+++ crates/biome_js_analyze/tests/spec_tests.rs
@@ -1,3 +1,4 @@
+#[expect(clippy::large_stack_arrays)]
 use biome_analyze::{
     AnalysisFilter, AnalyzerAction, AnalyzerPluginSlice, ControlFlow, Never, Queryable,
     RegistryVisitor, Rule, RuleDomain, RuleFilter, RuleGroup,
