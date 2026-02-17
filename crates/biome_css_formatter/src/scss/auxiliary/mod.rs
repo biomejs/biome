aa2e89242772c7d4e86260a72d4011542deb1b43
Denis Bezrukov <6227442+denbezrukov@users.noreply.github.com>
2026-02-17 11:16:51 +0200

    feat(css): support SCSS qualified names in values and function calls (#9096)
    (cherry picked from commit 11784e503d393ed2e3ea16df0a9972cf7df49fa2)

--- crates/biome_css_formatter/src/scss/auxiliary/mod.rs
+++ crates/biome_css_formatter/src/scss/auxiliary/mod.rs
@@ -1,4 +1,5 @@
 //! This is a generated file. Don't modify it by hand! Run 'cargo codegen formatter' to re-generate the file.
 
 pub(crate) mod declaration;
+pub(crate) mod qualified_name;
 pub(crate) mod variable_modifier;
