ecbd3f5e89ca63291ac47d7f3022676f1e85dffc
Emanuele Stoppa <my.burning@gmail.com>
2026-02-21 09:29:44 +0000

    now paths are normalized

--- crates/biome_service/src/workspace.tests.rs
+++ crates/biome_service/src/workspace.tests.rs
@@ -403,7 +403,7 @@ fn files_loaded_by_the_scanner_are_only_unloaded_when_the_project_is_unregistere
                         .clone()),
                 Some(BTreeMap::from([(
                     "./b.ts".to_string(),
-                    "/project/b.ts".replace('/', std::path::MAIN_SEPARATOR_STR),
+                    "/project/b.ts",
                 )])),
             );
         }};
