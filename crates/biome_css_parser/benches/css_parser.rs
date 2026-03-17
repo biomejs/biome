4692d1fefdb94fbf86bf9a2600a1f2dc361edbb8
Nicolas Hedger <nicolas@hedger.ch>
2026-03-17 14:49:23 +0100

    perf: use mimalloc for musl

--- crates/biome_css_parser/benches/css_parser.rs
+++ crates/biome_css_parser/benches/css_parser.rs
@@ -19,10 +19,10 @@ static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
 #[global_allocator]
 static GLOBAL: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
 
-// Jemallocator does not work on aarch64 with musl, so we'll use the system allocator instead
-#[cfg(all(target_env = "musl", target_os = "linux", target_arch = "aarch64"))]
+#[cfg(all(target_os = "linux", target_env = "musl"))]
 #[global_allocator]
-static GLOBAL: std::alloc::System = std::alloc::System;
+static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
+
 fn bench_css_parser(criterion: &mut Criterion) {
     let mut all_suites = HashMap::new();
     all_suites.insert("css", include_str!("libs-css.txt"));
--- crates/biome_js_analyze/benches/use_sorted_classes_parser.rs
+++ crates/biome_js_analyze/benches/use_sorted_classes_parser.rs
@@ -12,10 +12,9 @@ static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
 #[global_allocator]
 static GLOBAL: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
 
-// Jemallocator does not work on aarch64 with musl, so we'll use the system allocator instead
-#[cfg(all(target_env = "musl", target_os = "linux", target_arch = "aarch64"))]
+#[cfg(all(target_os = "linux", target_env = "musl"))]
 #[global_allocator]
-static GLOBAL: std::alloc::System = std::alloc::System;
+static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
 
 /// Benchmark for the Tailwind CSS class parser in `use_sorted_classes` rule,
 /// ported from divan to criterion.
