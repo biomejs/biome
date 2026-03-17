4b641457bb4555dbc25d081effec28a66b0f1272
Emanuele Stoppa <my.burning@gmail.com>
2026-03-17 21:56:14 +0000

    fix(core): idempotence formatting html-ish files (#9532)

--- crates/biome_service/src/file_handlers/html.rs
+++ crates/biome_service/src/file_handlers/html.rs
@@ -1595,24 +1595,27 @@ pub(crate) fn fix_all(params: FixAllParams) -> Result<FixFileResult, WorkspaceEr
         })?;
 
         if result.is_none() {
-            return process_fix_all.finish(|| {
-                Ok(if params.should_format {
-                    Either::Left(format_node(
-                        params.settings.format_options::<HtmlLanguage>(
-                            params.biome_path,
-                            &params.document_file_source,
-                        ),
-                        tree.syntax(),
-                        // NOTE: this is important that stays false. In this instance, the formatting of embedded
-                        // nodes has already happened, because the workspace during fix_all() process the embedded nodes
-                        // first, and then the root document. This means the embedded nodes don't need to be formatted and can
-                        // be printed verbatim by the formatter.
-                        false,
-                    ))
-                } else {
-                    Either::Right(tree.syntax().to_string())
-                })
-            });
+            return process_fix_all.finish(
+                || {
+                    Ok(if params.should_format {
+                        Either::Left(format_node(
+                            params.settings.format_options::<HtmlLanguage>(
+                                params.biome_path,
+                                &params.document_file_source,
+                            ),
+                            tree.syntax(),
+                            // NOTE: this is important that stays false. In this instance, the formatting of embedded
+                            // nodes has already happened, because the workspace during fix_all() process the embedded nodes
+                            // first, and then the root document. This means the embedded nodes don't need to be formatted and can
+                            // be printed verbatim by the formatter.
+                            false,
+                        ))
+                    } else {
+                        Either::Right(tree.syntax().to_string())
+                    })
+                },
+                params.embeds_initial_indent,
+            );
         }
     }
 }
