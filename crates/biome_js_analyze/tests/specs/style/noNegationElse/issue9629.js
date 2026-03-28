7a0ae0e9089471a2652f3f75ffbed9ed85525a59
Raashish Aggarwal <94279692+raashish1601@users.noreply.github.com>
2026-03-28 21:30:06 +0530

    test(lint): use enum-style issue9629 regression

--- crates/biome_js_analyze/tests/specs/style/noNegationElse/issue9629.js
+++ crates/biome_js_analyze/tests/specs/style/noNegationElse/issue9629.js
@@ -1,6 +1,10 @@
-/*before-not*/!/*after-not*/condition/*after-test*/
-	? /*consequent-leading*/ consequent /*consequent-trailing*/
-	: /*alternate-leading*/ alternate /*alternate-trailing*/;
+const keyName =
+	obj[val] !== undefined
+		? // TS reverse mapped enum
+			obj[val]
+		: // Normal enum / `const object`
+			// TODO: Figure out a way to cache the names of commonly-used enum numbers for performance if needed
+			testEnumValueToKey(obj, val);
 
 call(
 	!condition
