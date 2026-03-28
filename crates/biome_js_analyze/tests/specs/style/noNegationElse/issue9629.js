294cf2b8c7925070f1eac5cf58a37864e2f5b3b0
Raashish Aggarwal <94279692+raashish1601@users.noreply.github.com>
2026-03-28 08:06:39 +0530

    test(lint): cover noNegationElse alternate suffix trivia

--- crates/biome_js_analyze/tests/specs/style/noNegationElse/issue9629.js
+++ crates/biome_js_analyze/tests/specs/style/noNegationElse/issue9629.js
@@ -1,3 +1,9 @@
 /*before-not*/!/*after-not*/condition/*after-test*/
 	? /*consequent-leading*/ consequent /*consequent-trailing*/
 	: /*alternate-leading*/ alternate /*alternate-trailing*/;
+
+call(
+	!condition
+		? consequent /*consequent-trailing*/
+		: alternate
+);
