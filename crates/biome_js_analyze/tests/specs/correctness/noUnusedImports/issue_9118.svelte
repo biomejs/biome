82f4f09fe0c596839b7407086c86b4f21029fb9e
Raashish Aggarwal <94279692+raashish1601@users.noreply.github.com>
2026-03-29 06:37:55 +0530

    test(js): add noUnusedImports regression for Svelte bind:this

---
+++ crates/biome_js_analyze/tests/specs/correctness/noUnusedImports/issue_9118.svelte
@@ -0,0 +1,8 @@
+<!-- should not generate diagnostics -->
+<script lang="ts">
+import MyElement, { type MyElementShape } from "./my-element.svelte";
+
+let myElement = $state<MyElementShape>();
+</script>
+
+<MyElement bind:this={myElement} />
