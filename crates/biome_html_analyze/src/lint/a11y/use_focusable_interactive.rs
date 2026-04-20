dc85b02f3924a9d7f6c88ca2856146f3d131a9d3
Netail <maikel.van.dort@gmail.com>
2026-04-20 21:16:54 +0200

    feat(html_analyze): implement useFocusableInteractive

---
+++ crates/biome_html_analyze/src/lint/a11y/use_focusable_interactive.rs
@@ -0,0 +1,95 @@
+use biome_analyze::{Rule, RuleDiagnostic, context::RuleContext, declare_lint_rule};
+use biome_aria_metadata::AriaRole;
+use biome_console::markup;
+use biome_diagnostics::Severity;
+use biome_html_syntax::element_ext::AnyHtmlTagElement;
+use biome_rowan::AstNode;
+use biome_rule_options::use_focusable_interactive::UseFocusableInteractiveOptions;
+
+use crate::Aria;
+
+declare_lint_rule! {
+    /// Elements with an interactive role and interaction handlers must be focusable.
+    ///
+    /// HTML elements with interactive roles must have `tabindex` defined to ensure they are
+    /// focusable. Without `tabindex`, assistive technologies may not recognize these elements as
+    /// interactive.
+    /// You could also consider switching from an interactive role to its semantic HTML element
+    /// instead.
+    ///
+    /// ## Examples
+    ///
+    /// ### Invalid
+    ///
+    /// ```html,expect_diagnostic
+    /// <div role="button"></div>
+    /// ```
+    ///
+    /// ```html,expect_diagnostic
+    /// <div role="tab"></div>
+    /// ```
+    ///
+    /// ### Valid
+    ///
+    /// ```html
+    /// <div role="button" tabindex="0"></div>
+    /// ```
+    ///
+    /// ```html
+    /// <div></div>
+    /// ```
+    ///
+    pub UseFocusableInteractive {
+        version: "next",
+        name: "useFocusableInteractive",
+        language: "html",
+        recommended: true,
+        severity: Severity::Error,
+    }
+}
+
+impl Rule for UseFocusableInteractive {
+    type Query = Aria<AnyHtmlTagElement>;
+    type State = Box<str>;
+    type Signals = Option<Self::State>;
+    type Options = UseFocusableInteractiveOptions;
+
+    fn run(ctx: &RuleContext<Self>) -> Self::Signals {
+        let node = ctx.query();
+
+        if node.is_custom_component() {
+            return None;
+        }
+
+        if ctx.aria_roles().is_not_interactive_element(node) {
+            let role_attribute = node.find_attribute_by_name("role");
+            if let Some(role_attribute) = role_attribute {
+                let tabindex_attribute = node.find_attribute_by_name("tabindex");
+                let role_attribute_value = role_attribute.initializer()?.value().ok()?;
+                let role = AriaRole::from_roles(role_attribute_value.as_static_value()?.text())?;
+                if role.is_interactive() && !role.is_composite() && tabindex_attribute.is_none() {
+                    return Some(role_attribute_value.to_trimmed_text().text().into());
+                }
+            }
+        }
+        None
+    }
+
+    fn diagnostic(ctx: &RuleContext<Self>, state: &Self::State) -> Option<RuleDiagnostic> {
+        let node = ctx.query();
+        Some(
+            RuleDiagnostic::new(
+                rule_category!(),
+                node.range(),
+                markup! {
+                    "The HTML element with the interactive role "<Emphasis>{state}</Emphasis>" is not focusable."
+                },
+            ).note(markup! {
+                "A non-interactive HTML element that is not focusable may not be reachable for users that rely on keyboard navigation, even with an added role like "<Emphasis>{state}</Emphasis>"."
+            })
+            .note(markup! {
+                "Add a "<Emphasis>"tabindex"</Emphasis>" attribute to make this element focusable."
+            }),
+        )
+    }
+}
