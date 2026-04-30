9d04dc4bc34b5c9a9f0c6a0c9720217a351d2846
Denis Bezrukov <6227442+denbezrukov@users.noreply.github.com>
2026-04-30 10:25:24 +0300

    feat(css_formatter): improve handling of SCSS `@while` rule formatting

--- crates/biome_css_formatter/src/scss/statements/while_at_rule.rs
+++ crates/biome_css_formatter/src/scss/statements/while_at_rule.rs
@@ -1,6 +1,7 @@
 use crate::prelude::*;
+use crate::utils::scss_expression::single_expression_item;
 use biome_css_syntax::{ScssWhileAtRule, ScssWhileAtRuleFields};
-use biome_formatter::write;
+use biome_formatter::{format_args, write};
 
 #[derive(Debug, Clone, Default)]
 pub(crate) struct FormatScssWhileAtRule;
@@ -13,13 +14,36 @@ impl FormatNodeRule<ScssWhileAtRule> for FormatScssWhileAtRule {
             block,
         } = node.as_fields();
 
+        let is_parenthesized_condition = condition.as_ref().is_ok_and(|condition| {
+            single_expression_item(condition)
+                .is_some_and(|item| item.as_scss_parenthesized_expression().is_some())
+        });
+
+        if is_parenthesized_condition {
+            return write!(
+                f,
+                [
+                    while_token.format(),
+                    space(),
+                    condition.format(),
+                    space(),
+                    block.format()
+                ]
+            );
+        }
+
+        let header_group_id = f.group_id("scss_while_header");
+
         write!(
             f,
             [
                 while_token.format(),
                 space(),
-                condition.format(),
-                space(),
+                group(&format_args![
+                    indent_if_group_breaks(&condition.format(), header_group_id),
+                    soft_line_break_or_space()
+                ])
+                .with_group_id(Some(header_group_id)),
                 block.format()
             ]
         )
