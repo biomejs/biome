aa8c8052a2d0a6f54d92eabf01e24f96a4f8a2b2
Denis Bezrukov <6227442+denbezrukov@users.noreply.github.com>
2026-04-30 12:00:47 +0300

    feat(css_formatter): improve handling of SCSS parenthesized binary expressions formatting

--- crates/biome_css_formatter/src/scss/auxiliary/binary_expression.rs
+++ crates/biome_css_formatter/src/scss/auxiliary/binary_expression.rs
@@ -1,5 +1,7 @@
 use crate::prelude::*;
-use biome_css_syntax::{ScssBinaryExpression, ScssBinaryExpressionFields};
+use biome_css_syntax::{
+    ScssBinaryExpression, ScssBinaryExpressionFields, ScssParenthesizedExpression,
+};
 use biome_formatter::{format_args, write};
 
 #[derive(Debug, Clone, Default)]
@@ -12,15 +14,36 @@ impl FormatNodeRule<ScssBinaryExpression> for FormatScssBinaryExpression {
             operator,
             right,
         } = node.as_fields();
+        let should_indent_right = is_nested_in_parenthesized_expression(node);
+        let formatted_right = format_with(|f| {
+            if should_indent_right {
+                write!(
+                    f,
+                    [indent(&format_args![
+                        soft_line_break_or_space(),
+                        right.format()
+                    ])]
+                )
+            } else {
+                write!(f, [soft_line_break_or_space(), right.format()])
+            }
+        });
+
         write!(
             f,
             [group(&format_args![
                 left.format(),
                 space(),
                 operator.format(),
-                soft_line_break_or_space(),
-                right.format()
+                formatted_right
             ])]
         )
     }
+}
+
+fn is_nested_in_parenthesized_expression(node: &ScssBinaryExpression) -> bool {
+    node.syntax()
+        .ancestors()
+        .skip(1)
+        .any(|ancestor| ScssParenthesizedExpression::can_cast(ancestor.kind()))
 }
