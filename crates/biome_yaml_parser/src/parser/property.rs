cc7412262189dc6647d54bee9ab4487945418f97
Vo Hoang Long <vo.hoang.long@outlook.com>
2026-05-04 22:40:42 +0900

    feat(yaml_parser): parse properties

--- crates/biome_yaml_parser/src/parser/property.rs
+++ crates/biome_yaml_parser/src/parser/property.rs
@@ -1,5 +1,5 @@
 use biome_parser::{
-    Parser,
+    CompletedMarker, Parser,
     parse_lists::ParseNodeList,
     parse_recovery::ParseRecoveryTokenSet,
     prelude::ParsedSyntax::{self, *},
@@ -18,12 +18,18 @@ impl ParseNodeList for PropertyList {
 
     const LIST_KIND: Self::Kind = YAML_PROPERTY_LIST;
 
-    fn parse_element(&mut self, _p: &mut Self::Parser<'_>) -> ParsedSyntax {
-        Absent
+    fn parse_element(&mut self, p: &mut Self::Parser<'_>) -> ParsedSyntax {
+        if p.at(ANCHOR_PROPERTY_LITERAL) {
+            Present(parse_anchor_property(p))
+        } else if p.at(TAG_PROPERTY_LITERAL) {
+            Present(parse_tag_property(p))
+        } else {
+            Absent
+        }
     }
 
-    fn is_at_list_end(&self, _p: &mut Self::Parser<'_>) -> bool {
-        true
+    fn is_at_list_end(&self, p: &mut Self::Parser<'_>) -> bool {
+        !is_at_property(p)
     }
 
     fn recover(
@@ -37,4 +43,20 @@ impl ParseNodeList for PropertyList {
             |p, range| p.err_builder("expected property", range),
         )
     }
+}
+
+fn parse_anchor_property(p: &mut YamlParser) -> CompletedMarker {
+    let m = p.start();
+    p.bump(ANCHOR_PROPERTY_LITERAL);
+    m.complete(p, YAML_ANCHOR_PROPERTY)
+}
+
+fn parse_tag_property(p: &mut YamlParser) -> CompletedMarker {
+    let m = p.start();
+    p.bump(TAG_PROPERTY_LITERAL);
+    m.complete(p, YAML_TAG_PROPERTY)
+}
+
+pub(super) fn is_at_property(p: &YamlParser) -> bool {
+    p.at(ANCHOR_PROPERTY_LITERAL) || p.at(TAG_PROPERTY_LITERAL)
 }
