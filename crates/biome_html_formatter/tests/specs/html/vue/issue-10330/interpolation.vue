d21daab0d371b4f410c9d8db202fc386afbc10fa
Puneet Dixit <rvit23bcs086.rvitm@rvei.edu.in>
2026-05-17 02:30:14 +0530

    test(html_formatter): add Vue interpolation snapshots

---
+++ crates/biome_html_formatter/tests/specs/html/vue/issue-10330/interpolation.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <span>
+      <div>
+        <v-btn v-if="store.state.user" variant="text" to="/my-rooms">
+          {{
+            $t('nav.my-rooms')
+          }}
+        </v-btn>
+      </div>
+    </span>
+  </div>
+</template>
+
+<template>
+<div>
+<span>
+<div>
+<v-btn v-if="store.state.user" variant="text" to="/my-rooms">{{
+	$t("nav.my-rooms")
+}}</v-btn>
+</div>
+</span>
+</div>
+</template>
