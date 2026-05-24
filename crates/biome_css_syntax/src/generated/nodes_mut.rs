d4369603088053250b8abdbe5f81668632116124
Denis Bezrukov <6227442+denbezrukov@users.noreply.github.com>
2026-05-24 16:40:10 +0300

    fix(css_parser): parse interpolated scss nested properties

--- crates/biome_css_syntax/src/generated/nodes_mut.rs
+++ crates/biome_css_syntax/src/generated/nodes_mut.rs
@@ -4530,7 +4530,7 @@ impl ScssNamespacedVariable {
     }
 }
 impl ScssNestingDeclaration {
-    pub fn with_name(self, element: CssIdentifier) -> Self {
+    pub fn with_name(self, element: AnyCssDeclarationName) -> Self {
         Self::unwrap_cast(
             self.syntax
                 .splice_slots(0usize..=0usize, once(Some(element.into_syntax().into()))),
