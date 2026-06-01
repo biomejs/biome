b55d10f5e8f3099b3f246911ca19cc79758d55e0
Carson McManus <dyc3@users.noreply.github.com>
2026-06-01 15:24:22 -0400

    fix(parse/js/vue): allow v-on directives with multiple inline statements (#10520)

--- crates/biome_service/src/file_handlers/html/parse_embedded_nodes.tests.rs
+++ crates/biome_service/src/file_handlers/html/parse_embedded_nodes.tests.rs
@@ -127,3 +127,90 @@ fn svelte_each_with_incorrect_method_call_key() {
 
     assert_diagnostics(FILE_PATH, FILE_CONTENT);
 }
+
+#[test]
+fn vue_v_on_accepts_inline_statements_and_expression_handlers() {
+    // Mirrors Vue's v-on handler cases:
+    // https://github.com/vuejs/core/blob/86ad0764fd9f7b01cef75b4fc941b03419306bf8/packages/compiler-core/__tests__/transforms/vOn.spec.ts#L148-L161
+    // https://github.com/vuejs/core/blob/86ad0764fd9f7b01cef75b4fc941b03419306bf8/packages/compiler-core/__tests__/transforms/vOn.spec.ts#L163-L179
+    // https://github.com/vuejs/core/blob/86ad0764fd9f7b01cef75b4fc941b03419306bf8/packages/compiler-core/__tests__/transforms/vOn.spec.ts#L181-L197
+    // https://github.com/vuejs/core/blob/86ad0764fd9f7b01cef75b4fc941b03419306bf8/packages/compiler-core/__tests__/transforms/vOn.spec.ts#L260-L273
+    // https://github.com/vuejs/core/blob/86ad0764fd9f7b01cef75b4fc941b03419306bf8/packages/compiler-core/__tests__/transforms/vOn.spec.ts#L305-L320
+    // https://github.com/vuejs/core/blob/86ad0764fd9f7b01cef75b4fc941b03419306bf8/packages/compiler-core/__tests__/transforms/vOn.spec.ts#L347-L370
+    // https://github.com/vuejs/core/blob/86ad0764fd9f7b01cef75b4fc941b03419306bf8/packages/compiler-core/__tests__/transforms/vOn.spec.ts#L372-L385
+    const FILE_PATH: &str = "/project/file.vue";
+    const FILE_CONTENT: &str = r#"<script setup>
+let counter = 0;
+function foo() {}
+function bar() {}
+const a = { b: foo };
+const c = "b";
+</script>
+<template>
+  <button @click="counter++; counter++;"></button>
+  <button @click="foo();bar()"></button>
+  <button @click="
+    counter++;
+    counter++;
+  "></button>
+  <button @click="foo"></button>
+  <button @click="a['b' + c]"></button>
+  <button @click="$event => foo($event)"></button>
+  <button @click="async $event => foo($event)"></button>
+  <button @click="function($event) { foo($event) }"></button>
+  <button v-on:click="counter++; counter++;"></button>
+</template>
+"#;
+
+    assert_no_diagnostics(FILE_PATH, FILE_CONTENT);
+}
+
+#[test]
+fn vue_v_on_accepts_typescript_function_handler() {
+    // Mirrors Vue's TypeScript function-expression v-on handler case:
+    // https://github.com/vuejs/core/blob/86ad0764fd9f7b01cef75b4fc941b03419306bf8/packages/compiler-core/__tests__/transforms/vOn.spec.ts#L275-L303
+    const FILE_PATH: &str = "/project/file.vue";
+    const FILE_CONTENT: &str = r#"<script lang="ts">
+let counter = 0;
+</script>
+<template>
+  <button @click="($event: MouseEvent) => counter++"></button>
+</template>
+"#;
+
+    assert_no_diagnostics(FILE_PATH, FILE_CONTENT);
+}
+
+#[test]
+fn vue_v_on_rejects_function_handler_with_trailing_statement() {
+    // Enforces Vue's split between function-expression handlers and inline
+    // statement handlers by combining these upstream cases:
+    // https://github.com/vuejs/core/blob/86ad0764fd9f7b01cef75b4fc941b03419306bf8/packages/compiler-core/__tests__/transforms/vOn.spec.ts#L260-L273
+    // https://github.com/vuejs/core/blob/86ad0764fd9f7b01cef75b4fc941b03419306bf8/packages/compiler-core/__tests__/transforms/vOn.spec.ts#L163-L179
+    const FILE_PATH: &str = "/project/file.vue";
+    const FILE_CONTENT: &str = r#"<template>
+  <button @click="() => {counter++}; counter++;"></button>
+</template>
+"#;
+
+    assert_diagnostics(FILE_PATH, FILE_CONTENT);
+}
+
+#[test]
+fn vue_non_event_directives_stay_expression_only() {
+    const FILE_PATH: &str = "/project/file.vue";
+    const FILE_CONTENT: &str = r#"<script setup>
+const isActive = true;
+const foo = "foo";
+const bar = "bar";
+const duration = 100;
+</script>
+<template>
+  <div :class="{ active: isActive }"></div>
+  <div v-bind="{ id: foo, class: bar }"></div>
+  <div>{{ { duration } }}</div>
+</template>
+"#;
+
+    assert_no_diagnostics(FILE_PATH, FILE_CONTENT);
+}
