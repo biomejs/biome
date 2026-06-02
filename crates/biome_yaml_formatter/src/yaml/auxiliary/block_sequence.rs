cb7f4bbdb9fc0eacb7d9d80c898c3920af84d9f3
Naoki Ikeguchi <me@s6n.jp>
2026-06-02 22:06:11 +0900

    fix(formatter/yaml): always format every token

--- crates/biome_yaml_formatter/src/yaml/auxiliary/block_sequence.rs
+++ crates/biome_yaml_formatter/src/yaml/auxiliary/block_sequence.rs
@@ -6,11 +6,18 @@ pub(crate) struct FormatYamlBlockSequence;
 impl FormatNodeRule<YamlBlockSequence> for FormatYamlBlockSequence {
     fn fmt_fields(&self, node: &YamlBlockSequence, f: &mut YamlFormatter) -> FormatResult<()> {
         let YamlBlockSequenceFields {
-            sequence_start_token: _,
+            sequence_start_token,
             entries,
-            sequence_end_token: _,
+            sequence_end_token,
         } = node.as_fields();
 
-        write!(f, [entries.format()])
+        write!(
+            f,
+            [
+                format_removed(&sequence_start_token?), // always empty, ignored
+                entries.format(),
+                format_removed(&sequence_end_token?), // always empty, ignored
+            ]
+        )
     }
 }
