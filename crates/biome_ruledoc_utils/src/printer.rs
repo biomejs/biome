544e15c63a7b7342ce9fc35df523183de68a09b6
Emanuele Stoppa <my.burning@gmail.com>
2026-06-22 14:29:48 +0100

    refactor(tools): provide diagnostics printer (#10732)

--- crates/biome_ruledoc_utils/src/printer.rs
+++ crates/biome_ruledoc_utils/src/printer.rs
@@ -2,25 +2,32 @@ use biome_console::{Console, markup};
 use biome_diagnostics::{DiagnosticExt, PrintDiagnostic};
 use biome_json_syntax::TextSize;
 
+pub trait DiagnosticWriter {
+    fn write_diagnostic(&mut self, diag: biome_diagnostics::Error);
+    fn write_parse_error(&mut self, diag: biome_diagnostics::Error);
+    fn print_all_diagnostics(&mut self);
+    fn subtract_offset(&mut self, offset: TextSize);
+}
+
 #[derive(Default)]
-pub struct DiagnosticWriter {
+pub struct DiagnosticConsoleWriter {
     pub all_diagnostics: Vec<biome_diagnostics::Error>,
     pub has_parse_error: bool,
     pub subtract_offset: TextSize,
 }
 
-impl DiagnosticWriter {
-    pub fn write_diagnostic(&mut self, diag: biome_diagnostics::Error) {
+impl DiagnosticWriter for DiagnosticConsoleWriter {
+    fn write_diagnostic(&mut self, diag: biome_diagnostics::Error) {
         self.all_diagnostics.push(self.adjust_span_offset(diag));
     }
 
-    pub fn write_parse_error(&mut self, diag: biome_diagnostics::Error) {
+    fn write_parse_error(&mut self, diag: biome_diagnostics::Error) {
         self.has_parse_error = true;
         self.write_diagnostic(diag);
     }
 
     /// Prints all diagnostics to help the user.
-    pub fn print_all_diagnostics(&mut self) {
+    fn print_all_diagnostics(&mut self) {
         let mut console = biome_console::EnvConsole::default();
         for diag in self.all_diagnostics.iter() {
             console.println(
@@ -32,6 +39,12 @@ impl DiagnosticWriter {
         }
     }
 
+    fn subtract_offset(&mut self, offset: TextSize) {
+        self.subtract_offset = offset;
+    }
+}
+
+impl DiagnosticConsoleWriter {
     /// Adjusts the location of the diagnostic to account for synthetic nodes
     /// that arent't present in the source code but only in the AST.
     pub fn adjust_span_offset(&self, diag: biome_diagnostics::Error) -> biome_diagnostics::Error {
