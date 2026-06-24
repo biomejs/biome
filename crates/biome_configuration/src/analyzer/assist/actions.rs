79580974201e2ba862bc54d51190bf82e61f0afb
Emanuele Stoppa <my.burning@gmail.com>
2026-06-24 18:47:45 +0100

    refactor: configuration opt-in lang

--- crates/biome_configuration/src/analyzer/assist/actions.rs
+++ crates/biome_configuration/src/analyzer/assist/actions.rs
@@ -1,7 +1,15 @@
 //! Generated file, do not edit by hand, see `xtask/codegen`
 
 use crate::analyzer::presets::PresetConfig;
-use crate::analyzer::{RuleAssistConfiguration, RuleAssistPlainConfiguration};
+#[cfg(any(
+    feature = "lang_css",
+    feature = "lang_graphql",
+    feature = "lang_html",
+    feature = "lang_js",
+    feature = "lang_json"
+))]
+use crate::analyzer::RuleAssistConfiguration;
+use crate::analyzer::RuleAssistPlainConfiguration;
 use biome_analyze::RulePreset;
 use biome_analyze::{RuleFilter, options::RuleOptions};
 use biome_deserialize_macros::{Deserializable, Merge};
