a21463e5f616a2db5035b470cd206ac9da4d9423
Carson McManus <dyc3@users.noreply.github.com>
2026-07-07 00:42:28 -0400

    fix(html): fix inner_string_text for unquoted attribute values (#10867)

--- crates/biome_html_syntax/src/lib.rs
+++ crates/biome_html_syntax/src/lib.rs
@@ -124,10 +124,14 @@ impl TryFrom<HtmlSyntaxKind> for TriviaPieceKind {
 pub fn inner_string_text(token: &HtmlSyntaxToken) -> TokenText {
     let mut text = token.token_text_trimmed();
     if token.kind() == HtmlSyntaxKind::HTML_STRING_LITERAL {
-        // remove string delimiters
-        // SAFETY: string literal token have a delimiters at the start and the end of the string
-        let range = TextRange::new(1.into(), text.len() - TextSize::from(1));
-        text = text.slice(range);
+        let text_str = text.text();
+        if text_str.len() >= 2
+            && ((text_str.starts_with('"') && text_str.ends_with('"'))
+                || (text_str.starts_with('\'') && text_str.ends_with('\'')))
+        {
+            let range = TextRange::new(1.into(), text.len() - TextSize::from(1));
+            text = text.slice(range);
+        }
     }
     text
 }
