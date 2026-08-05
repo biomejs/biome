6395bae697c692b5fa00b66d8e570bf49a6e063c
Carson McManus <carson.mcmanus1@gmail.com>
2026-08-05 14:08:40 -0400

    tests: unformatted tests

--- crates/biome_html_formatter/tests/specs/html/svelte/double_curly.svelte
+++ crates/biome_html_formatter/tests/specs/html/svelte/double_curly.svelte
@@ -3,8 +3,6 @@
 <p>{JSON.stringify({ foo: "bar" })}</p>
 <div class={{ active: isActive }}></div>
 
-<section>
-<p>{{ a: true }}</p>
-<p>{JSON.stringify({ foo: "bar" })}</p>
-<div class={{ active: isActive }}></div>
-</section>
+<p>{   { a: true }    }</p>
+<p>{JSON.stringify(  { foo: "bar" }  )}</p>
+<div class={   {  active: isActive }   }></div>
