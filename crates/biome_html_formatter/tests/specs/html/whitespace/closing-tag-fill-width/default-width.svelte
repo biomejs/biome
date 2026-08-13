67c3bf01131c0c079fa058eef96dc28d52c2c59f
Carson McManus <dyc3@users.noreply.github.com>
2026-08-13 08:50:17 -0400

    fix(format/html): include closing tags in fill width (#11325)

---
+++ crates/biome_html_formatter/tests/specs/html/whitespace/closing-tag-fill-width/default-width.svelte
@@ -0,0 +1,9 @@
+<a-long-long-long-element>foo bar foo bar foo bar foo bar foo bar foo bar foo bar</a-long-long-long-element>
+<span>short content</span>
+
+<a-long-long-long-element
+>foo bar foo bar foo bar foo bar foo bar foo bar foo bar</a-long-long-long-element
+>
+<span
+>short content</span
+>
