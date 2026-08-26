afb1c0733ebf5c177a8223772ad06f2dcda85b08
Levin Rickert <me@levrik.io>
2026-08-26 21:35:34 +0200

    fix(noVueRefAsOperand): cover destructured params in test

--- crates/biome_js_analyze/tests/specs/nursery/noVueRefAsOperand/valid-nested-callback-param.vue
+++ crates/biome_js_analyze/tests/specs/nursery/noVueRefAsOperand/valid-nested-callback-param.vue
@@ -3,4 +3,6 @@
 import { computed } from 'vue'
 
 const result = computed(() => [{ label: 'a' }].find((item) => item.label === 'a'))
+const result2 = computed(() => [{ label: 'a' }].find(({ label }) => label === 'a'))
+const result3 = computed(() => [['a']].find(([label]) => label === 'a'))
 </script>
