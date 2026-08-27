6a0ae3a15ac32659948dec17ea45de1df5434c05
Emanuele Stoppa <my.burning@gmail.com>
2026-08-27 15:55:44 +0100

    fix build

--- crates/biome_configuration/src/overrides.rs
+++ crates/biome_configuration/src/overrides.rs
@@ -131,6 +131,10 @@ impl OverridePattern {
             files,
             #[cfg(feature = "plugins")]
             plugins,
+            #[cfg(feature = "lang_md")]
+            mut markdown,
+            #[cfg(feature = "lang_yaml")]
+            mut yaml,
         } = self.clone();
 
         #[cfg(feature = "lang_js")]
@@ -284,6 +288,37 @@ impl OverridePattern {
             formatter.bracket_same_line = formatter.bracket_same_line.or(global.bracket_same_line);
             formatter.attribute_position =
                 formatter.attribute_position.or(global.attribute_position);
+            formatter.trailing_newline = formatter.trailing_newline.or(global.trailing_newline);
+        }
+
+        #[cfg(feature = "lang_md")]
+        if let Some(global) = formatter.as_ref() {
+            let formatter = markdown
+                .get_or_insert_with(Default::default)
+                .formatter
+                .get_or_insert_with(Default::default);
+            formatter.enabled = formatter
+                .enabled
+                .or(global.enabled.map(|enabled| enabled.value().into()));
+            formatter.indent_style = formatter.indent_style.or(global.indent_style);
+            formatter.indent_width = formatter.indent_width.or(global.indent_width);
+            formatter.line_ending = formatter.line_ending.or(global.line_ending);
+            formatter.line_width = formatter.line_width.or(global.line_width);
+            formatter.trailing_newline = formatter.trailing_newline.or(global.trailing_newline);
+        }
+
+        #[cfg(feature = "lang_yaml")]
+        if let Some(global) = formatter.as_ref() {
+            let formatter = yaml
+                .get_or_insert_with(Default::default)
+                .formatter
+                .get_or_insert_with(Default::default);
+            formatter.enabled = formatter
+                .enabled
+                .or(global.enabled.map(|enabled| enabled.value().into()));
+            formatter.indent_width = formatter.indent_width.or(global.indent_width);
+            formatter.line_ending = formatter.line_ending.or(global.line_ending);
+            formatter.line_width = formatter.line_width.or(global.line_width);
             formatter.trailing_newline = formatter.trailing_newline.or(global.trailing_newline);
         }
 
