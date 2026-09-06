c6b0bd2941f2e2a160921b1c42790745b56014ed
Mnigos <makowskiigor@gmail.com>
2026-09-07 01:27:22 +0200

    test(a11y): cover load handlers combined with interactions

--- crates/biome_html_analyze/tests/specs/a11y/noNoninteractiveElementInteractions/svelte/invalidResourceEvents.svelte
+++ crates/biome_html_analyze/tests/specs/a11y/noNoninteractiveElementInteractions/svelte/invalidResourceEvents.svelte
@@ -2,3 +2,6 @@
 <img src="image.png" alt="Example" onerror={() => {}} onclick={() => {}} />
 <img src="image.png" alt="Example" onerror={() => {}} onkeydown={() => {}} />
 <img src="image.png" alt="Example" onerror={() => {}} onfocus={() => {}} />
+<img src="image.png" alt="Example" onload={() => {}} onclick={() => {}} />
+<img src="image.png" alt="Example" onload={() => {}} onkeydown={() => {}} />
+<img src="image.png" alt="Example" onload={() => {}} onfocus={() => {}} />
