22e9966804906969d1fa9d411aacf31ebfa63d76
Ziwen <ziwensong.cs@gmail.com>
2026-09-11 09:06:12 -0400

    fix(format): preserve template closing brace indentation (#11461)

---
+++ crates/biome_js_formatter/tests/specs/js/module/template/odd_indentation/odd_indentation.js
@@ -0,0 +1,15 @@
+const fiveSpaceIndent = (data) => `
+     ${
+       data
+       ?`<bar>${data.aBitLongerNameHelpingReproduceThisProblem}</bar>`
+       :''
+     }
+`
+
+const sixSpaceIndent = (data) => `
+      ${
+       data
+       ?`<bar>${data.aBitLongerNameHelpingReproduceThisProblem}</bar>`
+       :''
+     }
+`
