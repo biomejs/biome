41dc9af6a88e333397f006ca7c7654199199bf98
Denis Bezrukov <6227442+denbezrukov@users.noreply.github.com>
2026-09-13 15:35:08 +0300

    test(css_formatter): remove deferred escape test registration

--- crates/biome_css_formatter/tests/spec_tests.rs
+++ crates/biome_css_formatter/tests/spec_tests.rs
@@ -10,17 +10,3 @@ mod formatter {
         tests_macros::gen_tests! {"tests/specs/scss/**/*.scss", crate::spec_test::run, ""}
     }
 }
-
-#[test]
-#[ignore = "Deferred: hex-escape terminators gain a space before comments on reformat"]
-fn deferred_escaped_identifier_comments_scss() {
-    crate::spec_test::run(
-        concat!(
-            env!("CARGO_MANIFEST_DIR"),
-            "/tests/specs/deferred/scss/parity-function-adjacency-escape-comments.scss"
-        ),
-        "",
-        concat!(env!("CARGO_MANIFEST_DIR"), "/tests/specs/deferred/scss"),
-        "",
-    );
-}
